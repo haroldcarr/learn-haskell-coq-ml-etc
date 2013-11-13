9b9302d7e59e21ba7882963019e338f8f29d7041
Harold Carr <harold.carr@gmail.com>
2013-11-12 22:15:03 -0800

    did 99 questions 11-20

--- haskell/paper/haskellwiki/X99Questions.hs
+++ haskell/paper/haskellwiki/X99Questions.hs
@@ -1,11 +1,13 @@
 {-
 Created       : 2013 Nov 11 (Mon) 20:51:18 by carr.
-Last Modified : 2013 Nov 11 (Mon) 23:08:22 by carr.
+Last Modified : 2013 Nov 12 (Tue) 22:13:42 by carr.
 -}
 
 import Control.Arrow -- for "&&&"
 import Test.HUnit
 import Test.HUnit.Util -- https://github.com/haroldcarr/test-hunit-util
+
+-- Problem 1
 
 myLast :: [a] -> a
 myLast [] = error "myLast of empty"
@@ -15,6 +17,8 @@ myLast (h:h2:t) = myLast (h2:t)
 t1 = t "t1" (myLast          "z")  'z'
 t2 = t "t2" (myLast        "xyz")  'z'
 t3 = t "t3" (myLast ['a' ..  'z']) 'z'
+
+-- Problem 2
 
 myButLast :: [a] -> a
 myButLast [] = error "myButLast of empty"
@@ -26,6 +30,8 @@ t4 = t "t4" (myButLast         "yz")  'y'
 t5 = t "t5" (myButLast        "xyz")  'y'
 t6 = t "t6" (myButLast ['a' ..  'z']) 'y'
 
+-- Problem 3
+
 elementAt :: [a] -> Int -> a
 elementAt [] _ = error "elementAt of emptyList"
 elementAt (h:_) 1 = h
@@ -34,11 +40,15 @@ elementAt (_:t) n = elementAt t (n - 1)
 t7 = t "t7" (elementAt   [1,2,3] 2) 2
 t8 = t "t8" (elementAt "haskell" 5) 'e'
 
+-- Problem 4
+
 myLength :: [a] -> Int
 myLength = foldl (\n _ -> n + 1) 0
 
 t9  = t  "t9" (myLength [123, 456, 789]) 3
 t10 = t "t10" (myLength "Hello, world!") 13
+
+-- Problem 5
 
 myReverse :: [a] -> [a]
 myReverse = foldl (flip (:)) []
@@ -46,12 +56,16 @@ myReverse = foldl (flip (:)) []
 t11 = t "t11" (myReverse "A man, a plan, a canal, panama!") "!amanap ,lanac a ,nalp a ,nam A"
 t12 = t "t12" (myReverse [1,2,3,4]) [4,3,2,1]
 
+-- Problem 6
+
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome xs = xs == myReverse xs
 
 t13 = t "t13" (isPalindrome [1,2,3]) False
 t14 = t "t14" (isPalindrome "madamimadam") True
 t15 = t "t15" (isPalindrome [1,2,4,8,16,8,4,2,1]) True
+
+-- Problem 7
 
 data NestedList a = Elem a | List [NestedList a] deriving (Eq, Show)
 flatten :: NestedList a -> [a]
@@ -63,6 +77,8 @@ t16 = t "t16" (flatten (Elem 5)) [5]
 t17 = t "t17" (flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])) [1,2,3,4,5]
 t18 = t "t18" (flatten (List [])) ([] :: [Int])
 
+-- Problem 8
+
 compress :: Eq a => [a] -> [a]
 compress [] = []
 compress (h:t) = h : compress' t h
@@ -71,6 +87,8 @@ compress (h:t) = h : compress' t h
                                         else h : compress' t h
 
 t19 = t "t19" (compress "aaaabccaadeeee") "abcade"
+
+-- Problem 9
 
 pack :: Eq a => [a] -> [[a]]
 pack [] = []
@@ -81,12 +99,117 @@ pack (h:t) = pack' [[h]] t
 
 t20 = t "t20" (pack "aaaabccaadeeee") ["aaaa","b","cc","aa","d","eeee"]
 
+-- Problem 10
+
 encode :: Eq a => [a] -> [(Int, a)]
 encode xs = map (length &&& head) (pack xs)
 
 t21 = t "t21" (encode  "aaaabccaadeeee") [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
 
-main = runTestTT $ TestList $ t1 ++ t2 ++ t3 ++ t4 ++ t5 ++ t6 ++ t7 ++ t8 ++ t9 ++ t10 ++
+-- Problem 11
+
+data Encoding a = Multiple Int a | Single a deriving (Eq, Show)
+encodeModified :: Eq a => [a] -> [Encoding a]
+encodeModified xs = map (\(i, a) -> if i == 1 then Single a else Multiple i a) (encode xs)
+
+t22 = t "t22" (encodeModified "aaaabccaadeeee") [Multiple 4 'a',Single 'b',Multiple 2 'c',  Multiple 2 'a',Single 'd',Multiple 4 'e']
+
+-- Problem 12
+
+decodeModified :: Eq a => [Encoding a] -> [a]
+decodeModified exs = foldr step [] exs
+    where step (Multiple i a) acc = replicate i a ++ acc
+          step (Single     a) acc =             a :  acc
+
+t23 = t "t23" (decodeModified [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e']) "aaaabccaadeeee"
+
+-- Problem 13
+
+encodeDirect :: Eq a => [a] -> [Encoding a]
+encodeDirect    [] = []
+encodeDirect (h:t) = encodeDirect' [] 1 h t
+    where encodeDirect' acc n c    []  =                               acc ++ mkEncoding n c
+          encodeDirect' acc n c (x:xs) = if c == x then encodeDirect'  acc                   (n + 1) c xs
+                                                   else encodeDirect' (acc ++ mkEncoding n c) 1      x xs
+          mkEncoding 1 c = [Single     c]
+          mkEncoding n c = [Multiple n c]
+
+t24 = t "t24" (encodeDirect "aaaabccaadeeee") [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e']
+
+-- Problem 14
+
+dupli :: [a] -> [a]
+dupli xs = foldr (\x acc -> x:x:acc) [] xs
+
+t25 = t "t25" (dupli "abccd") "aabbccccdd"
+t26 = t "t26" (dupli [1,2,3]) [1,1,2,2,3,3]
+
+-- Problem 15
+repli :: [a] -> Int -> [a]
+repli xs n = foldr (\x acc -> replicate n x ++ acc) [] xs
+
+t27 = t "t27" (repli "abc" 3) "aaabbbccc"
+
+-- Problem 16
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery xs n = dropEvery' xs n
+    where dropEvery'    [] _  = []
+          dropEvery' (h:t) 1  =     dropEvery' t  n
+          dropEvery' (h:t) n' = h : dropEvery' t (n' - 1)
+
+t28 = t "t28" (dropEvery "abcdefghik" 3) "abdeghk"
+
+-- Problem 17
+
+split :: [a] -> Int -> ([a], [a])
+split xs n = (take n xs, drop n xs)
+
+t29 = t "t29" (split "abcdefghik" 3) ("abc", "defghik")
+
+-- Problem 18
+
+slice :: [a] -> Int -> Int -> [a]
+slice   []     _  _ = []
+slice (h:t) from to =
+    if from <= 1 && to >= 1
+    then h : slice t (from -1) (to - 1)
+    else     slice t (from -1) (to - 1)
+
+t30 = t "t30" (slice "abcdefghik" 3 7) "cdefg"
+
+-- Problem 19
+
+rotate :: [a] -> Int -> [a]
+rotate xs n
+    | n > 0     = rotateLeft  xs n
+    | otherwise = rotateRight xs n
+  where
+    rotateLeft     xs 0 = xs
+    rotateLeft  (h:t) n = rotateLeft (t ++ [h]) (n - 1)
+    rotateRight    xs 0 = xs
+    rotateRight    xs n = rotateRight ((last xs) : (init xs)) (n + 1)
+
+t31 = t "t31" (rotate ['a','b','c','d','e','f','g','h']   3 ) "defghabc"
+t32 = t "t32" (rotate ['a','b','c','d','e','f','g','h'] (-2)) "ghabcdef"
+
+-- Problem 20
+
+removeAt :: Int -> [a] -> (a, [a])
+removeAt n xs = removeAt' n [] xs
+    where removeAt' _   _   []  = error "n too big"
+          removeAt' 1 acc (h:t) = (h, acc ++ t)
+          removeAt' n acc (h:t) = removeAt' (n - 1) (h:acc) t
+
+
+t33 = t "t33" (removeAt  2 "abcd") ('b',"acd")
+t34 = t "t34" (removeAt 10 "abcd") ('b',"acd")
+
+
+-- ------------------------------------------------------------------------------
+
+main = runTestTT $ TestList $  t1 ++  t2 ++  t3 ++  t4 ++  t5 ++  t6 ++  t7 ++  t8 ++  t9 ++ t10 ++
                               t11 ++ t12 ++ t13 ++ t14 ++ t15 ++ t16 ++ t17 ++ t18 ++ t19 ++ t20 ++
-                              t21
+                              t21 ++ t22 ++ t23 ++ t24 ++ t25 ++ t26 ++ t27 ++ t28 ++ t29 ++ t30 ++
+                              t31 ++ t32 ++ t33 ++ t34
 
