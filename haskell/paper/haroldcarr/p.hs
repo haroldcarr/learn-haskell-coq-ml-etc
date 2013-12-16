3ba0a4c0eb28f001c63d76b1d8051d21fa487ef3
Harold Carr <harold.carr@gmail.com>
2013-12-16 12:05:36 -0800

    learning concurrent

--- haskell/paper/haroldcarr/p.hs
+++ haskell/paper/haroldcarr/p.hs
@@ -1,54 +1,91 @@
 {-
 Created       : 2013 Dec 15 (Sun) 21:08:32 by carr.
-Last Modified : 2013 Dec 16 (Mon) 10:25:18 by carr.
+Last Modified : 2013 Dec 16 (Mon) 12:02:53 by carr.
 -}
 
 import Control.Concurrent
 import Control.Monad (when)
 import Data.Numbers.Primes (isPrime)
-import System.IO.Unsafe (unsafePerformIO)
 
-listPrimesInRange :: Integral a => a -> a -> Int
-listPrimesInRange i block = listPrimesInRange' ((i * block) + 1) []
+import Test.HUnit       as T
+import Test.HUnit.Util  as U -- https://github.com/haroldcarr/test-hunit-util
+import System.IO.Unsafe -- for unit tests
+
+default (Integer)
+
+------------------------------------------------------------------------------
+numPrimesInRange :: Integral a => a -> a -> Int
+numPrimesInRange i block = numPrimesInRange' ((i * block) + 1) []
   where
-    listPrimesInRange' j acc
-        | j <= (i + 1) * block = listPrimesInRange' (j + 1) (if isPrime j then j:acc else acc)
+    numPrimesInRange' j acc
+        | j <= (i + 1) * block = numPrimesInRange' (j + 1) (if isPrime j then j:acc else acc)
         | otherwise            = length acc -- could calculate length at each step, but WANT to take the unnecessary time hit here
 
 
-lpir :: Integral a => a -> a -> IO [Int]
-lpir j block = do
-    counts <- newEmptyMVar
-    putMVar counts []
-    _ <- lpir' 0 counts
-    takeMVar counts
+listNumPrimesInRanges :: Integral a => a -> a -> IO [Int]
+listNumPrimesInRanges j block = do
+    numPrimesFoundInEachBlock <- newEmptyMVar ; putMVar numPrimesFoundInEachBlock []
+    children                  <- newMVar []
+    listNumPrimesInRanges' children 0 numPrimesFoundInEachBlock
+    waitForChildren children
+    takeMVar numPrimesFoundInEachBlock
  where
-    lpir' i counts | i <= j = do _ <- forkIO (push (listPrimesInRange i block) counts)
-                                 lpir' (i+1) counts
-                   | otherwise = return ()
+    listNumPrimesInRanges' c i np | i <= j = do forkChild c (push (numPrimesInRange i block) np)
+                                                listNumPrimesInRanges' c (i+1) np
+                                  | otherwise = return ()
 
 
 push :: Int -> MVar [Int] -> IO ()
-push x counts = do
-    v <- takeMVar counts
+push x numPrimesFoundInEachBlock = do
+    v <- takeMVar numPrimesFoundInEachBlock
     let v' = x:v
-    putMVar counts v'
+    putMVar numPrimesFoundInEachBlock v'
 
 
 {-
-listPrimesInRange 0 (10^9)
-listPrimesInRange 0 (10^7)
+numPrimesInRange 0 (10^9)
+numPrimesInRange 0 (10^7)
 => Segmentation fault: 11
-
-listPrimesInRange 0 (10^5)
-listPrimesInRange 1 (10^5)
-listPrimesInRange 2 (10^5)
-listPrimesInRange 3 (10^5)
-listPrimesInRange 4 (10^5)
-listPrimesInRange 5 (10^5)
-
-lpir 5 (10^5)
 -}
+
+tr0 :: [T.Test]
+tr0 = U.t "tr0"
+      (numPrimesInRange 0 (10^5))
+      9592
+
+tr1 :: [T.Test]
+tr1 = U.t "tr1"
+      (numPrimesInRange 1 (10^5))
+      8392
+
+tr2 :: [T.Test]
+tr2 = U.t "tr2"
+      (numPrimesInRange 2 (10^5))
+      8013
+
+tr3 :: [T.Test]
+tr3 = U.t "tr3"
+      (numPrimesInRange 3 (10^5))
+      7863
+
+tr4 :: [T.Test]
+tr4 = U.t "tr4"
+      (numPrimesInRange 4 (10^5))
+      7678
+
+tr5 :: [T.Test]
+tr5 = U.t "tr5"
+      (numPrimesInRange 5 (10^5))
+      7560
+
+trExpectedResult :: [Int]
+trExpectedResult = [7560,7678,7863,8013,8392,9592]
+tr :: [T.Test]
+tr = U.t "tr"
+     (unsafePerformIO (listNumPrimesInRanges 5 (10^5)))
+     trExpectedResult
+
+------------------------------------------------------------------------------
 
 inc :: MVar Int -> IO Int
 inc count = do { v <- takeMVar count; putMVar count (v+1); return v }
@@ -58,63 +95,73 @@ findPrime limit ints primes = do
     i <- inc ints
     when (i < limit) $
         if isPrime i
-        then do _ <- inc primes
+        then do inc primes
                 findPrime limit ints primes
         else findPrime limit ints primes
 
-fop :: IO (Int, Int)
-fop = do
-    i <- newEmptyMVar
-    putMVar i 2
-    p <- newEmptyMVar
-    putMVar p 0
-    findPrime 10000 i p
-    ri <- takeMVar i
-    rp <- takeMVar p
+fop :: Int -> IO (Int, Int)
+fop limit = do
+    intSupply      <- newEmptyMVar ; putMVar intSupply      2
+    numPrimesFound <- newEmptyMVar ; putMVar numPrimesFound 0
+    findPrime limit intSupply numPrimesFound
+    ri <- takeMVar intSupply
+    rp <- takeMVar numPrimesFound
     return (ri, rp)
-
-intSupply :: IO (MVar a)
-intSupply = newEmptyMVar
-
-numPrimesFound :: IO (MVar a)
-numPrimesFound = newEmptyMVar
 
 fp :: (Num a, Ord a) => a -> Int -> IO (Int, Int)
 fp j limit = do
-    i <- intSupply
-    putMVar i 2
-    p <- numPrimesFound
-    putMVar p 0
-    _ <- fp' 0 i p
-    waitForChildren
-    ri <- takeMVar i
-    rp <- takeMVar p
+    intSupply      <- newEmptyMVar ; putMVar intSupply      2
+    numPrimesFound <- newEmptyMVar ; putMVar numPrimesFound 0
+    children       <- newMVar []
+    fp' children 0 intSupply numPrimesFound
+    waitForChildren children
+    ri <- takeMVar intSupply
+    rp <- takeMVar numPrimesFound
     return (ri, rp)
  where
-    fp' i ints primes  | i <= j = do _ <- forkChild (findPrime limit ints primes)
-                                     fp' (i+1) ints primes
-                       | otherwise = return ()
+    fp' c i ints primes  | i <= j = do forkChild c (findPrime limit ints primes)
+                                       fp' c (i+1) ints primes
+                         | otherwise = return ()
 
+tfop :: [T.Test]
+tfop = U.t "tfop"
+       (unsafePerformIO (fop (10^6)))
+       (1000001,78498)
 
+tfp :: [T.Test]
+tfp = U.t "tfp"
+      (unsafePerformIO (fp 6 (10^6)))
+      (1000007, sum trExpectedResult)
+{-
+expected: (1000007,49098)
+ but got: (1000007,78498)
+-}
+
+------------------------------------------------------------------------------
 -- from http://www.haskell.org/ghc/docs/7.6.2/html/libraries/base/Control-Concurrent.html
-children :: MVar [MVar ()]
-children = unsafePerformIO (newMVar [])
 
-waitForChildren :: IO ()
-waitForChildren = do
+waitForChildren :: MVar [MVar a] -> IO ()
+waitForChildren children = do
     cs <- takeMVar children
     case cs of
         []   -> return ()
         m:ms -> do
             putMVar children ms
             takeMVar m
-            waitForChildren
+            waitForChildren children
 
-forkChild :: IO () -> IO ThreadId
-forkChild io = do
+forkChild :: MVar [MVar ()] -> IO a -> IO ThreadId
+forkChild children io = do
     mvar <- newEmptyMVar
     childs <- takeMVar children
     putMVar children (mvar:childs)
     forkFinally io (\_ -> putMVar mvar ())
+
+------------------------------------------------------------------------------
+
+runTests :: IO Counts
+runTests =
+    T.runTestTT $ TestList $ tr0 ++ tr1 ++ tr2 ++ tr3 ++ tr4 ++ tr5 ++ tr ++
+                             tfop ++ tfp
 
 -- End of file.
