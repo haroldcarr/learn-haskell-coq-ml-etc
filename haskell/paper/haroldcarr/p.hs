8d587b2017ef1197ce7c651ee3885bc737ed8ffa
Harold Carr <harold.carr@gmail.com>
2013-12-16 22:03:41 -0800

    more cleanup

--- haskell/paper/haroldcarr/p.hs
+++ haskell/paper/haroldcarr/p.hs
@@ -1,6 +1,6 @@
 {-
 Created       : 2013 Dec 15 (Sun) 21:08:32 by carr.
-Last Modified : 2013 Dec 16 (Mon) 18:31:38 by carr.
+Last Modified : 2013 Dec 16 (Mon) 21:49:41 by carr.
 -}
 
 import Control.Concurrent
@@ -13,32 +13,77 @@ import System.IO.Unsafe -- for unit tests
 
 default (Integer)
 
+{-
+Example from
+_The Art of Multiprocessor Programming, Revised Reprint_, Maurice Herlihy, Nir Shavit
+Section 1.1
+-}
+
 ------------------------------------------------------------------------------
-listNumPrimesInRanges :: Integral a => a -> a -> IO [Int]
-listNumPrimesInRanges j block = do
+
+listNumPrimesInRangesTo :: Integral a => a -> IO [Int]
+listNumPrimesInRangesTo block = do
     numPrimesFoundInEachBlock <- newMVar []
     children                  <- newMVar []
-    listNumPrimesInRanges' children 0 numPrimesFoundInEachBlock
+    listNumPrimesInRangesTo' children 0 numPrimesFoundInEachBlock
     waitForChildren children
     takeMVar numPrimesFoundInEachBlock
- where
-    listNumPrimesInRanges' c i np | i <= j = do forkChild c (push (numPrimesInRange i block) np)
-                                                listNumPrimesInRanges' c (i+1) np
-                                  | otherwise = return ()
+ where -- the algorithm requires always forking 10 children
+    listNumPrimesInRangesTo' c i np | i <= 9 = do forkChild c (numPrimesInRangeIO i block np)
+                                                  listNumPrimesInRangesTo' c (i+1) np
+                                    | otherwise = return ()
+
+
+listNumPrimesInRangesToSequential :: Integral a => a -> [Int]
+listNumPrimesInRangesToSequential block = lnps 0 []
+  where
+    lnps i acc | i <= 9    = lnps (i + 1) ((numPrimesInRange i block):acc)
+               | otherwise = acc
 
 
 numPrimesInRange :: Integral a => a -> a -> Int
-numPrimesInRange i block = numPrimesInRange' ((i * block) + 1) []
+numPrimesInRange i block  = numPrimesInRange' ((i * block) + 1) []
   where
     numPrimesInRange' j acc
         | j <= (i + 1) * block = numPrimesInRange' (j + 1) (if isPrime j then j:acc else acc)
         | otherwise            = length acc -- could calculate length at each step, but WANT to take the unnecessary time hit here
+
+numPrimesInRangeIO :: Integral a => a -> a -> MVar [Int] -> IO ()
+numPrimesInRangeIO i block primes = do
+    let p = numPrimesInRange i block
+    push p primes
+--    tid <- myThreadId
+--    forkIO (putStrLn ("id: " ++ (show tid) ++ " ; prime: " ++ (show p)))
+--    putStrLn (show tid ++ " DONE")
+--    return ()
 
 
 {-
 numPrimesInRange 0 (10^9)
 numPrimesInRange 0 (10^7)
 => Segmentation fault: 11
+
+10^5
+=> 100000
+(0 * (10^5)) + 1
+1
+(0 + 1) * (10^5)
+100000
+
+(1 * (10^5)) + 1
+100001
+(1 + 1) * (10^5)
+200000
+
+(5 * (10^5)) + 1
+500001
+(5 + 1) * (10^5)
+600000
+
+(9 * (10^5)) + 1
+900001
+(9 + 1) * (10^5)
+1000000
 -}
 
 tr0 :: [T.Test]
@@ -48,44 +93,35 @@ tr0 = U.t "tr0"
 
 tr1 :: [T.Test]
 tr1 = U.t "tr1"
-      (numPrimesInRange 1 (10^5))
-      8392
-
-tr2 :: [T.Test]
-tr2 = U.t "tr2"
       (numPrimesInRange 2 (10^5))
       8013
 
-tr3 :: [T.Test]
-tr3 = U.t "tr3"
-      (numPrimesInRange 3 (10^5))
-      7863
-
-tr4 :: [T.Test]
-tr4 = U.t "tr4"
+tr2 :: [T.Test]
+tr2 = U.t "tr2"
       (numPrimesInRange 4 (10^5))
       7678
 
-tr5 :: [T.Test]
-tr5 = U.t "tr5"
-      (numPrimesInRange 5 (10^5))
-      7560
-
 trExpectedResult :: [Int]
-trExpectedResult = [7560,7678,7863,8013,8392,9592]
+trExpectedResult = [7224,7323,7408,7445,7560,7678,7863,8013,8392,9592]
+
 tr :: [T.Test]
 tr = U.t "tr"
-     (unsafePerformIO (listNumPrimesInRanges 5 (10^5)))
+     (unsafePerformIO (listNumPrimesInRangesTo (10^5)))
+     trExpectedResult
+
+trs :: [T.Test]
+trs = U.t "trs"
+     (listNumPrimesInRangesToSequential (10^5))
      trExpectedResult
 
 ------------------------------------------------------------------------------
 
 findPrimesTo :: (Num a, Ord a) => a -> Int -> IO (Int, Int)
-findPrimesTo numChildren limit = do
+findPrimesTo numChildren limit = do -- numChildren is 0-based
     intSupply      <- newMVar 2
     numPrimesFound <- newMVar 0
     children       <- newMVar []
-    findPrimesTo' children 1 intSupply numPrimesFound
+    findPrimesTo' children 0 intSupply numPrimesFound
     waitForChildren children
     ri <- takeMVar intSupply
     rp <- takeMVar numPrimesFound
@@ -98,25 +134,22 @@ findPrimesTo numChildren limit = do
 findPrime :: Int -> MVar Int -> MVar Int -> IO ()
 findPrime limit ints primes = do
     i <- getAndInc ints
-    when (i < limit) $
-        if isPrime i
-        then do getAndInc primes
-                findPrime limit ints primes
-        else findPrime limit ints primes
+    when (i < limit) $ do
+        when (isPrime i) $ do
+            getAndInc primes
+            return ()
+        findPrime limit ints primes
 
 -- http://answers.yahoo.com/question/index?qid=1006050901081
-expectedTenToTheSixthResult :: Int
-expectedTenToTheSixthResult = 78498
-
 tfp0 :: [T.Test]
 tfp0 = U.t "tfp0"
-       (unsafePerformIO (findPrimesTo 1 (10^6)))
-       (1000001, expectedTenToTheSixthResult)
+       (unsafePerformIO (findPrimesTo 0 (10^6)))
+       (1000001, sum trExpectedResult)
 
 tfp1 :: [T.Test]
 tfp1 = U.t "tfp1"
-      (unsafePerformIO (findPrimesTo 6 (10^6)))
-      (1000006, expectedTenToTheSixthResult)
+       (unsafePerformIO (findPrimesTo 4 (10^6)))
+       (1000005, sum trExpectedResult)
 
 ------------------------------------------------------------------------------
 
@@ -150,7 +183,7 @@ forkChild children io = do
 
 runTests :: IO Counts
 runTests =
-    T.runTestTT $ TestList $ tr0 ++ tr1 ++ tr2 ++ tr3 ++ tr4 ++ tr5 ++ tr ++
+    T.runTestTT $ TestList $ tr0 ++ tr1 ++ tr2 ++ tr ++ trs ++
                              tfp0 ++ tfp1
 
 -- End of file.
