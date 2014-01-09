2d06e3764c70c659ad010feee8b6418e4a411aac
Harold Carr <harold.carr@gmail.com>
2014-01-08 21:25:02 -0800

    working on sparql

--- haskell/interview/s.hs
+++ haskell/interview/s.hs
@@ -1,8 +1,12 @@
 {-
 Created       : 2013 Dec 23 (Mon) 23:15:11 by carr.
-Last Modified : 2013 Dec 24 (Tue) 22:32:34 by carr.
+Last Modified : 2014 Jan 03 (Fri) 21:43:11 by Harold Carr.
 -}
 
+module SS
+where
+
+import Data.Text as T
 import Database.HSparql.Connection
 import Database.HSparql.QueryGenerator
 
@@ -14,9 +18,20 @@ q = do
     triple s p o
     return SelectQuery { queryVars = [s, p, o] }
 
-dq :: IO ()
-dq = do
-    result <- selectQuery "http://localhost:3030/ds/query" q
+-- PREFIX dc: <http://purl.org/dc/elements/1.1/> INSERT DATA {  <http://example/book1> dc:title "A new book" ; dc:creator "A.N.Other" . }
+q' :: Query SelectQuery
+q' = do
+    dc <- prefix "dc" (iriRef "http://purl.org/dc/elements/1.1/")
+    ex <- prefix "ex" (iriRef "http://example/")
+    x  <- var
+
+    triple (ex .:. "book1")  (dc .:. "title") (T.pack "A new book")
+
+    return SelectQuery { queryVars = [x] }
+
+dq :: Query SelectQuery -> IO ()
+dq query = do
+    result <- selectQuery "http://localhost:3030/ds/query" query
     case result of
         (Just s) -> putStrLn . show $ s
         Nothing  -> error "bad"
