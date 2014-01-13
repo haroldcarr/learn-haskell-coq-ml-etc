79478cf3d36d8cb4c76ad54624de1e1b00613711
Harold Carr <harold.carr@gmail.com>
2014-01-12 19:59:33 -0800

    working - cleaned things up a bit - needs more refactoring, especially RDF usage.

--- haskell/interview/s.hs
+++ haskell/interview/s.hs
@@ -1,36 +1,28 @@
 {-
 Created       : 2013 Dec 23 (Mon) 23:15:11 by carr.
-Last Modified : 2014 Jan 12 (Sun) 16:03:43 by Harold Carr.
+Last Modified : 2014 Jan 12 (Sun) 19:03:44 by Harold Carr.
 
 TODO:
-- Use Shelly to start up fuseki and then program that runs through it using below
+- use Shelly to
+  - start up fuseki
+  - run test program
+- CLI with flags instead of order (and to enable optional arguments)
+  - optional: dbAddress; multiple users, groups, permissions in one CLI command
+- factor RDF utilities
+- use Lens
+- threepenny-gui
 -}
 
 import Control.Monad (unless)
+import Data.RDF.Types (Node(..), LValue(..))
 import Data.String.Utils (replace)
 import Data.Text as T (pack, unpack)
 import Database.HSparql.Connection
 import Database.HSparql.QueryGenerator
 import System.Environment (getArgs)
 
-dbAddress         = "http://localhost:3030/ds/"
-dbQueryAddress    = dbAddress ++ "query"
-dbUpdateAddress   = dbAddress ++ "update"
-emailAddress      = ohcP "emailAddress"
-group             = ohcP "group"
-groupName         = ohcP "groupName"
-hasPermissions    = ohcP "hasPermissions"
-initialized       = ohcP "initialized"
-isA               = ohcP "isA"
-memberOf          = ohcP "memberOf"
-openHcOrgPrefix   = prefix "openHcOrg" (iriRef "http://openhc.org/")
-readP             = "read"
-readPermission    = ohcP "readPermission"
-user              = ohcP "user"
-writeP            = "write"
-writePermission   = ohcP "writePermission"
-
-ohcP x = do { ohc <- openHcOrgPrefix ; return (ohc .:. (T.pack x)) }
+------------------------------------------------------------------------------
+-- CLI
 
 main :: IO ()
 main = do
@@ -58,84 +50,79 @@ newUserCLI uEmail = do
     r <- newUser uEmail
     putStrLn $ show r
 
-newUser uEmail = update u
-  where
-    u = do
-        ohc <- openHcOrgPrefix
-        id  <- ohcP $ emailAddressToId uEmail;    ia  <- isA;    u   <- user
-        u1  <- updateTriple id ia u
-        ema <- emailAddress
-        u2  <- updateTriple id ema (T.pack uEmail)
-        return UpdateQuery { queryUpdate = [u1,u2] }
-
 listUsersCLI = do
     r <- listUsers
     putStrLn $ show r
-
-listUsers = do query q
-  where
-    q = do
-        s <- var;    ia  <- isA;    u   <- user
-        triple s ia u
-        return SelectQuery { queryVars = [s] }
 
 newGroupCLI gName = do
     r <- newGroup gName
     putStrLn $ show r
 
-newGroup gName = update u
-  where
-    gnp = (T.pack gName)
-    u = do
-        ohc <- openHcOrgPrefix
-        id  <- ohcP gName;    ia  <- isA;    grp <- group
-        u1  <- updateTriple id ia grp
-        gn  <- groupName
-        u2  <- updateTriple id gn gnp
-        return UpdateQuery { queryUpdate = [u1,u2] }
-
 addUserToGroupCLI uEmail gName = do
     r <- addUserToGroup uEmail gName
     putStrLn $ show r
-
-addUserToGroup uEmail gName = update u
-  where
-    u = do
-        ohc <- openHcOrgPrefix
-        id  <- ohcP $ emailAddressToId uEmail;    mo  <- memberOf;    g  <- ohcP gName
-        u1  <- updateTriple id mo g
-        return UpdateQuery { queryUpdate = [u1] }
 
 addPermissionToGroupCLI gName permission resource = do
     r <- addPermissionToGroup gName permission resource
     putStrLn $ show r
 
-addPermissionToGroup gName permission resource = update u
-  where
-    perm = pToP permission
-    u = do
-        ohc <- openHcOrgPrefix
-        id  <- ohcP gName
-        p   <- perm
-        u1  <- updateTriple id p (T.pack resource)
-        return UpdateQuery { queryUpdate = [u1] }
-
 listGroupsCLI = do
     r <- listGroups
-    putStrLn $ show r
-
-listGroups = do query q
+    banner "group"
+    case r of
+        Just g  -> mapM_ format g
+        Nothing -> putStrLn "no groups"
   where
-    q = do
-        s <- var;    ia  <- isA;    g   <- group
-        triple s ia g
-        return SelectQuery { queryVars = [s] }
+    format (_:n:[]) = do
+        (\(Bound v) -> putStrLn $ show $ getValue $ v) n
+
+getValue (Data.RDF.Types.LNode (Data.RDF.Types.PlainL x)) = x
+
+banner x = do
+    putStrLn "--------------------------------------------------"
+    putStrLn x
+
+------------------------------------------------------------------------------
+-- persistent operations
+-- Note: if these were high-frequency/volume operations then would batch calls where possible
+
+newUser uEmail = do
+    update  id isA          user
+    updateL id emailAddress uEmail
+  where
+    id = ohcP $ emailAddressToId uEmail
+
+listUsers = do
+    queryVTT var isA user
+
+newGroup gName = do
+    update  id isA       group
+    updateL id groupName gName
+  where
+    id = ohcP gName
+
+addUserToGroup uEmail gName = do
+    update id memberOf (ohcP gName)
+  where
+    id = ohcP $ emailAddressToId uEmail
+
+addPermissionToGroup gName permission resource =
+    updateL id perm resource
+  where
+    id   = ohcP gName
+    perm = pToP permission
+
+listGroups = do
+    queryVTTVTV isA group groupName
+
+------------------------------------------------------------------------------
+-- DB initialization
 
 isDBAlreadyPopulated :: IO Bool
-isDBAlreadyPopulated = ask aq
+isDBAlreadyPopulated = sendAsk a
   where
-    aq :: Query AskQuery
-    aq = do
+    a :: Query AskQuery
+    a = do
         ohc <- openHcOrgPrefix
         i   <- initialized
         ask <- askTriple i i i
@@ -164,31 +151,59 @@ initializeDB = do
         addPermissionToGroup mathGroup writeP scheduling
         --     has read-only access to financial reports at https://reports.finance.projectdelta.com/
         addPermissionToGroup mathGroup readP  "https://reports.finance.projectdelta.com/"
-        update u
-        return ()
-    u = do
-        ohc <- openHcOrgPrefix
-        -- set the initialized marker
-        i   <- initialized
-        u1  <- updateTriple i i i
-        return UpdateQuery { queryUpdate = [u1] }
 
-mkDoUpdate s o p = do update u
+        -- set the initialized marker
+        update initialized initialized initialized
+        return ()
+
+------------------------------------------------------------------------------
+-- rdf utilities
+
+update s p o = do sendUpdate u
   where
     u = do
         ohc <- openHcOrgPrefix
-        s' <- s;    o' <- o;    p' <- p
-        u  <- updateTriple s' o' p'
+        s' <- s;    p' <- p;    o' <- o;
+        u  <- updateTriple s' p' o'
         return UpdateQuery { queryUpdate = [u] }
 
-update :: Query UpdateQuery -> IO Bool
-update u = do r <- updateQuery dbUpdateAddress u; return r
+updateL s p o = do sendUpdate u
+  where
+    u = do
+        ohc <- openHcOrgPrefix
+        s' <- s;    p' <- p
+        u  <- updateTriple s' p' (T.pack o)
+        return UpdateQuery { queryUpdate = [u] }
 
-query :: Query SelectQuery -> IO (Maybe [[BindingValue]])
-query  q = do r <- selectQuery dbQueryAddress  q; return r
+queryVTT s p o = do sendQuery q
+  where
+    q = do
+        ohc <- openHcOrgPrefix
+        s' <- s;    p' <- p;    o' <- o;
+        triple s' p' o'
+        return SelectQuery { queryVars = [s'] }
 
-ask :: Query AskQuery -> IO Bool
-ask    a = do r <- askQuery    dbQueryAddress  a; return r
+queryVTTVTV p1 o1 p2 = do sendQuery q
+  where
+    q = do
+        ohc <- openHcOrgPrefix
+        id  <- var;    p1' <- p1;    o1' <- o1
+        p2' <- p2;     o2' <- var
+        triple id p1' o1'
+        triple id p2' o2'
+        return SelectQuery { queryVars = [id,o2'] }
+
+sendUpdate :: Query UpdateQuery -> IO Bool
+sendUpdate u = do r <- updateQuery dbUpdateAddress u; return r
+
+sendQuery  :: Query SelectQuery -> IO (Maybe [[BindingValue]])
+sendQuery  q = do r <- selectQuery dbQueryAddress  q; return r
+
+sendAsk    :: Query AskQuery    -> IO Bool
+sendAsk    a = do r <- askQuery    dbQueryAddress  a; return r
+
+------------------------------------------------------------------------------
+-- misc utilities
 
 emailAddressToId uEmail = replace "@" "AT" uEmail
 
@@ -197,46 +212,63 @@ pToP x | x == readP  = readPermission
        | otherwise   = ohcP x -- TODO: restrict
 
 ------------------------------------------------------------------------------
+-- constants
+
+dbAddress         = "http://localhost:3030/ds/"
+dbQueryAddress    = dbAddress ++ "query"
+dbUpdateAddress   = dbAddress ++ "update"
+emailAddress      = ohcP "emailAddress"
+group             = ohcP "group"
+groupName         = ohcP "groupName"
+hasPermissions    = ohcP "hasPermissions"
+initialized       = ohcP "initialized"
+isA               = ohcP "isA"
+memberOf          = ohcP "memberOf"
+openHcOrgPrefix   = prefix "openHcOrg" (iriRef "http://openhc.org/")
+readP             = "read"
+readPermission    = ohcP "readPermission"
+user              = ohcP "user"
+writeP            = "write"
+writePermission   = ohcP "writePermission"
+
+ohcP x = do { ohc <- openHcOrgPrefix ; return (ohc .:. (T.pack x)) }
+
+------------------------------------------------------------------------------
 -- Experiments
 
-q :: Query SelectQuery
-q = do
-    s <- var
-    p <- var
-    o <- var
-    triple s p o
-    return SelectQuery { queryVars = [s, p, o] }
+qAll = do sendQuery q
+  where
+    q = do
+        s <- var; p <- var; o <- var
+        triple s p o
+        return SelectQuery { queryVars = [s, p, o] }
 
-q' :: Query SelectQuery
-q' = do
-    dc <- prefix "dc" (iriRef "http://purl.org/dc/elements/1.1/")
-    ex <- prefix "ex" (iriRef "http://example/")
-    x  <- var
+qBook1 = do sendQuery q
+  where
+    q = do
+        dc <- prefix "dc" (iriRef "http://purl.org/dc/elements/1.1/")
+        ex <- prefix "ex" (iriRef "http://example/")
+        x  <- var
+        triple (ex .:. "book1")  (dc .:. "title") (T.pack "A new book")
+        return SelectQuery { queryVars = [x] }
 
-    triple (ex .:. "book1")  (dc .:. "title") (T.pack "A new book")
-
-    return SelectQuery { queryVars = [x] }
-
-a :: Query AskQuery
-a = do
-    dc <- prefix "dc" (iriRef "http://purl.org/dc/elements/1.1/")
-    ex <- prefix "ex" (iriRef "http://example/")
-
-    ask <- askTriple (ex .:. "book1")  (dc .:. "title") (T.pack "A new book")
-
-    return AskQuery { queryAsk = [ask] }
+aBook1 = do sendAsk a
+  where
+    a = do
+        dc <- prefix "dc" (iriRef "http://purl.org/dc/elements/1.1/")
+        ex <- prefix "ex" (iriRef "http://example/")
+        ask <- askTriple (ex .:. "book1")  (dc .:. "title") (T.pack "A new book")
+        return AskQuery { queryAsk = [ask] }
 
 -- PREFIX dc: <http://purl.org/dc/elements/1.1/> INSERT DATA {  <http://example/book1> dc:title "A new book" ; dc:creator "A.N.Other" . }
-u :: Query UpdateQuery
-u = do
-    dc <- prefix "dc" (iriRef "http://purl.org/dc/elements/1.1/")
-    ex <- prefix "ex" (iriRef "http://example/")
-
-    ut1 <- updateTriple (ex .:. "book1")  (dc .:. "title") (T.pack "A HASKELL book")
-    ut2 <- updateTriple (ex .:. "book1")  (dc .:. "title") (T.pack "Another HASKELL book")
-    ut3 <- updateTriple (ex .:. "book1")  (dc .:. "title") (T.pack "Yet another HASKELL book")
-
-    return UpdateQuery { queryUpdate = [ut1,ut2,ut3] }
-
+uhb = do sendUpdate u
+  where
+    u = do
+        dc <- prefix "dc" (iriRef "http://purl.org/dc/elements/1.1/")
+        ex <- prefix "ex" (iriRef "http://example/")
+        ut1 <- updateTriple (ex .:. "book1")  (dc .:. "title") (T.pack "A HASKELL book")
+        ut2 <- updateTriple (ex .:. "book1")  (dc .:. "title") (T.pack "Another HASKELL book")
+        ut3 <- updateTriple (ex .:. "book1")  (dc .:. "title") (T.pack "Yet another HASKELL book")
+        return UpdateQuery { queryUpdate = [ut1,ut2,ut3] }
 
 -- End of file.
