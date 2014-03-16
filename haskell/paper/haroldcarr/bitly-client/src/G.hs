071ed7beb1b9db95d0f768adcbd5305f18bbe26d
Harold Carr <harold.carr@gmail.com>
2014-03-15 21:43:14 -0700

    Successfully finding/returning then traversing record declarations.

--- haskell/paper/haroldcarr/bitly-client/src/G.hs
+++ haskell/paper/haroldcarr/bitly-client/src/G.hs
@@ -1,43 +1,54 @@
 {-
 Created       : 2014 Mar 15 (Sat) 17:38:34 by Harold Carr.
-Last Modified : 2014 Mar 15 (Sat) 18:23:48 by Harold Carr.
+Last Modified : 2014 Mar 15 (Sat) 21:42:25 by Harold Carr.
 
 http://neilmitchell.blogspot.com/2013/10/haskell-type-graphs-with-uniplate-and.html
 -}
 
 import           Data.Generics.Uniplate.Data
-import           Data.List
 import           Language.Haskell.Exts
 import           System.Environment
 
 main :: IO ()
 main = do
-    [interest,filename] <- getArgs
-    doit interest filename
+    [filename] <- getArgs
+    doit filename
 
-doit :: String -> FilePath -> IO ()
-doit interest filename =
-    -- writeFile "graph.dot" . graph . interesting "Exp" . reach =<< getModule
-    writeFile "graph.dot" . graph . interesting interest . reach =<< (getModule filename)
+doit :: FilePath -> IO ()
+doit filename =
+    writeFile "JUNK.hs" . show . convertIt . reach =<< getModule filename
 
 getModule :: FilePath -> IO Module
 getModule filename = do
-    -- src <- readFile "../../haskell-src-exts/src/Language/Haskell/Exts/Syntax.hs"
     src <- readFile filename
-    return $ fromParseResult $ parseModule $ unlines $ filter (not . bad) $ lines src
-        where bad x = head (words x ++ [""]) `elem` words "#if #else #endif deriving #ifdef"
+    return $ fromParseResult $ parseModule src
 
-reach :: Module -> [(String, [String])]
-reach m = [ (prettyPrint name0, nub [prettyPrint x | TyCon x <- universeBi ctors])
+reach :: Module -> [(String, [(String, [(String, Type)])])]
+reach m = [ (prettyPrint name0, [ (prettyPrint rcName, [ (fName, typ)
+                                                       | ([Ident fName], UnBangedTy typ) <- y ])
+                                | RecDecl rcName y <- universeBi ctors])
           | DataDecl _ _ _ name0 _ ctors _ <- universeBi m]
 
-interesting :: String -> [(String, [String])] -> [(String, [String])]
-interesting target xs0 = [(a,b `intersect` keep) | (a,b) <- xs0, a `elem` keep]
-    where keep      = f [target] xs0
-          f want xs = if null new then want else f (map fst new ++ want) rest
-              where (new,rest) = partition (not . null . intersect want . snd) xs
+-- convertIt :: [(String, [(String, [(String, Type)])])] -> String
+-- convertIt xs = show xs
 
-graph :: [(String, [String])] -> String
-graph xs = unlines $ ["digraph g {"] ++ [from ++ " -> " ++ t ++ ";" | (from,to) <- xs, t <- to] ++ ["}"]
+--             Request   Expand    url     String
+convertIt :: [(String, [(String, [(String, Type)])])] -> [(String, [(String, [(String, Type)])])]
+convertIt (("Request", vc):xs) = ("Request", convertVc vc) : convertIt xs
+convertIt [] = []
+
+convertVc :: [(t, [(t1, Type)])] -> [(t, [(t1, Type)])]
+convertVc ((vcName, fields):xs) = (vcName, convertFields fields) : convertVc xs
+convertVc [] = []
+
+convertFields :: [(t, Type)] -> [(t, Type)]
+convertFields ((field, typ):xs) = (field, convertTyp typ) : convertFields xs
+convertFields [] = []
+
+convertTyp :: Type -> Type
+convertTyp                                 t@(TyList (TyCon (UnQual (Ident i)))) = t
+convertTyp                                         t@(TyCon (UnQual (Ident i)))  = t
+convertTyp t@(TyApp (TyCon (UnQual (Ident "Maybe"))) (TyCon (UnQual (Ident i)))) = t
+convertTyp t = error $ "Unexpected type: " ++ show t
 
 -- End of file.
