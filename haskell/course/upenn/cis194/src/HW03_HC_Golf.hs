1265b4c6016593671e779063d2c909e05ab6059d
Harold Carr <harold.carr@gmail.com>
2014-06-01 21:52:48 -0700

    HW03/E2 works.

--- haskell/course/upenn/cis194/src/HW03_HC_Golf.hs
+++ haskell/course/upenn/cis194/src/HW03_HC_Golf.hs
@@ -1,6 +1,6 @@
 {-
 Created       : 2014 Jun 01 (Sun) 19:54:26 by Harold Carr.
-Last Modified : 2014 Jun 01 (Sun) 21:31:12 by Harold Carr.
+Last Modified : 2014 Jun 01 (Sun) 21:50:15 by Harold Carr.
 -}
 
 module HW03_HC_Golf where
@@ -15,17 +15,17 @@ import qualified Test.HUnit.Util as U
 skips :: [a] -> [[a]]
 skips xs0 = map f (zip ((init . tails) xs0) [1 ..])
   where
-    f x = let (xs, n) = x
-              g i | i >= length xs = Nothing
-                  | otherwise      = Just (xs!!i, i+n)
-          in unfoldr g 0
+    f (xs, n) = unfoldr g 0
+      where
+        g i | i >= length xs = Nothing
+            | otherwise      = Just (xs!!i, i+n)
 
 e1 :: T.Test
 e1 = T.TestList
     [
       U.teq "skips0" (skips "ABCD")       ["ABCD", "BD", "C", "D"]
     , U.teq "skips1" (skips "hello!")     ["hello!", "el!", "l!", "l", "o", "!"]
-    , U.teq "skips2" (skips [1])          [[1]]
+    , U.teq "skips2" (skips [1::Int])     [[1]]
     , U.teq "skips3" (skips [True,False]) [[True,False], [False]]
 --  TODO: skips [] returns the correct value when typed into GHCi after loading this file,
 --        but need to figure out how to get it to accept the following input in test
@@ -33,8 +33,26 @@ e1 = T.TestList
     ]
 
 ------------------------------------------------------------------------------
+-- Exercise 2
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima l@(x1:x2:x3:_) | x1 < x2 && x2 > x3 = x2 : lmt
+                           | otherwise          =      lmt
+  where lmt = localMaxima (tail l)
+localMaxima _ = []
+
+e2 :: T.Test
+e2 = T.TestList
+    [
+      U.teq "lm0" (localMaxima [2,9,5,6,1]) [9,6]
+    , U.teq "lm1" (localMaxima [2,3,4,1,5]) [4]
+    , U.teq "lm2" (localMaxima [1,2,3,4,5]) []
+    ]
+
+------------------------------------------------------------------------------
 hw03 :: IO T.Counts
 hw03 = do
-    T.runTestTT e1
+    _ <- T.runTestTT e1
+    T.runTestTT e2
 
 -- End of file.
