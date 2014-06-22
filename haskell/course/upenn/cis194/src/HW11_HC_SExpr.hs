a6e48d25b205eb1fa2d293e873cf0aeb6951e940
Harold Carr <harold.carr@gmail.com>
2014-06-21 23:13:31 -0700

    Working on HW11 e3.  Stuck on recusion again.

--- haskell/course/upenn/cis194/src/HW11_HC_SExpr.hs
+++ haskell/course/upenn/cis194/src/HW11_HC_SExpr.hs
@@ -1,11 +1,11 @@
 {-
 Created       : 2014 Jun 19 (Thu) 17:22:43 by Harold Carr.
-Last Modified : 2014 Jun 21 (Sat) 22:16:08 by Harold Carr.
+Last Modified : 2014 Jun 21 (Sat) 22:59:21 by Harold Carr.
 -}
 
 module HW11_HC_SExpr where
 
-import           HW10_HC_AParser
+import           HW11_AParser
 
 import           Control.Applicative
 import           Data.Char           (isAlpha, isAlphaNum, isSpace, isUpper)
@@ -16,7 +16,7 @@ import qualified Test.HUnit.Util     as U
 ------------------------------------------------------------------------------
 -- Examples from lecture
 
--- data Employee = Emp { name :: String, phone :: String } deriving (Eq, Show)
+data Employee = Emp { name :: String, phone :: String } deriving (Eq, Show)
 
 -- nondeterministic arithmetic
 (.+), (.*) :: [Integer] -> [Integer] -> [Integer]
@@ -55,8 +55,8 @@ oneOrMore v = oneOrMore_v
     zeroOrMore_v = oneOrMore_v <|> pure []
     oneOrMore_v = (:) <$> v <*> zeroOrMore_v
 
-ex1' :: T.Test
-ex1' = T.TestList
+ex1 :: T.Test
+ex1 = T.TestList
     [
       U.teq "e10" (runParser (zeroOrMore (satisfy isUpper)) "ABCdEfgH") (Just ("ABC","dEfgH"))
     , U.teq "e11" (runParser (oneOrMore  (satisfy isUpper)) "ABCdEfgH") (Just ("ABC","dEfgH"))
@@ -74,8 +74,8 @@ spaces = zeroOrMore (satisfy isSpace)
 ident :: Parser String
 ident = (++) <$> oneOrMore (satisfy isAlpha) <*> zeroOrMore (satisfy isAlphaNum)
 
-ex2'' :: T.Test
-ex2'' = T.TestList
+ex2 :: T.Test
+ex2 = T.TestList
     [
       U.teq "e20" (runParser ident  "foobar baz") (Just ("foobar"," baz"))
     , U.teq "e21" (runParser ident  "foo33fA")    (Just ("foo33fA",""))
@@ -95,16 +95,34 @@ type Ident = String
 
 -- An "atom" is either an integer value or an identifier.
 data Atom = N Integer | I Ident
-  deriving Show
+  deriving (Eq, Show)
 
 -- An S-expression is either an atom, or a list of S-expressions.
 data SExpr = A Atom
            | Comb [SExpr]
-  deriving Show
+  deriving (Eq, Show)
 
-ex3' :: T.Test
-ex3' = T.TestList
+-- atom ::= int | ident
+atom :: Parser SExpr
+atom = A <$> (N <$> posInt <|> I <$> ident)
+
+-- S ::= atom | (S∗)
+s :: Parser SExpr
+s = atom <|> (satisfy (== '(') *> (Comb <$> oneOrMore s) <* satisfy (== ')'))
+
+parseSExpr :: Parser SExpr
+parseSExpr = s
+
+ex3 :: T.Test
+ex3 = T.TestList
     [
+      U.teq "e30" (runParser parseSExpr "5")                                        (Just (A (N 5),""))
+    , U.teq "e31" (runParser parseSExpr "foo3")                                     (Just (A (I "foo3"),""))
+    , U.teq "e32" (runParser parseSExpr "(foo)")                                    (Just (Comb [A (I "foo")],""))
+    , U.teq "e33" (runParser parseSExpr "(foo bar)")                                (Just (Comb [A (I "foo")],""))
+    , U.teq "e34" (runParser parseSExpr "(bar (foo) 3 5 874)")                      Nothing
+    , U.teq "e35" (runParser parseSExpr "(((lambda x (lambda y (plus x y))) 3) 5)") Nothing
+    , U.teq "e36" (runParser parseSExpr "( lots of ( spaces in ) this ( one ) )") Nothing
     ]
 
 ------------------------------------------------------------------------------
@@ -112,6 +130,6 @@ ex3' = T.TestList
 hw11 :: IO T.Counts
 hw11 = do
     T.runTestTT lec
-    T.runTestTT ex1'
-    T.runTestTT ex2''
-    T.runTestTT ex3'
+    T.runTestTT ex1
+    T.runTestTT ex2
+    T.runTestTT ex3
