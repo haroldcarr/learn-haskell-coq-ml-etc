b41464c3618b9e46f6540edcb083fc0c20bca190
Harold Carr <harold.carr@gmail.com>
2014-07-07 08:41:06 -0700

    A little cleanup.

--- haskell/course/2014-07-utrecht/2014-07-07-monday/MasterMind/MasterMind.hs
+++ haskell/course/2014-07-utrecht/2014-07-07-monday/MasterMind/MasterMind.hs
@@ -1,6 +1,6 @@
 {-
 Created       : by Andres Loh
-Last Modified : 2014 Jul 07 (Mon) 08:25:46 by Harold Carr.
+Last Modified : 2014 Jul 07 (Mon) 08:37:45 by Harold Carr.
 -}
 
 module Main where
@@ -12,12 +12,9 @@ type Row = [Int]
 type Guess = Row
 type Solution = Row
 
+colors, width :: Int
 colors = 6
 width  = 4
-
--- A function that indicates places on which you have to work:
-tODO :: a -> a
-tODO = id
 
 -- This is the complete main function. It just initializes the
 -- game by generating a solution and then calls the game loop.
@@ -29,6 +26,7 @@ main =
 
 -- The following function is given. It generates a random solution of the
 -- given width, and using the given number of colors.
+generateSolution :: IO [Int]
 generateSolution =
   do
     g <- getStdGen
@@ -49,15 +47,14 @@ loop s =
         then do { putStrLn "YES"; return () }
         else do loop s
 
-
 black, white :: Solution -> Guess -> Int
 black []           _         = 0
-black (s:solution) (g:guess) = huh s g + black solution guess
-  where
-    huh x y = if x == y then 1 else 0
+black (s:ss) (g:gs) = (if s == g then 1 else 0) + black ss gs
 
-white _ [] = 0
-white solution (g:guess) = (if g `elem` solution then 1 else 0) + white solution guess
+white solution0 guess0 = white' solution0 guess0
+  where
+    white' _ [] = 0
+    white' (s:ss) (g:gs) = (if g `elem` solution0 && g /= s then 1 else 0) + white' ss gs
 
 check :: Solution -> Guess -> (Int,   -- number of black points,
                                Int,   -- number of white points
@@ -94,6 +91,10 @@ readInt x =
   case reads x of
     [(n, "")] -> n
     _         -> -1
+
+-- A function that indicates places on which you have to work:
+tODO :: a -> a
+tODO = id
 
 -- The function valid tests a guess for validity. This is a bonus
 -- exercise, so you do not have to implement this function.
