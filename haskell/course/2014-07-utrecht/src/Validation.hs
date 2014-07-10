6c3790821f061e40f58bef25e84ba68232f7f69f
Harold Carr <harold.carr@gmail.com>
2014-07-10 00:50:54 -0700

    More "validation".

--- haskell/course/2014-07-utrecht/src/Validation.hs
+++ haskell/course/2014-07-utrecht/src/Validation.hs
@@ -1,6 +1,6 @@
 {-
 Created       : 2014 Jul 09 (Wed) 13:18:07 by Harold Carr.
-Last Modified : 2014 Jul 09 (Wed) 14:20:53 by Harold Carr.
+Last Modified : 2014 Jul 10 (Thu) 00:50:24 by Harold Carr.
 -}
 
 module Validation where
@@ -8,7 +8,7 @@ module Validation where
 -- This contains the "Validating Credit Card Numbers" functions (exact same thing in Brent Yorgey's course).
 import           HW01_HC
 
-import           Data.List       (intercalate)
+import           Data.List       (intercalate, isPrefixOf)
 import           Data.List.Split (chunksOf)
 
 import qualified Test.HUnit      as T
@@ -46,8 +46,12 @@ rawData = [ "34 15 American Express"
           , "417500 16 Visa Electron"
           ]
 
-lookupIssuer :: Integer -> String
-lookupIssuer = undefined
+-- lookupIssuer :: Integer -> String
+lookupIssuer cardNum = lu (show cardNum) rawData
+  where
+    lu _ []      = "Unknown"
+    lu cn (x:xs) = if check cn (words x) then x else lu cn xs
+    check cn (prefix:n:_) = isPrefixOf prefix cn && length cn == read n
 
 ------------------------------------------------------------------------------
 
@@ -56,6 +60,7 @@ t0 = T.TestList
     [
       U.teq "t000" (readCC "4012 8888 8888 1881") 4012888888881881
     , U.teq "t001" (showCC 4012888888881881) "4012 8888 8888 1881"
+    , U.teq "t002" (lookupIssuer (4012888888881881::Int)) "4 16 Visa"
     ]
 
 v :: IO T.Counts
