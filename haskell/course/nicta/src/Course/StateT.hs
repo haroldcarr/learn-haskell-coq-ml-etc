67e9441961169d6094b94b2ea99f52c48c2234a6
Harold Carr <harold.carr@gmail.com>
2014-07-15 12:07:25 -0700

    Updating StateT to latest.

--- haskell/course/nicta/src/Course/StateT.hs
+++ haskell/course/nicta/src/Course/StateT.hs
@@ -1,5 +1,12 @@
+{-
+Created       : by NICTA.
+Last Modified : 2014 Jul 15 (Tue) 12:04:55 by Harold Carr.
+-}
+
+{-# LANGUAGE InstanceSigs        #-}
 {-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RebindableSyntax    #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Course.StateT where
@@ -20,6 +27,11 @@ import qualified Prelude            as P
 import qualified Test.HUnit         as T
 import qualified Test.HUnit.Util    as U
 
+-- $setup
+-- >>> import Test.QuickCheck
+-- >>> import qualified Prelude as P(fmap)
+-- >>> instance Arbitrary a => Arbitrary (List a) where arbitrary = P.fmap listh arbitrary
+
 -- | A `StateT` is a function from a state value `s` to a functor f of (a produced value `a`, and a resulting state `s`).
 newtype StateT s f a =
   StateT {
@@ -33,8 +45,12 @@ newtype StateT s f a =
 -- >>> runStateT ((+1) <$> (pure 2) :: StateT Int List Int) 0
 -- [(3,0)]
 instance Functor f => Functor (StateT s f) where
-    f <$> StateT k = StateT (\ s -> (\ ~(a, s') -> (f a, s')) <$> (k s))
---C f <$> StateT k = StateT ((<$>) (first f) . k)
+  (<$>) ::
+    (a -> b)
+    -> StateT s f a
+    -> StateT s f b
+  f <$> StateT k = StateT ((<$>) (first f) . k)
+-- HC f <$> StateT k = StateT (\ s -> (\ ~(a, s') -> (f a, s')) <$> (k s))
 
 {-
 :t (pure 3) :: StateT a Optional Int
@@ -64,9 +80,13 @@ tstf = U.tt "tstf"
 -- >>> runStateT (StateT (\s -> Full ((+2), s P.++ [1])) <*> (StateT (\s -> Full (2, s P.++ [2])))) [0]
 -- Full (4,[0,1,2])
 instance Bind f => Apply (StateT s f) where
+  (<*>) ::
+    StateT s f (a -> b)
+    -> StateT s f a
+    -> StateT s f b
   StateT f <*> StateT a =
-      -- StateT (\s -> (\(g, t) -> (\(z, u) -> (g z, u)) <$> a t) =<< f s)
-      StateT ((\(g, t) -> first g <$> a t) <=< f)
+    -- StateT (\s -> (\(g, t) -> (\(z, u) -> (g z, u)) <$> a t) =<< f s)
+    StateT ((\(g, t) -> first g <$> a t) <=< f)
 
 -- | Implement the `Applicative` instance for @StateT s f@ given a @Applicative f@.
 --
@@ -76,6 +96,9 @@ instance Bind f => Apply (StateT s f) where
 -- >>> runStateT ((pure 2) :: StateT Int List Int) 0
 -- [(2,0)]
 instance Monad f => Applicative (StateT s f) where
+  pure ::
+    a
+    -> StateT s f a
   pure a = StateT (\s -> pure (a, s))
 
 -- | Implement the `Bind` instance for @StateT s f@ given a @Monad f@.
@@ -84,24 +107,32 @@ instance Monad f => Applicative (StateT s f) where
 -- >>> runStateT ((const $ putT 2) =<< putT 1) 0
 -- ((),2)
 instance Monad f => Bind (StateT s f) where
+  (=<<) ::
+    (a -> StateT s f b)
+    -> StateT s f a
+    -> StateT s f b
+  f =<< StateT k = StateT ((=<<) (\(a, t) -> runStateT (f a) t) . k)
+{-
   k =<< m = StateT (\s -> runStateT m s >>= \(a, s') ->
                           runStateT (k a) s')
-
+-}
 instance Monad f => Monad (StateT s f) where
 
 -- | A `State'` is `StateT` specialised to the `Id` functor.
 type State' s a =
   StateT s Id a
 
--- | Provide a constructor for `State'` values.
+-- | Provide a constructor for `State'` values
 --
 -- >>> runStateT (state' $ runState $ put 1) 0
 -- Id ((),1)
 state' ::
   (s -> (a, s))
   -> State' s a
+state' k = StateT (Id . k)
+{-
 state' f = StateT (\s -> Id (f s))
-
+-}
 -- | Provide an unwrapper for `State'` values.
 --
 -- >>> runState' (state' $ runState $ put 1) 0
@@ -110,8 +141,10 @@ runState' ::
   State' s a
   -> s
   -> (a, s)
+runState' (StateT k) = runId . k
+{-
 runState' st s = let (Id p) = runStateT st s in p
-
+-}
 -- | Run the `StateT` seeded with `s` and retrieve the resulting state.
 execT ::
   Functor f =>
@@ -125,8 +158,10 @@ exec' ::
   State' s a
   -> s
   -> s
+exec' t = runId . execT t
+{-
 exec' s' a = snd $ runState' s' a
-
+-}
 -- | Run the `StateT` seeded with `s` and retrieve the resulting value.
 evalT ::
   Functor f =>
@@ -140,8 +175,10 @@ eval' ::
   State' s a
   -> s
   -> a
+eval' t = runId . evalT t
+{-
 eval' s' a = fst $ runState' s' a
-
+-}
 -- | A `StateT` where the state also distributes into the produced value.
 --
 -- >>> (runStateT (getT :: StateT Int List Int) 3)
@@ -149,7 +186,7 @@ eval' s' a = fst $ runState' s' a
 getT ::
   Monad f =>
   StateT s f s
-getT   = StateT $ \s -> pure (s, s)
+getT = StateT (\s -> pure (s, s))
 
 -- | A `StateT` where the resulting state is seeded with the given value.
 --
@@ -162,11 +199,15 @@ putT ::
   Monad f =>
   s
   -> StateT s f ()
+putT = StateT . const . pure . (,) ()
+{-
 putT s = StateT $ \s -> pure ((), s)
-
+-}
 -- | Remove all duplicate elements in a `List`.
 --
 -- /Tip:/ Use `filtering` and `State'` with a @Data.Set#Set@.
+--
+-- prop> distinct' xs == distinct' (flatMap (\x -> x :. x :. Nil) xs)
 distinct' ::
   (Ord a, Num a) =>
   List a
@@ -241,6 +282,9 @@ instance Apply (Logger l) where
   (<*>) = error "TODO/Apply/Logger"
 
 -- | Implement the `Applicative` instance for `Logger`.
+--
+-- >>> pure "table" :: Logger Int P.String
+-- Logger [] "table"
 instance Applicative (Logger l) where
   pure = error "TODO/Applicative/Logger"
 
@@ -258,7 +302,10 @@ instance Monad (Logger l) where
 --
 -- >>> log1 1 2
 -- Logger [1] 2
-log1 :: l -> a -> Logger l a
+log1 ::
+  l
+  -> a
+  -> Logger l a
 log1 = error "TODO/log1"
 
 -- | Remove all duplicate integers from a list. Produce a log as you go.
