43367ed107757670419bbbafbda3423b0f59215c
Harold Carr <harold.carr@gmail.com>
2014-12-03 17:39:38 -0800

    edx fp101x lab5 (finished implementation - not tested yet)

--- haskell/course/edx-delft-fp101x-intro-to-fp-erik-meijer/lab5-pmc.hs
+++ haskell/course/edx-delft-fp101x-intro-to-fp-erik-meijer/lab5-pmc.hs
@@ -1,7 +1,5 @@
 module Lab5 where
 
-import           Control.Monad
-
 data Concurrent a = Concurrent ((a -> Action) -> Action)
 
 data Action
@@ -46,23 +44,22 @@ atom  ioa = Concurrent (\c -> Atom (ioa >>= \a -> return $ c a))
 -- ===================================
 
 fork :: Concurrent a -> Concurrent ()
-fork (Concurrent f) = Concurrent (\_ -> Fork (f (\_ -> Stop)) Stop)
+fork f = Concurrent (\c -> Fork (action f) (c ()))
 
 par :: Concurrent a -> Concurrent a -> Concurrent a
-par (Concurrent f1) (Concurrent f2) = Concurrent (\a -> Fork (f1 a) (f2 a))
+par (Concurrent f1) (Concurrent f2) = Concurrent (\c -> Fork (f1 c) (f2 c))
 
 -- ===================================
 -- Ex. 4
 -- ===================================
 
-cb ::       ((a -> Action) -> Action) ->
+cb ::       ((a -> Action) -> Action)  ->
       (a -> ((b -> Action) -> Action)) ->
             ((b -> Action) -> Action)
-cb ma f = let m = ma (\_ -> Stop)
-          in \c -> m
+cb m f = \c -> m (\x -> (f x) c)
 
 instance Monad Concurrent where
-    (Concurrent f) >>= g = undefined
+    (Concurrent m) >>= f = Concurrent $ \c -> m (\x -> let (Concurrent fx) = f x in fx c)
     return x = Concurrent (\c -> c x)
 
 -- ===================================
@@ -70,7 +67,12 @@ instance Monad Concurrent where
 -- ===================================
 
 roundRobin :: [Action] -> IO ()
-roundRobin = error "You have to implement roundRobin"
+roundRobin xs0 =
+    case xs0 of
+        [] -> return ()
+        ((Atom ioa)   : xs) -> ioa >>= \x -> roundRobin (xs++[x])
+        ((Fork a1 a2) : xs) -> roundRobin (a1:a2:xs)
+        (Stop         : xs) -> roundRobin xs
 
 -- ===================================
 -- Tests
@@ -93,9 +95,9 @@ run :: Concurrent a -> IO ()
 run x = roundRobin [action x]
 
 genRandom :: Int -> [Int]
-genRandom 1337 = [1, 96, 36, 11, 42, 47, 9, 1, 62, 73]
-genRandom 7331 = [17, 73, 92, 36, 22, 72, 19, 35, 6, 74]
-genRandom 2600 = [83, 98, 35, 84, 44, 61, 54, 35, 83, 9]
+genRandom 1337 = [ 1, 96, 36, 11, 42, 47,  9,  1, 62, 73]
+genRandom 7331 = [17, 73, 92, 36, 22, 72, 19, 35,  6, 74]
+genRandom 2600 = [83, 98, 35, 84, 44, 61, 54, 35, 83,  9]
 genRandom 42   = [71, 71, 17, 14, 16, 91, 18, 71, 58, 75]
 
 loop :: [Int] -> Concurrent ()
