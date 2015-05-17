dcc8a4cf57a5360b96d5099748947e15342b9d94
Harold Carr <harold.carr@gmail.com>
2015-05-17 16:46:45 -0700

    Working on LambdaConf presentation.

--- haskell/book/Program_Design_by_Calculation-Oliveira/2015-LambdaConf/SumAbsorption.hs
+++ haskell/book/Program_Design_by_Calculation-Oliveira/2015-LambdaConf/SumAbsorption.hs
@@ -9,5 +9,9 @@ sumAbsorptionLeft  g h i j = (Either.either g h) . (Sum.sum i j)
 
 sumAbsorptionRight g h i j = Either.either (g . i) (h . j)
 
+sal,sar :: String
+sal = sumAbsorptionLeft  (++" g::d->c ") (++ " h:e->c ") (++" i::a->d ") (++" j::b->e ")   (Left  "foo")
+sar = sumAbsorptionRight (++" g::d->c ") (++ " h:e->c ") (++" i::a->d ") (++" j::b->e ")   (Right "foo")
+
 -- End
 
--- haskell/book/Program_Design_by_Calculation-Oliveira/2015-LambdaConf/SumFunctor.hs
+++ haskell/book/Program_Design_by_Calculation-Oliveira/2015-LambdaConf/SumFunctor.hs
@@ -6,4 +6,8 @@ sumFunctorLeft, sumFunctorRight :: (e -> c) -> (a -> e) -> (f -> d) -> (b -> f) 
 sumFunctorLeft  g h i j = Sum.sum (g . h) (i . j)
 sumFunctorRight g h i j = (Sum.sum g i) . (Sum.sum h j)
 
+el,er :: Either String String
+el = sumFunctorLeft  (++" g::e->c ") (++" h::a->e ") (++" i::f->d ") (++" j::b->f ")    (Left  "foo")
+er = sumFunctorRight (++" g::e->c ") (++" h::a->e ") (++" i::f->d ") (++" j::b->f ")    (Right "foo")
+
 -- End
