70b0fc0dfbe4d4fcbdf6b0833c1278360f0ce005
Harold Carr <harold.carr@gmail.com>
2015-05-19 17:23:18 -0700

    Working on LambdaConf 2015 presentation.

--- haskell/book/Program_Design_by_Calculation-Oliveira/2015-LambdaConf/Const.hs
+++ haskell/book/Program_Design_by_Calculation-Oliveira/2015-LambdaConf/Const.hs
@@ -3,4 +3,21 @@ module Const where
 length' :: [a] -> Int
 length' = foldl (const . succ) 0
 
+foldll f z []     = z
+foldll f z (x:xs) = foldl f (f z x) xs
+
+equationalReasoning :: [Int]
+equationalReasoning =
+    [
+      length' ['a','b']
+    , foldl (const . succ)                 0      ['a','b']
+    , foldl (const . succ) ((const . succ) 0 'a')     ['b']
+    , foldl (const . succ)                 1          ['b']
+    , foldl (const . succ) ((const . succ) 1 'b')        []
+    , foldl (const . succ)                 2             []
+    ,                                      2
+    ]
+
+r = all (==2) equationalReasoning
+
 -- End
--- haskell/book/Program_Design_by_Calculation-Oliveira/2015-LambdaConf/Identity.hs
+++ haskell/book/Program_Design_by_Calculation-Oliveira/2015-LambdaConf/Identity.hs
@@ -12,4 +12,22 @@ flm0 = flatMap (\x -> [x*2]) [1,2,3]
 
 flm1 = flatMap id [[1],[2],[3]]
 
+foldrr f z []     = z
+foldrr f z (x:xs) = f x (foldr f z xs)
+
+equationalReasoning :: [[Integer]]
+equationalReasoning =
+    [
+      flatMap    id                                                      [[1],[2],[3]]
+    ,                                 foldr (\x acc -> id x   ++ acc) [] [[1],[2],[3]]
+    , (\x acc -> id x   ++ acc) [1] $ foldr (\x acc -> id x   ++ acc) []     [[2],[3]]
+    ,            id [1] ++            foldr (\x acc -> id x   ++ acc) []     [[2],[3]]
+    ,               [1] ++            foldr (\x acc -> id x   ++ acc) []     [[2],[3]]
+    ,               [1] ++                             id [2] ++ foldr (\x acc -> id x   ++ acc) []  [[3]]
+    ,               [1] ++                             id [2] ++                  id [3] ++ foldr (\x acc -> id x ++ acc) [] []
+    ,               [1] ++                             id [2] ++                  id [3] ++      []
+    ]
+
+r = all (==[1,2,3]) equationalReasoning
+
 -- End
