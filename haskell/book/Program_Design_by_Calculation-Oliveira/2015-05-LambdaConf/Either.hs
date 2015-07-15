baa6fb8c67a72fdd5338ac488cf160296cf42116
Harold Carr <harold.carr@gmail.com>
2015-07-14 22:11:58 -0700

    Version presented at Lambda Lounge Utah 2015-07-14.

--- haskell/book/Program_Design_by_Calculation-Oliveira/2015-05-LambdaConf/Either.hs
+++ haskell/book/Program_Design_by_Calculation-Oliveira/2015-05-LambdaConf/Either.hs
@@ -7,10 +7,10 @@ either f _ (Left  a) = f a
 either _ g (Right b) = g b
 
 e0,e1,e2,e3 :: Integer
-e0 = ((*11)     `Either.either`     (+1))   (Left   9)
-e1 = ((*11)     `Either.either`     (+1))   (Right 98)
-e2 = (arr (*11)  |||            arr (+1))   (Left   9)
-e3 = (arr (*11)  |||            arr (+1))   (Right 98)
+e0 = ((*11)     `Either.either`     (+1))   (Left  9)
+e1 = ((*11)     `Either.either`     (+1))   (Right 1)
+e2 = (arr (*11)  |||            arr (+1))   (Left  9)
+e3 = (arr (*11)  |||            arr (+1))   (Right 1)
 
 -- => 99
 
--- haskell/book/Program_Design_by_Calculation-Oliveira/2015-05-LambdaConf/ProductFunctor.hs
+++ haskell/book/Program_Design_by_Calculation-Oliveira/2015-05-LambdaConf/ProductFunctor.hs
@@ -10,7 +10,6 @@ productFunctorRight g h i j = Product.product g i . Product.product h j
 
 e0,e1 :: (Int, Double)
 e0 = productFunctorLeft  (+2) (+4) (+6.0) (+8.0)       (1,100.0)
-
 e1 = productFunctorRight (+2) (+4) (+6.0) (+8.0)       (1,100.0)
 
 -- (7,114.0)
