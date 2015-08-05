e8b5a96724e08d62b5f9b80cdb30f69b9feea43e
Harold Carr <harold.carr@gmail.com>
2015-08-04 18:48:17 -0700

    PLT-REDEX

--- foundations/book/Semantic_Engineering_with_PLT_REDEX/summer-2015/07-hc-lab-type-checking-e6.rkt
+++ foundations/book/Semantic_Engineering_with_PLT_REDEX/summer-2015/07-hc-lab-type-checking-e6.rkt
@@ -7,63 +7,7 @@
 ;; 7 Lab Type Checking
 ;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Exercise 6.
-;; Develop a reduction system for which the trace expression from the
-;; lecture preserves types
-#|
-(module+ test
-  (traces ->
-          (term (((lambda ((x (int -> int))) x) (lambda ((x int)) x)) 1))
-          #:pred (lambda (e)
-                   (judgment-holds (⊢ () ,e int)))))
-|#
-
-(define lambda-tc? (redex-match? TLambda-tc e))
-
-(define e1
-  (term (lambda ((x int) (f (int -> int))) (+ (f (f x)) (f x)))))
-(define e2
-  (term (lambda ((x int) (f ((int -> int) -> int))) (f x))))
-(define e3
-  (term (lambda ((x int) (x (int -> int))) x)))
-
-(module+ test
-  (test-equal (judgment-holds (⊢ () ,e1 (int (int -> int) -> int))) #true)
-  (test-equal (judgment-holds (⊢ () ,e2 t)) #false)
-  (displayln  (judgment-holds (⊢ () ,e1 t) t))
-  (displayln  (judgment-holds (⊢ () ,e2 t) t)))
-
-(define-extended-language Standard-tc TLambda-tc
-  (v ::= n + (lambda ((x t) ...) e))
-  (E ::=
-     hole
-     (v ... E e ...)))
-
-(define s->βv-tc
-  (reduction-relation
-   Standard-tc
-   (--> (in-hole E ((lambda ((x_1 t_1) ..._n) e) v_1 ..._n))
-        (in-hole E (subst ((v_1 x_1) ...) e))
-        β)
-   (--> (in-hole E (+ n_1 n_2))
-        (in-hole E ,(+ (term n_1) (term n_2)))
-        +)
-   ))
-
-(define-metafunction Standard-tc
-  eval-value-tc : e -> v or closure
-  [(eval-value-tc e) any_1 (where any_1 (run-value-tc e))])
-
-(define-metafunction Standard-tc
-  run-value-tc : e -> v or closure
-  [(run-value-tc n) n]
-  [(run-value-tc v) closure]
-  [(run-value-tc e)
-   (run-value-tc e_again)
-   ; (v) means that we expect s->βv to be a function 
-   (where (e_again) ,(apply-reduction-relation s->βv-tc (term e)))])
-
+;; Given
 
 (define-judgment-form TLambda-tc
   #:mode (⊢ I I O)
@@ -86,10 +30,52 @@
    ------------------------------------------------- "application"
    (⊢ Γ (e_1 e_2 ...) t)])
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 6.
+;; Develop a reduction system for which the trace expression from the
+;; lecture preserves types
+
+(module+ test
+  (traces s->vβ-tc
+          (term (((lambda ((f (int -> int))) f)
+                  (lambda ((i int)) i))
+                 1))
+          #:pred (lambda (e) (judgment-holds (⊢ () ,e int)))))
+
+(define-extended-language Standard-tc TLambda-tc
+  (v ::= n + (lambda ((x t) ...) e))
+  (E ::=
+     hole
+     (v ... E e ...)))
+
+(define s->vβ-tc
+  (reduction-relation
+   Standard-tc
+   (--> (in-hole E ((lambda ((x_1 t_1) ..._n) e) v_1 ..._n))
+        (in-hole E (subst ((v_1 x_1) ...) e))
+        β)
+   (--> (in-hole E (+ n_1 n_2))
+        (in-hole E ,(+ (term n_1) (term n_2)))
+        +)
+   ))
+
+(define-metafunction Standard-tc
+  eval-value-tc : e -> v or closure
+  [(eval-value-tc e) any_1 (where any_1 (run-value-tc e))])
+
+(define-metafunction Standard-tc
+  run-value-tc : e -> v or closure
+  [(run-value-tc n) n]
+  [(run-value-tc v) closure]
+  [(run-value-tc e)
+   (run-value-tc e_again)
+   ; (v) means that we expect s->βv to be a function
+   (where (e_again) ,(apply-reduction-relation s->vβ-tc (term e)))])
+
 ; this now works
 #;
 (module+ test
-  (traces s->βv-tc
+  (traces s->vβ-tc
           (term (((lambda ((x (int -> int))) x) (lambda ((x int)) x)) 1))
           #:pred (lambda (e)
                    (judgment-holds (⊢ () ,e int)))))
@@ -97,7 +83,7 @@
 ; this works too
 #;
 (module+ test
-  (traces s->βv-tc
+  (traces s->vβ-tc
           (term (+ 1 (+ (+ 2 3) (+ 4 5))))
           #:pred (lambda (e)
                    (judgment-holds (⊢ () ,e int)))))
