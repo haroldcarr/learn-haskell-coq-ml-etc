f041354c7d94c1f72729ab835ea99d023a4820bd
Harold Carr <harold.carr@gmail.com>
2016-02-27 16:14:31 -0800

    calculate truth table for any propositional formula

--- haskell/book/Handbook_of_Practical_Logic_and_Automated_Reasoning/src/X_2_2_Prop_Semantics.hs
+++ haskell/book/Handbook_of_Practical_Logic_and_Automated_Reasoning/src/X_2_2_Prop_Semantics.hs
@@ -14,14 +14,14 @@ import           X_2_1_Prop_Syntax
 eval fm v = case fm of
     F          -> False
     T          -> True
-    (Atom x)   -> fromJust (Map.lookup x v)
+    (Atom x)   -> v x
     (Not  p)   -> not (eval p v)
     (And  p q) ->      eval p v  && eval q v
     (Or   p q) ->      eval p v  || eval q v
     (Impl p q) -> not (eval p v) || eval q v
     (Iff  p q) ->      eval p v  == eval q v
 
-te v fs = let pq = Map.fromList v
+te v fs = let pq x = fromJust (Map.lookup x (Map.fromList v))
           in fmap ((`eval` pq) . pr) fs
 
 tev1 = U.tt "tev1"
@@ -43,13 +43,64 @@ tev4 = U.tt "tev4"
        (te [("p",True),("q",True)]
            [ "(p ^ q)", "p v q", "p -> q", "p <-> q"])
        True
+{-
+Theorem 2.1 For any prop formula p, the set atoms(p) is finite.
+Proof By induction on structure of p.
 
+- If p is T or F, then atoms(p) is the empty set: finite.
+- If p is an atom, atoms(p) is a singleton set: finite.
+- If p is of form ~q, then by the induction hypothesis, atoms(q) is finite
+  and by definition atoms(¬q) = atoms(q).
+- If p is of the form q ∧ r, q ∨ r, q ⇒ r or q ⇔ r, then atoms(p) = atoms(q) ∪ atoms(r).
+  By inductive hypothesis, both atoms(q) and atoms(r) are finite
+  and the union of two finite sets is finite. QED
+
+Theorem 2.2 For any prop formula p, if two valuations υ and υ' agree on atoms(p)
+(i.e. υ(x) = υ'(x) for all x in atoms(p)), then eval p υ = eval p υ'.
+Proof By induction on structure of p.
+- If p is of T or F, then it is independent of the valuation.
+- If p is atom x, then atoms(x) = {x} and by assumption υ(x) = υ'(x).
+  Hence eval p υ = υ(x) = υ'(x) = eval p υ'.
+- If p is q ∧ r, q ∨ r, q ⇒ r or q ⇔ r, then atoms(p) = atoms(q) ∪ atoms(r).
+  Since valuations agree on the union of the two sets, they agree, a fortiori, on each of atoms(q)
+  and atoms(r).
+  We can therefore apply the inductive hypothesis to conclude
+  that eval q υ = eval q υ' and
+  that eval r υ = eval r υ'.
+  Since evaluation of p is a function of these subevaluations, eval p υ = eval p υ'. QED
+-}
 atoms :: (Eq a, Ord a, Foldable t) => t a -> [a]
 atoms = sort . nub . foldr (:) []
 
 ta1 = U.t "ta1"
       (atoms (pr "p ^ q v s -> ~p v (r <-> s)"))
       ["p","q","r","s"]
+
+onAllValuations f v as = case as of
+    []     -> f v
+    (p:ps) -> let v' t q = if q == p then t else v q
+              in onAllValuations f (v' False) ps ++
+                 onAllValuations f (v' True ) ps
+
+truthtable fm =
+    let ats        = atoms fm
+        toString p = if p then "T" else "F"
+        mkRow v    =
+            let lis = fmap (toString . v) ats
+                ans = toString (eval fm v)
+                in [(lis,ans)]
+    in onAllValuations mkRow (const False) ats
+
+ttt1 = U.t "ttt1"
+       (truthtable (pr "p ^ q -> q ^ r"))
+       [ (["F","F","F"],"T")
+       , (["F","F","T"],"T")
+       , (["F","T","F"],"T")
+       , (["F","T","T"],"T")
+       , (["T","F","F"],"T")
+       , (["T","F","T"],"T")
+       , (["T","T","F"],"F")
+       , (["T","T","T"],"T") ]
 
 ------------------------------------------------------------------------------
 -- test
@@ -58,6 +109,7 @@ test :: IO U.Counts
 test =
     U.runTestTT $ U.TestList $
     tev1 ++ tev2 ++ tev3 ++ tev4 ++
-    ta1
+    ta1 ++
+    ttt1
 
 -- end of file ---
