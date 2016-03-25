8eb0397bf0e0ec9a9755c9a11c3123db7e8bb6d7
Harold Carr <harold.carr@gmail.com>
2016-03-25 06:50:47 -0700

    Searching a Aeson represented Swagger graph.

--- haskell/playpen/aeson-playground/src/Search.hs
+++ haskell/playpen/aeson-playground/src/Search.hs
@@ -1,35 +1,52 @@
+{-# LANGUAGE MultiWayIf        #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Search where
 
-import           Control.Applicative
-import           Control.Monad       (liftM, (>=>))
+import           Control.Monad       ((>=>))
 import           Data.Aeson
-import           Data.Aeson.Types
 import           Data.ByteString     as BS
 import qualified Data.HashMap.Strict as HM
 import           Data.Text           as T
+import           Prelude             as P
 import           System.IO           (IOMode (ReadMode), withFile)
 
 readSwagger :: FilePath -> IO (Maybe Value)
 readSwagger filename =
     withFile filename ReadMode (BS.hGetContents >=> return . decodeStrict)
 
-{-
 --                      path   ref
-findRefs :: Value -> [([Text],[Text])]
+findRefs :: Value -> [([Text], (Text, Value))]
 findRefs v = findRefs' v [] []
 
---                    path      result
-findRefs' :: Value -> [Text] -> [([Text],[Text])] -> [([Text],[Text])]
-findRefs' (Object o) p r = let kvs = HM.toList o
-                           in if (fst (head kvs)) == "$ref"
-                                 then [(p, (head kvs))]
-findRefs'         _  _ r = r
--}
+--                    path      result-so-far                result
+findRefs' :: Value -> [Text] -> [([Text], (Text, Value))] -> [([Text], (Text, Value))]
+findRefs' (Object o) path result =
+    let kvs = HM.toList o
+    in if P.null kvs
+           then result
+           else let (hk,_) = P.head kvs
+                in if hk == "$ref"
+                       then (P.reverse path, P.head kvs) : result
+                       else P.concatMap (\(k,v) -> (findRefs' v (k:path) result)) kvs
+findRefs' _ _ result = result
 
-{-
-s <- readSwagger "/Users/carr/.sync/.ksync/rpt/0-catalog/validation-playground/swagger-validation/resources/examples/swagger20/simple-ref.json"
-decode s :: Maybe Value
+swagger :: T.Text
+swagger =
+    "{\"swagger\": \"2.0\",\
+      \\"paths\": {\
+         \\"/pets\": {\
+             \\"get\": {\
+                 \\"tags\"       : [\"pet\"],\
+                 \\"summary\"    : \"list the pets\",\
+                 \\"operationId\": \"getPets\",\
+                 \\"responses\"  : { \"default\": { \"description\": \"Generic Error\" },\
+                                     \\"200\"    : { \"description\": \"Pets list\",\
+                                                    \\"schema\": { \"type\": \"array\",\
+                                                                  \\"items\": {\"$ref\":\"#/definitions/Pet\"}}}}}}},\
+      \\"definitions\": {\
+         \\"Parent\": { \"$ref\": \"#/definitions/DoesNotExist\" },\
+         \\"Pet\"   : { \"properties\": { \"tags\": {  \"type\" : \"array\",\
+                                                      \\"items\": {\"$ref\": \"#/definitions/Tag\"}}}},\
+         \\"Tag\"   : { \"properties\": { \"n ame\": { \"type\": \"string\" }}}}}"
 
--}
