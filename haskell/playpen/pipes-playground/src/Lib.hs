08d767835d8756c404a5de3d8f06f4d2f9ddfdd1
Harold Carr <harold.carr@gmail.com>
2016-04-11 20:24:32 -0700

    pipes

--- haskell/playpen/pipes-playground/src/Lib.hs
+++ haskell/playpen/pipes-playground/src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Rank2Types        #-}
 
@@ -6,14 +7,9 @@ module Lib where
 import           Control.Monad.IO.Class
 import           Data.ByteString        as B
 import           Data.ByteString.Char8  as BC (putStrLn)
-import           Data.ByteString.Lazy   as BL
+import           Data.String            (IsString)
 import           Pipes
 import           Pipes.Network.TCP
-import           Pipes.Prelude          (stdoutLn)
-import           Prelude                as P
-
-someFunc :: IO ()
-someFunc = P.putStrLn "someFunc"
 
 connect :: MonadIO m => m (Socket, SockAddr)
 connect = connectSock "127.0.0.1" "3000"
@@ -24,16 +20,25 @@ close = closeSock
 mkP :: MonadIO m => Socket -> Producer' B.ByteString m ()
 mkP s = fromSocket s 4096
 
+mkC :: MonadIO m => Socket -> Consumer' B.ByteString m r
+mkC = toSocket
+
 doRecv p = for p $ \b -> lift $ BC.putStrLn b
 
 doit = do
     c <- Lib.connect
     let s = fst c
-        p = mkP s
-    send s "foo\n"
-    runEffect $ doRecv p
+--     sendIt s
+    recIt s
     close s
-    return ()
+
+sendIt soc = do
+    let c = mkC soc
+    runEffect $ "foo\n" >~ c
+
+recIt soc = do
+    let p = mkP soc
+    runEffect $ doRecv p
 
 {-
 c <- connect
