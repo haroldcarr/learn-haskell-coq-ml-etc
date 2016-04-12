e1be3bc1b4259611aae92765b94ae3f523a689a1
Harold Carr <harold.carr@gmail.com>
2016-04-12 08:16:38 -0700

    pipes example working (with type signatures)

--- haskell/playpen/pipes-playground/src/Lib.hs
+++ haskell/playpen/pipes-playground/src/Lib.hs
@@ -7,9 +7,10 @@ module Lib where
 import           Control.Monad.IO.Class
 import           Data.ByteString        as B
 import           Data.ByteString.Char8  as BC (putStrLn)
-import           Data.String            (IsString)
 import           Pipes
+import qualified Pipes.ByteString       as PB
 import           Pipes.Network.TCP
+import           Pipes.Prelude          as PP (take)
 
 connect :: MonadIO m => m (Socket, SockAddr)
 connect = connectSock "127.0.0.1" "3000"
@@ -23,22 +24,27 @@ mkP s = fromSocket s 4096
 mkC :: MonadIO m => Socket -> Consumer' B.ByteString m r
 mkC = toSocket
 
-doRecv p = for p $ \b -> lift $ BC.putStrLn b
-
+doit :: IO ()
 doit = do
     c <- Lib.connect
     let s = fst c
---     sendIt s
+    sendIt s
     recIt s
     close s
 
+sendIt :: MonadIO m => Socket -> m ()
 sendIt soc = do
     let c = mkC soc
-    runEffect $ "foo\n" >~ c
+    runEffect $ PB.stdin >-> PP.take 1 >-> c
 
+recIt :: Socket -> IO ()
 recIt soc = do
     let p = mkP soc
     runEffect $ doRecv p
+
+-- doRecv :: Proxy x' x () ByteString IO a' -> Proxy x' x c' c IO a'
+doRecv :: Producer' B.ByteString IO () -> Effect IO ()
+doRecv p = for p $ \b -> lift $ BC.putStrLn b
 
 {-
 c <- connect
