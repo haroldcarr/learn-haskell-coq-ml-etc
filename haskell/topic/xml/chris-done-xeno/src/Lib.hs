ffa06b5da3028372d70c4d87a04932aa39a11fc7
Harold Carr <harold.carr@gmail.com>
2017-01-21 13:56:53 -0800

    learning done's xeno

---
+++ haskell/topic/xml/chris-done-xeno/src/Lib.hs
@@ -0,0 +1,117 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+
+module Lib where
+
+import           Control.Monad           (when)
+import           Data.ByteString         as BS
+import           Data.HashMap.Strict     as M
+import           Data.String.Conversions
+import           Prelude                 as P
+import           Text.RawString.QQ
+import           Xeno.SAX
+
+x :: ByteString
+x = "Text<tag prop='value'>Hello, World!</tag><x><y prop=\"x\">Content!</y></x>Trailing."
+
+f1 = fold const           (\m _ _ -> m + 1) const const const 0 x -- Count elements.
+
+f2 = fold (\m _ -> m + 1) (\m _ _ -> m)     const const const 0 x -- Count attributes.
+
+p = process
+  (\x   -> P.putStrLn $ "open tag: "       <> cs' x)
+  (\x y -> P.putStrLn $ "tag attributed: " <> cs' x <> " " <> cs' y)
+  (\x   -> P.putStrLn $ "end open tag: "   <> cs' x)
+  (\x   -> P.putStrLn $ "text: "           <> cs' x)
+  (\x   -> P.putStrLn $ "close tag: "      <> cs' x)
+
+p1 = p x
+
+po = p opa
+
+lfo = let (Right (_,_,_,kvs)) = fo in P.length kvs
+
+fo =
+  fold
+    openTag
+    attributeKeyValue
+    endTag
+    textValue
+    closeTag
+    (False, False, "", M.empty)
+    opa
+ where
+  openTag           state tag  =
+    case (state, tag) of
+      ((False, setText, _, kvs), "read") -> (True,   setText, tag, kvs)
+      ((True,  setText, _, kvs), tag)    -> (True,   setText, tag, kvs)
+      ((isRead,setText, _, kvs), tag)    -> (isRead, setText, tag, kvs)
+  attributeKeyValue state _ _ = state
+  endTag  state@(isRead, _, startTag, kvs) tag =
+    if (startTag /= tag)
+      then error ("bad: startTag: " <> cs' startTag <> " " <> "endTag: " <> cs' tag)
+      else (isRead, True, tag, kvs)
+  textValue         state val =
+    case state of
+      (True, True, tag, kvs) -> (True, True, tag, M.insert tag val kvs)
+      _                      -> state
+  closeTag          state tag  =
+    case (state, tag) of
+      ((isRead, setText, _, kvs), "read") -> (False,  False, tag, kvs)
+      ((isRead, _,       _, kvs), _)      -> (isRead, False, tag, kvs)
+
+cs' = convertString
+
+opa :: BS.ByteString
+opa =
+  [r|<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<data>
+	<userid>1.0</userid>
+	<caseid>11.0</caseid>
+	<read>
+		<global0:loan_amount>300000.0</global0:loan_amount>
+		<global0:customer_name>Chris</global0:customer_name>
+		<global0:customer_resident>true</global0:customer_resident>
+		<global0:income_source0:income_amount>1500.0</global0:income_source0:income_amount>
+		<global0:loan_asset_value>500000.0</global0:loan_asset_value>
+		<global0:customer_previous_job>Professional</global0:customer_previous_job>
+		<global0:asset0:asset_value>15000.0</global0:asset0:asset_value>
+		<global0:asset0:asset>Car</global0:asset0:asset>
+		<global0:income_source0:income_source>Salary</global0:income_source0:income_source>
+		<global0:customer_employed>true</global0:customer_employed>
+		<global0:customer_age>32.0</global0:customer_age>
+		<global0:customer_previous_job_start_date>2000-01-01</global0:customer_previous_job_start_date>
+	</read>
+	<write>
+		<global0:customer_minimum_eligibility>true</global0:customer_minimum_eligibility>
+		<global0:customer_total_assets>15000.0</global0:customer_total_assets>
+		<global0:maximum_loan_amount>500000.0</global0:maximum_loan_amount>
+		<global0:customer_supplied_sufficient_supporting_documentation>true</global0:customer_supplied_sufficient_supporting_documentation>
+		<global0:customer_weekly_income>1500.0</global0:customer_weekly_income>
+		<global0:loan_value_ratio>60.0</global0:loan_value_ratio>
+		<global0:loan_within_thresholds>true</global0:loan_within_thresholds>
+		<global0:customer_eligible_loan>true</global0:customer_eligible_loan>
+		<global0:minimum_loan_amount>5000.0</global0:minimum_loan_amount>
+		<global0:maximum_loan_value_ratio>95.0</global0:maximum_loan_value_ratio>
+		<global0:customer_offer_sufficient_security>true</global0:customer_offer_sufficient_security>
+		<global0:customer_passes_employment_tests>true</global0:customer_passes_employment_tests>
+		<global0:customer_meets_loan_serviceability_tests>true</global0:customer_meets_loan_serviceability_tests>
+		<global0:weekly_loan_repayment_amount>300.0</global0:weekly_loan_repayment_amount>
+		<global0:customer_passes_credit_history_tests_applicable>true</global0:customer_passes_credit_history_tests_applicable>
+		<global0:minimum_age>18.0</global0:minimum_age>
+		<global0:credit_history_test_waived>true</global0:credit_history_test_waived>
+		<global0:customer_earnings_sufficient>true</global0:customer_earnings_sufficient>
+		<global0:residency_tests_waived>false</global0:residency_tests_waived>
+	</write>
+</data>|]
+
+vor = validate oparead
+
+oparead :: BS.ByteString
+oparead =
+  [r|<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <data>
+      <read>
+        <g:loan_amount>300000.0</g:loan_amount>
+     </read>
+    </data>|]
