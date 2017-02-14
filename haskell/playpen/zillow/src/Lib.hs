275a65dcc0e7a8afd502ce6ee48fd38bff632208
Harold Carr <harold.carr@gmail.com>
2017-02-13 19:36:56 -0800

    zillow

--- haskell/playpen/zillow/src/Lib.hs
+++ haskell/playpen/zillow/src/Lib.hs
@@ -10,23 +10,17 @@ import           Text.HTML.TagSoup
 
 -- curl http://www.zillow.com/homes/for_sale/84103_rb/?fromHomePage=true&shouldFireSellPageImplicitClaimGA=false&fromHomePageTab=buy
 
-pa = do
-  tags <- fileParseArticleTags "./test/p1.html"
-  printTags (P.map cleanse (P.map (pickData . pickTags) tags))
+------------------------------------------------------------------------------
+-- page links
 
-cleanse [] = []
-cleanse (x:xs) = if T.isPrefixOf "/homedetails/" x
-                 then x : cleanse xs
-                 else if T.isPrefixOf "/homedetail/AuthRequired.htm" x ||
-                         T.isPrefixOf "/" x ||
-                         P.elem x zillowIgnore
-                      then     cleanse xs
-                      else x : cleanse xs
+pp = ppp "./test/p1.html" pageLinks
 
-pl = do
-  tags <- fileParsePageLinks "./test/p1.html"
-  printTags (L.nub (pickOutPageLinks (P.takeWhile (\x -> x /= TagClose "ol")
-                                                  (P.head tags))))
+pageLinks txt =
+  L.nub (pickOutPageLinks (P.takeWhile (\x -> x /= TagClose "ol")
+                                       (P.head (getPageLinks txt))))
+
+getPageLinks :: Text -> [[Tag Text]]
+getPageLinks t = textParseTags "<ol class=zsg-pagination>" t
 
 pickOutPageLinks [] = []
 pickOutPageLinks (x:xs) =
@@ -35,30 +29,26 @@ pickOutPageLinks (x:xs) =
                                  -> (snd $ P.head attributes) : pickOutPageLinks xs
     _                            ->                             pickOutPageLinks xs
 
-printTags tags = do
-  mapM_ print tags
+------------------------------------------------------------------------------
+-- listings (via <article>) in page
 
-fileParseArticleTags :: String -> IO [[Tag Text]]
-fileParseArticleTags filename = do
-  d  <- T.readFile filename
-  return $ getArticles d
+pl = ppp "./test/p1.html" listings
 
-fileParsePageLinks :: String -> IO [[Tag Text]]
-fileParsePageLinks filename = do
-  d  <- T.readFile filename
-  return $ getPageLinks d
+listings txt =
+  P.map cleanse (P.map (pickData . pickTags) (getArticles txt))
 
 getArticles :: Text -> [[Tag Text]]
 getArticles t = textParseTags "<article>" t
 
-getPageLinks :: Text -> [[Tag Text]]
--- getPageLinks t = textParseTags "<div id=search-pagination-wrapper class=zsg-content-item>" t
-getPageLinks t = textParseTags "<ol class=zsg-pagination>" t
-
-textParseTags :: String -> Text -> [[Tag Text]]
-textParseTags tag t = do
-  let ts = parseTags t
-  partitions (~== tag) ts
+cleanse [] = []
+cleanse (x:xs) =
+  if T.isPrefixOf "/homedetails/" x
+  then      x : cleanse xs
+  else if T.isPrefixOf "/homedetail/AuthRequired.htm" x ||
+          T.isPrefixOf "/" x ||
+          P.elem x zillowIgnore
+       then     cleanse xs
+       else x : cleanse xs
 
 pickTags :: [Tag Text] -> [Tag Text]
 pickTags tags = do
@@ -84,22 +74,18 @@ pickData tags = do
 
 zillowIgnore = ["option","zsg-lightbox-show za-track-event","http://www.zillow.com/local-info/","http://www.facebook.com/Zillow","http://twitter.com/zillow","http://plus.google.com/+Zillow","zsg-notification-bar-close","mapped-result-count","#","#","#","#","#","#","menu-label","#fore-tip-filters","#coming-soon-tip-filters","#pm-tip-filters","#pmf-tip-filters","#pre-foreclosure-tip-filters","#mmm-tip-filters","#pending-tip-filters","price-menu-label","saf-entry-link","#payment","#income","#","saf-close zsg-button","saf-pre-approval-link","beds-menu-label","type-menu-label","menu-label","#hoa-dues-tooltip","http://www.zillow.com/community-pillar/","zsg-button_primary"]
 
-{-
+------------------------------------------------------------------------------
+-- util
 
-<div id="search-pagination-wrapper" class="zsg-content-item"><ol class="zsg-pagination"><li class="zsg-pagination_active"><a>1</a></li><li><a href="/homes/for_sale/84103_rb/2_p/" onclick="SearchMain.changePage(2);return false;">2</a></li><li><a href="/homes/for_sale/84103_rb/3_p/" onclick="SearchMain.changePage(3);return false;">3</a></li><li class="zsg-pagination-next"><a href="/homes/for_sale/84103_rb/2_p/" class="off" onclick="SearchMain.changePage(2);return false;">Next</a></li></ol></div></div>
+printTags tags = do
+  mapM_ print tags
 
-<div id="search-pagination-wrapper" class="zsg-content-item">
-  <ol class="zsg-pagination">
-    <li class="zsg-pagination_active"><a>1</a></li>
-    <li><a href="/homes/for_sale/84103_rb/2_p/" onclick="SearchMain.changePage(2);return false;">2</a></li>
-    <li><a href="/homes/for_sale/84103_rb/3_p/" onclick="SearchMain.changePage(3);return false;">3</a></li>
-    <li class="zsg-pagination-next">
-        <a href="/homes/for_sale/84103_rb/2_p/" class="off" onclick="SearchMain.changePage(2);return false;">Next</a></li>
-  </ol>
-</div>
+textParseTags :: String -> Text -> [[Tag Text]]
+textParseTags tag t = do
+  let ts = parseTags t
+  partitions (~== tag) ts
 
-    [TagText "141 E Edgecombe Dr",TagText "$624,999",TagOpen "a" [("href","/homedetails/141-E-Edgecombe-Dr-Salt-Lake-City-UT-84103/12717491_zpid/"),("class","zsg-photo-card-overlay-link routable hdp-link routable mask hdp-link")],TagOpen "a" [("href","/myzillow/UpdateFavorites.htm?zpid=12717491&operation=add&ajax=false"),("rel","nofollow"),("data-fm-zpid","12717491"),("data-fm-callback","windowReloadSuccessHandler"),("data-after-auth-action-type","Event"),("data-after-auth-global-event","favoriteManager:addFavoriteProperty"),("data-target-id","register"),("title","Save this home"),("data-show-home-owner-lightbox","false"),("data-za-label","Save Map:List"),("data-address","141 E Edgecombe Dr , Salt Lake City, UT 84103"),("class","zsg-lightbox-show open-on-any-click"),("id","register_opener_0")]]
+ppp filename f = do
+  txt <- T.readFile filename
+  printTags (f txt)
 
-
-TagOpen "a" [("href","/homedetails/141-E-Edgecombe-Dr-Salt-Lake-City-UT-84103/12717491_zpid/"),("class","zsg-photo-card-overlay-link routable hdp-link routable mask hdp-link")]
--}
