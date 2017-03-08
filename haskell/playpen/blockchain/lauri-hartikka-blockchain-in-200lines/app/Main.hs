d0d5a29bd585556011449cf02b3981e53af85a06
Harold Carr <harold.carr@gmail.com>
2017-03-07 20:36:03 -0800

    in transition towards multi-follower demo

--- haskell/playpen/blockchain/lauri-hartikka-blockchain-in-200lines/app/Main.hs
+++ haskell/playpen/blockchain/lauri-hartikka-blockchain-in-200lines/app/Main.hs
@@ -8,13 +8,32 @@ import           Control.Concurrent    (forkIO)
 import           Control.Monad         (forever)
 import           Data.ByteString       (ByteString)
 import           Data.ByteString.Char8 as BSC8 (pack)
+import           Data.List             (partition)
+import           System.Environment    (getArgs)
 
 host = "0.0.0.0"
 port = 9160
 
 main :: IO ()
 main = do
-  httpToConsensus <- newEmptyMVar
-  forkIO $ forever (runServer host port)
-  forkIO $ forever (runClient httpToConsensus host port)
-  site httpToConsensus
+  xs <- getArgs
+  case xs of
+    [] -> doIt [([port], [port])]
+    xs -> doIt (ss ((map read xs) :: [Int]))
+
+doIt lfs =
+  mapM_ (\([leader], followers) -> doIt' leader followers) lfs
+ where
+  doIt' leader peers = do
+    httpToConsensus <- newEmptyMVar
+    forkIO $ forever (runServer host leader)
+    forkIO $ forever (runClient httpToConsensus host leader)
+    site httpToConsensus
+
+ss :: Eq a => [a] -> [([a], [a])]
+ss xs =  map (\x -> partition ((==) x) xs) xs
+
+{-
+CMD --> L --> F1
+          --> F2
+-}
