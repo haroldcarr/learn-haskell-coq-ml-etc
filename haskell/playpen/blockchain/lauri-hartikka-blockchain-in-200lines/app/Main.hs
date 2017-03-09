88fdd9d36bc6cd72a33a587203aa4b2c453cd468
Harold Carr <harold.carr@gmail.com>
2017-03-09 15:35:26 -0800

    working on initiators and target peers

--- haskell/playpen/blockchain/lauri-hartikka-blockchain-in-200lines/app/Main.hs
+++ haskell/playpen/blockchain/lauri-hartikka-blockchain-in-200lines/app/Main.hs
@@ -10,8 +10,11 @@ import           Control.Monad         (forever)
 import           Data.ByteString       (ByteString)
 import           Data.ByteString.Char8 as BSC8 (pack)
 import           Data.List             (partition)
+import           Data.Monoid           ((<>))
 import           System.Environment    (getArgs)
 import           System.Log.Logger
+
+mainProgram = "main"
 
 host = "0.0.0.0"
 port = 9160
@@ -29,28 +32,37 @@ main = do
     xs -> do
       if not (even (length xs))
         then error "Usage"
-        else doIt (ss (sss xs))
+        else doIt (mkPartition (mkHostPortPairs xs))
 
-doIt lfs =
+doIt :: (Foldable t, Show a) => t ([(a, Int)], [(a, Int)]) -> IO ()
+doIt lfs = do
+  updateGlobalLogger mainProgram       (setLevel INFO)
+  updateGlobalLogger consensusFollower (setLevel INFO)
+  updateGlobalLogger consensusLeader   (setLevel INFO)
   mapM_ (\([leader], followers) -> doIt' leader followers) lfs
  where
-  doIt' (_,leader) _ = do
-    updateGlobalLogger consensusFollower (setLevel INFO)
-    updateGlobalLogger consensusLeader   (setLevel INFO)
+  doIt' (s,leader) fs = do
+    infoM mainProgram ("doIt': " <> show s <> " " <> show leader <> " " <> show fs)
     httpToConsensus <- newEmptyMVar
     forkIO $ forever (runFollower host leader)
     forkIO $ forever (runLeader httpToConsensus host leader)
     site httpToConsensus
 
-ss :: Eq a => [a] -> [([a], [a])]
-ss xs =  map (\x -> partition ((==) x) xs) xs
+-- splits the list so
+-- fst contains the initiator host/port
+-- snd contains target host/ports
+mkPartition :: Eq a => [a] -> [([a], [a])]
+mkPartition xs =  map (\x -> partition ((==) x) xs) xs
 
+-- assumes list is [ host, port, ... ]
 -- assumes even check done in advance
-sss []       = []
-sss (h:p:xs) = (h, read p::Int) : sss xs
+mkHostPortPairs []       = []
+mkHostPortPairs (h:p:xs) = (h, read p::Int) : mkHostPortPairs xs
 
 {-
 sss ["0.0.0.0", "9061", "0.0.0.0", "9062", "0.0.0.0", "9063"]
+
+ss [("0.0.0.0",9061),("0.0.0.0",9062),("0.0.0.0",9063)]
 
 
 CMD --> L --> F1
