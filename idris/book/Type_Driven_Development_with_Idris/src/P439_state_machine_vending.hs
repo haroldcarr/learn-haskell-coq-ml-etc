8efaf54a777ceeb9dd6199c2f6036a49913f33a9
Harold Carr <harold.carr@gmail.com>
2017-04-05 15:19:21 -0700

    turn runtime value into type

--- idris/book/Type_Driven_Development_with_Idris/src/P439_state_machine_vending.hs
+++ idris/book/Type_Driven_Development_with_Idris/src/P439_state_machine_vending.hs
@@ -53,40 +53,50 @@ vend :: MachineCmd () (Z, S Z) (Z, Z)
   InsertCoin `Bind` Vend
 -}
 
-data MachineCmd' :: * -> Nat -> Nat -> Nat -> Nat -> * where
-     InsertCoin' :: MachineCmd' ()    pounds     chocs  (S pounds) chocs
-     Vend'       :: MachineCmd' () (S pounds) (S chocs)    pounds  chocs
-     GetCoins'   :: MachineCmd' ()    pounds     chocs     Z       chocs
-     Refill      :: SNat bars
-                 -> MachineCmd' ()    Z          chocs     Z       (bars :+ chocs)
-     Bind'       :: MachineCmd' ()    s1p        s1c       s2p     s2c
-                 -> MachineCmd' ()    s2p        s2c       s3p     s3c
-                 -> MachineCmd' ()    s1p        s1c       s3p     s3c
+data MachineCmd ::            * ->  Nat ->     Nat -> Nat ->     Nat    -> * where
+     InsertCoin :: MachineCmd ()    pounds     chocs  (S pounds) chocs
+     Vend       :: MachineCmd () (S pounds) (S chocs)    pounds  chocs
+     GetCoins   :: MachineCmd ()    pounds     chocs     Z       chocs
+     Refill     :: SNat bars
+                -> MachineCmd ()    Z          chocs     Z       (bars :+ chocs)
+     Bind       :: MachineCmd ()    s1p        s1c       s2p     s2c
+                -> MachineCmd ()    s2p        s2c       s3p     s3c
+                -> MachineCmd ()    s1p        s1c       s3p     s3c
 
-deriving instance Show (MachineCmd' any poundsBefore chocsBefore poundsAfter chocsAfter)
+deriving instance Show (MachineCmd any poundsBefore chocsBefore poundsAfter chocsAfter)
 
-vend0 :: MachineCmd' () pounds chocs ('S pounds) chocs
-vend0  = InsertCoin'
+vend0 :: MachineCmd () pounds chocs ('S pounds) chocs
+vend0  = InsertCoin
 
-vend1 :: MachineCmd' () s1p s3c ('S ('S s1p)) s3c
-vend1  = InsertCoin' `Bind'` InsertCoin'
+vend1 :: MachineCmd () s1p s3c ('S ('S s1p)) s3c
+vend1  = InsertCoin `Bind` InsertCoin
 
-vend2 :: MachineCmd' () ('S pounds) ('S chocs) pounds chocs
-vend2  = Vend'
+vend2 :: MachineCmd () ('S pounds) ('S chocs) pounds chocs
+vend2  = Vend
 
-vend2' = vend0 `Bind'` vend2
+vend2':: MachineCmd () s3p ('S s3c) s3p s3c
+vend2' = vend0 `Bind` vend2
 
-vend3 :: MachineCmd' () ('S ('S s3p)) ('S ('S s3c)) s3p s3c
-vend3  = Vend' `Bind'` Vend'
+vend3 :: MachineCmd () ('S ('S s3p)) ('S ('S s3c)) s3p s3c
+vend3  = Vend `Bind` Vend
 
-vend4 :: MachineCmd' () 'Z chocs 'Z ('S ('S chocs))
+vend4 :: MachineCmd () 'Z chocs 'Z ('S ('S chocs))
 vend4  = Refill (SS (SS SZ))
 
-vend5 :: MachineCmd' () 'Z ('S 'Z) 'Z 'Z
-vend5  = InsertCoin' `Bind'` Vend'
+vend5 :: MachineCmd () 'Z ('S 'Z) 'Z 'Z
+vend5  = InsertCoin `Bind` Vend
 
-vend6 :: MachineCmd' () 'Z 'Z 'Z ('S 'Z)
-vend6  = Refill (SS (SS SZ)) `Bind'` InsertCoin' `Bind'` Vend'
+vend6 :: MachineCmd () 'Z 'Z 'Z ('S 'Z)
+vend6  = Refill (SS (SS SZ)) `Bind` InsertCoin `Bind` Vend
 
-vend7 :: MachineCmd' () 'Z chocs 'Z ('S ('S chocs))
+vend7 :: MachineCmd () 'Z chocs 'Z ('S ('S chocs))
 vend7  = reify (SS (SS SZ)) $ \p -> Refill (reflect p)
+
+vend8 :: SNat bars -> MachineCmd () 'Z chocs 'Z (bars :+ chocs)
+vend8 n = reify n $ \p -> Refill (reflect p)
+
+vend9 :: (bars :+ s1c) ~ 'S s3c
+      => SNat bars
+      -> MachineCmd () 'Z s1c 'Z s3c
+vend9 n = reify n $ \p -> Refill (reflect p) `Bind` InsertCoin `Bind` Vend
+
