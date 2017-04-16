633a459187e535d7827a777bbf6914f8c13778c0
Harold Carr <harold.carr@gmail.com>
2017-04-16 11:21:54 -0700

    misc

---
+++ haskell/playpen/misc/src/Misc.hs
@@ -0,0 +1,16 @@
+data ExprL a
+  = Const a
+  | Add [ExprL a]
+  | Mul [ExprL a]
+  deriving (Eq, Show)
+
+e = [Add [Const 3, Mul [Const 4, Const 5]]]
+
+i :: Num a => [ExprL a] -> [a]
+i [] = []
+i [x] = case x of
+  Const i -> [i]
+  Add axs -> let [[x],[y]] = map (\z -> i [z]) axs in [x+y]
+  Mul mxs -> let [[x],[y]] = map (\z -> i [z]) mxs in [x*y]
+i _ = error "NO"
+
