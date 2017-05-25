dcf63db73833e52f60e84a28d453d4a33128b848
Harold Carr <harold.carr@gmail.com>
2017-05-25 14:31:19 -0700

    misc

--- racket/emacs-key-bindings.rkt
+++ racket/emacs-key-bindings.rkt
@@ -1,20 +1,14 @@
 #lang s-exp framework/keybinding-lang
 
-; emacs style keybindings to evaluate program fragments:
-; http://docs.racket-lang.org/drracket/Keyboard_Shortcuts.html?q=keybindings#%28part._.Sending_.Program_.Fragments_to_the_.R.E.P.L%29
-
-; install keybindings:
-; http://docs.racket-lang.org/drracket/Keyboard_Shortcuts.html?q=keybindings#%28part._defining-shortcuts%29
- 
 (require drracket/tool-lib)
- 
+
 (module test racket/base)
- 
-(keybinding "c:c;c:e" (lambda (ed evt) (send-toplevel-form ed #f)))
-(keybinding "c:c;c:r" (lambda (ed evt) (send-selection ed #f)))
+
+(keybinding "c:c;c:e"    (lambda (ed evt) (send-toplevel-form ed #f)))
+(keybinding "c:c;c:r"    (lambda (ed evt) (send-selection ed #f)))
 (keybinding "c:c;~c:m:e" (lambda (ed evt) (send-toplevel-form ed #t)))
 (keybinding "c:c;~c:m:r" (lambda (ed evt) (send-selection ed #t)))
- 
+
 (define/contract (send-toplevel-form defs shift-focus?)
   (-> any/c boolean? any)
   (when (is-a? defs drracket:unit:definitions-text<%>)
@@ -25,11 +19,11 @@
          ;; we are inside some top-level expression;
          ;; find the enclosing expression
          (let loop ([pos sp])
-           (define next-up (send defs find-up-sexp pos)) 
+           (define next-up (send defs find-up-sexp pos))
            (cond
              [next-up (loop next-up)]
              [else
-              (send-range-to-repl defs 
+              (send-range-to-repl defs
                                   pos
                                   (send defs get-forward-sexp pos)
                                   shift-focus?)]))]
@@ -38,31 +32,31 @@
          (define fw (send defs get-forward-sexp sp))
          (define bw (send defs get-backward-sexp sp))
          (cond
-           [(and (not fw) (not bw)) 
+           [(and (not fw) (not bw))
             ;; no expressions in the file, give up
             (void)]
-           [(not fw) 
+           [(not fw)
             ;; no expression after the insertion point;
             ;; send the one before it
-            (send-range-to-repl defs 
+            (send-range-to-repl defs
                                 bw
                                 (send defs get-forward-sexp bw)
                                 shift-focus?)]
-           [else 
+           [else
             ;; send the expression after the insertion point
-            (send-range-to-repl defs 
+            (send-range-to-repl defs
                                 (send defs get-backward-sexp fw)
                                 fw
                                 shift-focus?)])]))))
-              
+
 (define/contract (send-selection defs shift-focus?)
   (-> any/c boolean? any)
   (when (is-a? defs drracket:unit:definitions-text<%>)
     (send-range-to-repl defs
-                        (send defs get-start-position) 
-                        (send defs get-end-position) 
+                        (send defs get-start-position)
+                        (send defs get-end-position)
                         shift-focus?)))
- 
+
 (define/contract (send-range-to-repl defs start end shift-focus?)
   (->i ([defs (is-a?/c drracket:unit:definitions-text<%>)]
         [start exact-positive-integer?]
@@ -73,11 +67,11 @@
     (define ints (send (send defs get-tab) get-ints))
     (define frame (send (send defs get-tab) get-frame))
     ;; copy the expression over to the interactions window
-    (send defs move/copy-to-edit 
+    (send defs move/copy-to-edit
           ints start end
           (send ints last-position)
           #:try-to-move? #f)
-    
+
     ;; erase any trailing whitespace
     (let loop ()
       (define last-pos (- (send ints last-position) 1))
@@ -86,14 +80,14 @@
         (when (char-whitespace? last-char)
           (send ints delete last-pos (+ last-pos 1))
           (loop))))
-    
+
     ;; put back a single newline
     (send ints insert
           "\n"
           (send ints last-position)
           (send ints last-position))
-    
-    ;; make sure the interactions is visible 
+
+    ;; make sure the interactions is visible
     ;; and run the submitted expression
     (send frame ensure-rep-shown ints)
     (when shift-focus? (send (send ints get-canvas) focus))
