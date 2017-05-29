e6a72434878053036b28e5cbff9499b50c526cfd
Harold Carr <harold.carr@gmail.com>
2017-05-29 11:35:25 -0700

    cleanup

--- haskell/topic/monads-lens/m/src/Writer.hs
+++ haskell/topic/monads-lens/m/src/Writer.hs
@@ -3,16 +3,17 @@
 module Writer where
 
 import Control.Monad.Writer
+import Prelude hiding (log)
 
-logNumber :: (MonadWriter [String] m, Show a) => a -> m a
-logNumber x = writer (x, ["number: " ++ show x])
+log :: (MonadWriter [String] m, Show a) => a -> m a
+log x = writer (x, ["number: " ++ show x])
 
-multWithLog :: Writer [String] Int
-multWithLog = do
-  _ <- logNumber 1
-  a <- logNumber 3
-  b <- logNumber 5
-  _ <- logNumber 9
+useLog :: Writer [String] Int
+useLog = do
+  _ <- log 1
+  a <- log 3
+  b <- log 5
+  _ <- log 9
   return (a*b)
 
--- runWriter multWithLog
+-- runWriter useLog
