89dedb987ffa844a2c11c7070aa120d1466d5bd3
Harold Carr <harold.carr@gmail.com>
2017-07-10 14:06:31 -0700

    lambda auth

--- racket/LambdaAuth.rkt
+++ racket/LambdaAuth.rkt
@@ -49,7 +49,7 @@
   ;; values
   (test-equal (λα-e? (term unit)) #t)
   (test-equal (λα-e? (term z)) #t)
-  (test-equal (λα-e? (term (lambda (w z) z))) #t)
+  (test-equal (λα-e? (term (lambda (w) z))) #t)
   (test-equal (λα-e? (term (inj1 unit))) #t)
   (test-equal (λα-e? (term (inj2 z))) #t)
   (test-equal (λα-e? (term (unit unit))) #t)
@@ -60,116 +60,24 @@
   (test-equal (λα-e? (term (case unit (lambda (w) w) (lambda (z) z)))) #t)
   )
 
-;; =============================================================================
-;; utilities
+(define-language nats
+  (n ::= z (s n)))
 
-;; -----------------------------------------------------------------------------
-;; (in x x_1 ...) is x a member of (x_1 ...)?
+(define-judgment-form nats
+  #:mode (sum I I O)
+  #:contract (sum n n n)
 
-(module+ test
-  (test-equal (term (in x (y z x y z))) #true)
-  (test-equal (term (in x ())) #false)
-  (test-equal (term (in x (y z w))) #false))
+  [---------- "zero"
+   (sum z n n)]
 
-(define-metafunction λα
-  in : x (x ...) -> boolean
-  [(in x (x_1 ... x x_2 ...)) #true]
-  [(in x (x_1 ...)) #false])
-
-;; -----------------------------------------------------------------------------
-;; (=α e_1 e_2) determines whether e_1 and e_2 are α equivalent
+  [(sum n_1 n_2 n_3)
+   ----------------- "add1"
+   (sum (s n_1) n_2 (s n_3))])
 
 (module+ test
-  (test-equal (term (=α (lambda (x) x) (lambda (y) y))) #true)
-  (test-equal (term (=α (lambda (x) (x 1)) (lambda (y) (y 1)))) #true)
-  (test-equal (term (=α (lambda (x) x) (lambda (y) z))) #false))
-
-(define-metafunction λα
-  =α : any any -> boolean
-  [(=α any_1 any_2) ,(equal? (term (sd any_1)) (term (sd any_2)))])
-
-(define (=α/racket x y) (term (=α ,x ,y)))
-
-;; (sd e) computes the static distance version of e
-(define-extended-language SD λα
-  (e ::= .... (K n))
-  (n ::= natural))
-
-(define SD? (redex-match? SD e))
-
-(module+ test
-  (define sd1 (term (K 1)))
-  (define sd2 (term 1))
-
-  (test-equal (SD? sd1) #true))
-
-(define-metafunction SD
-  sd : any -> any
-  [(sd any_1) (sd/a any_1 ())])
-
-(module+ test
-  (test-equal (term (sd/a x ())) (term x))
-  (test-equal (term (sd/a x ((y) (z) (x)))) (term (K 2 0)))
-  (test-equal (term (sd/a ((lambda (x) x) (lambda (y) y)) ()))
-              (term ((lambda () (K 0 0)) (lambda () (K 0 0)))))
-  (test-equal (term (sd/a (lambda (x) (x (lambda (y) y))) ()))
-              (term (lambda () ((K 0 0) (lambda () (K 0 0))))))
-  (test-equal (term (sd/a (lambda (z x) (x (lambda (y) z))) ()))
-              (term (lambda () ((K 0 1) (lambda () (K 1 0)))))))
-
-(define-metafunction SD
-  sd/a : any ((x ...) ...) -> any
-  [(sd/a x ((x_1 ...) ... (x_0 ... x x_2 ...) (x_3 ...) ...))
-   ;; bound variable
-   (K n_rib n_pos)
-   (where n_rib ,(length (term ((x_1 ...) ...))))
-   (where n_pos ,(length (term (x_0 ...))))
-   (where #false (in x (x_1 ... ...)))]
-  [(sd/a (lambda (x ...) any_1) (any_rest ...))
-   (lambda () (sd/a any_1 ((x ...) any_rest ...)))]
-  [(sd/a (any_fun any_arg ...) (any_rib ...))
-   ((sd/a any_fun (any_rib ...)) (sd/a any_arg (any_rib ...)) ...)]
-  [(sd/a any_1 any)
-   ;; free variable, constant, etc
-   any_1])
-
-;; -----------------------------------------------------------------------------
-;; (subst ([e x] ...) e_*) substitutes e ... for x ... in e_* (hygienically)
-
-(module+ test
-  (test-equal (term (subst ([1 x][2 y]) x)) 1)
-  (test-equal (term (subst ([1 x][2 y]) y)) 2)
-  (test-equal (term (subst ([1 x][2 y]) z)) (term z))
-  (test-equal (term (subst ([1 x][2 y]) (lambda (z w) (x y))))
-              (term (lambda (z w) (1 2))))
-  (test-equal (term (subst ([1 x][2 y]) (lambda (z w) (lambda (x) (x y)))))
-              (term (lambda (z w) (lambda (x) (x 2))))
-              #:equiv =α/racket)
-  (test-equal (term (subst ((2 x)) ((lambda (x) (1 x)) x)))
-              (term ((lambda (x) (1 x)) 2))
-              #:equiv =α/racket))
-
-(define-metafunction λα
-  subst : ((any x) ...) any -> any
-  [(subst [(any_1 x_1) ... (any_x x) (any_2 x_2) ...] x) any_x]
-  [(subst [(any_1 x_1) ... ] x) x]
-  [(subst [(any_1 x_1) ... ] (lambda (x ...) any_body))
-   (lambda (x_new ...)
-     (subst ((any_1 x_1) ...)
-            (subst-raw ((x_new x) ...) any_body)))
-   (where  (x_new ...)  ,(variables-not-in (term any_body) (term (x ...)))) ]
-  [(subst [(any_1 x_1) ... ] (any ...)) ((subst [(any_1 x_1) ... ] any) ...)]
-  [(subst [(any_1 x_1) ... ] any_*) any_*])
-
-(define-metafunction λα
-  subst-raw : ((x x) ...) any -> any
-  [(subst-raw ((x_n1 x_o1) ... (x_new x) (x_n2 x_o2) ...) x) x_new]
-  [(subst-raw ((x_n1 x_o1) ... ) x) x]
-  [(subst-raw ((x_n1 x_o1) ... ) (lambda (x ...) any))
-   (lambda (x ...) (subst-raw ((x_n1 x_o1) ... ) any))]
-  [(subst-raw [(any_1 x_1) ... ] (any ...))
-   ((subst-raw [(any_1 x_1) ... ] any) ...)]
-  [(subst-raw [(any_1 x_1) ... ] any_*) any_*])
+  (test-equal (judgment-holds (sum (s (s z)) (s z) (s (s (s z))))) #t)
+  (test-equal (judgment-holds (sum (s (s z)) (s z) (s (s (s n))))) #t)
+  (test-equal (judgment-holds (sum (s (s z)) (s z) (s (s (s (s n)))))) #f))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
