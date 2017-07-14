bcd59661610bf75901e262a85fcbd32fa9674b65
Harold Carr <harold.carr@gmail.com>
2017-07-14 09:26:54 -0700

    replace hand-written `subst` with redex's `substitute`

--- racket/LambdaAuth.rkt
+++ racket/LambdaAuth.rkt
@@ -100,6 +100,8 @@
      (let ((x E)) e)
      ))
 
+(default-language λα)
+
 (define λα-E? (redex-match? λα E))
 
 (module+ test
@@ -112,18 +114,16 @@
    λα
    #:domain P
    (--> (prog Π (in-hole E ((λ (x_1) e_1) v_1)))
-        (prog Π (in-hole E (subst ([v_1 x_1]) e_1)))
+        (prog Π (in-hole E (substitute e_1 x_1 v_1)))
         "βv-apply")
    (--> (prog Π (in-hole E ((rec x_1 (λ (x_2) e_1))
                             v_1)))
         (prog Π (in-hole E ((λ (x_2) e_2)
                             v_1)))
-        (where e_2 (subst [((rec x_1 (λ (x_2) e_1))
-                            x_1)]
-                          e_1))
+        (where e_2 (substitute e_1 x_1 (rec x_1 (λ (x_2) e_1))))
         "βv-rec")
    (--> (prog Π (in-hole E (let ((x_1 v_1)) e_2)))
-        (prog Π (in-hole E (subst ([v_1 x_1]) e_2)))
+        (prog Π (in-hole E (substitute e_2 x_1 v_1)))
         "βv-let")
    (--> (prog Π (in-hole E (case (inj1 v_1) v_2 v_3)))
         (prog Π (in-hole E (v_2 v_1)))
@@ -219,23 +219,24 @@
         (where h_1 (hash-shallow v_1))
         "-->P-auth")
    (--> (prog (π h ...) (in-hole E (unauth (α h_1 v_1))))
-        (prog (π h ... v_2) (in-hole E v_1))
-        (where v_2 (shallow-projection v_1))
+        (prog (π h ... (shallow-projection v_1))
+              (in-hole E v_1))
         "-->P-unauth")
    ))
 
 (define -->V
   (extend-reduction-relation -->βv λα-auth
    #:domain P
-   (--> (prog Π (in-hole E (auth v_1)))
-        (prog Π (in-hole E h_1))
-        (where h_1 ,(hash (term v_1)))
+   (--> (prog Π (in-hole E (auth v)))
+        (prog Π (in-hole E h))
+        (where h ,(hash (term v))) ;; TODO make hash metafu
         "-->V-auth")
-   (--> (prog (π h_0 h_1 ...)
-              (in-hole E (unauth h_3)))
+   (--> (prog (π h_s0 h_1 ...)
+              (in-hole E (unauth h)))
         (prog (π h_1 ...)
-              (in-hole E h_0))
-        (side-condition (equal? (hash (term h_0)) (term h_3)))
+              (in-hole E h_s0))
+        (side-condition (equal? (hash (term h_s0))
+                                (term h)))
         "-->V-unauth")
    ))
 
@@ -328,118 +329,6 @@
                          (α "0df9eea0bad5a55395db9ec290dfcf4a883d5d3e" unit)
                          (α "0df9eea0bad5a55395db9ec290dfcf4a883d5d3e" unit)))))
   )
-
-;; =============================================================================
-;; UTIL
-
-;; -----------------------------------------------------------------------------
-;; (in x x_1 ...) is x a member of (x_1 ...)?
-
-(module+ test
-  (test-equal (term (in x (y z x y z))) #true)
-  (test-equal (term (in x ())) #false)
-  (test-equal (term (in x (y z w))) #false))
-
-(define-metafunction λα
-  in : x (x ...) -> boolean
-  [(in x (x_1 ... x x_2 ...)) #true]
-  [(in x (x_1 ...)) #false])
-
-;; -----------------------------------------------------------------------------
-;; (=α e_1 e_2) determines whether e_1 and e_2 are α equivalent
-
-(module+ test
-  (test-equal (term (=α (λ (x) x) (λ (y) y))) #true)
-  (test-equal (term (=α (λ (x) (x 1)) (λ (y) (y 1)))) #true)
-  (test-equal (term (=α (λ (x) x) (λ (y) z))) #false))
-
-(define-metafunction λα
-  =α : any any -> boolean
-  [(=α any_1 any_2) ,(equal? (term (sd any_1)) (term (sd any_2)))])
-
-;; a Racket definition for use in Racket positions
-(define (=α/racket x y) (term (=α ,x ,y)))
-
-;; (sd e) computes the static distance version of e
-(define-extended-language SD λα
-  (e ::= .... (K n))
-  (n ::= natural))
-
-(define SD? (redex-match? SD e))
-
-(module+ test
-  (define sd1 (term (K 1)))
-  (define sd2 (term 1))
-
-  (test-equal (SD? sd1) #true))
-
-(define-metafunction SD
-  sd : any -> any
-  [(sd any_1) (sd/a any_1 ())])
-
-(module+ test
-  (test-equal (term (sd/a x ())) (term x))
-  (test-equal (term (sd/a x ((y) (z) (x)))) (term (K 2 0)))
-  (test-equal (term (sd/a ((λ (x) x) (λ (y) y)) ()))
-              (term ((λ () (K 0 0)) (λ () (K 0 0)))))
-  (test-equal (term (sd/a (λ (x) (x (λ (y) y))) ()))
-              (term (λ () ((K 0 0) (λ () (K 0 0))))))
-  (test-equal (term (sd/a (λ (z x) (x (λ (y) z))) ()))
-              (term (λ () ((K 0 1) (λ () (K 1 0)))))))
-
-(define-metafunction SD
-  sd/a : any ((x ...) ...) -> any
-  [(sd/a x ((x_1 ...) ... (x_0 ... x x_2 ...) (x_3 ...) ...))
-   ;; bound variable
-   (K n_rib n_pos)
-   (where n_rib ,(length (term ((x_1 ...) ...))))
-   (where n_pos ,(length (term (x_0 ...))))
-   (where #false (in x (x_1 ... ...)))]
-  [(sd/a (λ (x ...) any_1) (any_rest ...))
-   (λ () (sd/a any_1 ((x ...) any_rest ...)))]
-  [(sd/a (any_fun any_arg ...) (any_rib ...))
-   ((sd/a any_fun (any_rib ...)) (sd/a any_arg (any_rib ...)) ...)]
-  [(sd/a any_1 any)
-   ;; free variable, constant, etc
-   any_1])
-
-;; -----------------------------------------------------------------------------
-;; (subst ([e x] ...) e_*) substitutes e ... for x ... in e_* (hygienically)
-
-(module+ test
-  (test-equal (term (subst ([1 x][2 y]) x)) 1)
-  (test-equal (term (subst ([1 x][2 y]) y)) 2)
-  (test-equal (term (subst ([1 x][2 y]) z)) (term z))
-  (test-equal (term (subst ([1 x][2 y]) (λ (z w) (x y))))
-              (term (λ (z w) (1 2))))
-  (test-equal (term (subst ([1 x][2 y]) (λ (z w) (λ (x) (x y)))))
-              (term (λ (z w) (λ (x) (x 2))))
-              #:equiv =α/racket)
-  (test-equal (term (subst ((2 x)) ((λ (x) (1 x)) x)))
-              (term ((λ (x) (1 x)) 2))
-              #:equiv =α/racket))
-
-(define-metafunction λα
-  subst : ((any x) ...) any -> any
-  [(subst [(any_1 x_1) ... (any_x x) (any_2 x_2) ...] x) any_x]
-  [(subst [(any_1 x_1) ... ] x) x]
-  [(subst [(any_1 x_1) ... ] (λ (x ...) any_body))
-   (λ (x_new ...)
-     (subst ((any_1 x_1) ...)
-            (subst-raw ((x_new x) ...) any_body)))
-   (where  (x_new ...)  ,(variables-not-in (term any_body) (term (x ...)))) ]
-  [(subst [(any_1 x_1) ... ] (any ...)) ((subst [(any_1 x_1) ... ] any) ...)]
-  [(subst [(any_1 x_1) ... ] any_*) any_*])
-
-(define-metafunction λα
-  subst-raw : ((x x) ...) any -> any
-  [(subst-raw ((x_n1 x_o1) ... (x_new x) (x_n2 x_o2) ...) x) x_new]
-  [(subst-raw ((x_n1 x_o1) ... ) x) x]
-  [(subst-raw ((x_n1 x_o1) ... ) (λ (x ...) any))
-   (λ (x ...) (subst-raw ((x_n1 x_o1) ... ) any))]
-  [(subst-raw [(any_1 x_1) ... ] (any ...))
-   ((subst-raw [(any_1 x_1) ... ] any) ...)]
-  [(subst-raw [(any_1 x_1) ... ] any_*) any_*])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
