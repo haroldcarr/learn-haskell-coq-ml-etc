b0b10c426a3e093c108a8f04e4a0b7e3764ba8c6
Harold Carr <harold.carr@gmail.com>
2018-04-05 13:49:56 -0700

    refactor tests

--- haskell/playpen/blockchain/2018-06-lambda-conf/01-bc/test/BCSpec.hs
+++ haskell/playpen/blockchain/2018-06-lambda-conf/01-bc/test/BCSpec.hs
@@ -24,7 +24,7 @@ testEvidence =
       `shouldBe`
       "0000DDF9FB09F9A9C5A0EF57DC3E2916633BEDB95B38D54BDBFFF0B7D4D6E515"
     it "from genesisBlock + 1" $
-      evidence (bProof (eChain e1 !! 1)) (hashBlock (eChain e1 !! 1))  3805
+      evidence (bProof (eChain eTX0 !! 1)) (hashBlock (eChain eTX0 !! 1))  3805
       `shouldBe`
       "0000C5DD35BB87E83CFF98E2CE3D27C3CF7704362045D7D111B2F2D6D6C9DC03"
 
@@ -32,9 +32,9 @@ testProofOfWork :: Spec
 testProofOfWork =
   describe "proofOfWork" $ do
     it "from genesisBlock" $
-      proofOfWork 4 genesisBlock `shouldBe`          bProof (eChain e1 !! 1)
+      proofOfWork 4 genesisBlock `shouldBe`          bProof (eChain eTX0 !! 1)
     it "from genesisBlock + 1" $
-      proofOfWork 4 (eChain e1 !! 1) `shouldBe`      3805
+      proofOfWork 4 (eChain eTX0 !! 1) `shouldBe`      3805
 
 testIsValidChain :: Spec
 testIsValidChain =
@@ -46,8 +46,8 @@ testIsValidChain =
     it "invalid genesis" $
       let bg = genesisBlock { bIndex = 6 }
       in isValidChain 4 [bg] `shouldBe` Left "invalid genesis block"
-    it "valid e1" $
-      isValidChain 4 (eChain e1) `shouldBe` Right ()
+    it "valid eTX0" $
+      isValidChain 4 (eChain eTX0) `shouldBe` Right ()
     it "invalid previous hash" $
       isValidChain 4 (eChain e1BadPHash) `shouldBe` Left "invalid bPrevHash at 1"
     it "invalid proof" $
@@ -57,14 +57,14 @@ testResolveConflicts :: Spec
 testResolveConflicts =
   describe "ResolveConflicts" $ do
     it "found longer chain" $
-      resolveConflicts initialEnv  [eChain e1]
-      `shouldBe` (e1 , (True , ""))
+      resolveConflicts initialEnv  [eChain eTX0]
+      `shouldBe` (eTX0 , (True , ""))
     it "no    longer chain" $
-      resolveConflicts e1          [eChain initialEnv]
-      `shouldBe` (e1 , (False, ""))
+      resolveConflicts eTX0          [eChain initialEnv]
+      `shouldBe` (eTX0 , (False, ""))
     it "found longer chain and pool update" $
-      resolveConflicts e0'         [eChain e1]
-      `shouldBe` (e1', (True , ""))
+      resolveConflicts eLongerChainAndPoolUpdateIn  [eChain eTX0]
+      `shouldBe` (e1LongerChainAndPoolUpdateOut, (True , ""))
     it "invalid previous hash" $
       resolveConflicts initialEnv  [eChain e1BadPHash]
       `shouldBe` (initialEnv
@@ -81,10 +81,22 @@ testResolveConflicts =
 ------------------------------------------------------------------------------
 -- test data
 
-e0' :: Env
-e0' = initialEnv { eTXPool = ["TX-0","TX-should-stay"] }
+eTX0 :: Env
+(eTX0,_) = mine (addTransaction initialEnv "TX-0")
 
-makeChain :: BHash -> Proof -> [Block]
+eLongerChainAndPoolUpdateIn :: Env
+eLongerChainAndPoolUpdateIn = initialEnv { eTXPool = ["TX-0","TX-should-stay"] }
+
+e1LongerChainAndPoolUpdateOut :: Env
+e1LongerChainAndPoolUpdateOut = eTX0 { eTXPool = ["TX-should-stay"] }
+
+e1BadPHash :: Env
+e1BadPHash = eTX0 { eChain = makeChain "X" 658 }
+
+e1BadProof :: Env
+e1BadProof = eTX0 { eChain = makeChain (hashBlock genesisBlock) 0 }
+
+makeChain :: BHash -> Proof -> Chain
 makeChain ph p =
   [genesisBlock
   ,Block { bPrevHash = ph
@@ -94,53 +106,17 @@ makeChain ph p =
          }
   ]
 
-e1 :: Env
-(e1,_) = mine (addTransaction initialEnv "TX-0")
-
-e1' :: Env
-e1' = e1 { eTXPool = ["TX-should-stay"] }
-
-e1BadPHash :: Env
-e1BadPHash = e1 { eChain = makeChain "X" 658 }
-
-e1BadProof :: Env
-e1BadProof = e1 { eChain = makeChain (hashBlock genesisBlock) 0 }
-
 ------------------------------------------------------------------------------
 -- data to ensure TXs not lost
 
 e1NotLost :: Env
-e1NotLost = Env
-  { eTXPool = []
-  , eChain =
-    [ genesisBlock
-    , Block { bPrevHash = hashBlock genesisBlock
-            , bIndex = 1
-            , bTXs = ["TX1","TX2"]
-            , bProof = bProof (eChain e1 !! 1)
-            }
-    ]
-  , eProofDifficulty = 4, eNodes = [], eThisNode = ""
-  }
+(e1NotLost,_) = mine (addTransaction (addTransaction initialEnv "TX1") "TX2")
 
 e2NotLost :: Env
-e2NotLost = Env
-  { eTXPool = []
-  , eChain =
-    [ genesisBlock
-    , Block { bPrevHash = hashBlock genesisBlock
-            , bIndex = 1
-            , bTXs = ["TX1"]
-            , bProof = bProof (eChain e1 !! 1)
-            }
-    , Block { bPrevHash = "\233\237\222\222E\138\254\241k\232\151\DLE\177\175g\191\134e\248q\158\153V^\182\244r\156\187.\244\193"
-            , bIndex = 2
-            , bTXs = ["TX3"]
-            , bProof = 52668
-            }
-    ]
-  , eProofDifficulty = 4, eNodes = [], eThisNode = ""
-  }
+e2NotLost =
+  let (etx1,_) = mine (addTransaction initialEnv "TX1")
+      (etx2,_) = mine (addTransaction etx1       "TX3")
+   in  etx2
 
 e1NotLastAfterResolve :: Env
 e1NotLastAfterResolve = e2NotLost { eTXPool = ["TX2"] }
