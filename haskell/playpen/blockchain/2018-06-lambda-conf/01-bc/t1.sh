76b7f7c6c60772d6e1bacf9c4f5b3f0407f9a670
Harold Carr <harold.carr@gmail.com>
2018-04-06 10:48:42 -0700

    Made tests follow expository progression of code.

--- haskell/playpen/blockchain/2018-06-lambda-conf/01-bc/t1.sh
+++ haskell/playpen/blockchain/2018-06-lambda-conf/01-bc/t1.sh
@@ -15,12 +15,10 @@ doall env
 doone 1 tx-no-forward?TX3
 doone 1 mine
 sleep 2
-section "at this point the heights should be 0:2 1:3"
-section "0 should have TX1 TX2 in its chain"
-section "1 should have TX1 TX3 in its chain"
+section "at this point the heights should be 0:2 1:3; 0 should have TX1 TX2 in its chain; 1 should have TX1 TX3 in its chain"
 doall env
 
 doone 0 resolve
-section "0 should have 1's chain; *but it should TX2 in its pool*"
+section "0 should have 1's chain; *but it should have TX2 in its pool*"
 doall env
 
--- haskell/playpen/blockchain/2018-06-lambda-conf/01-bc/test/BCSpec.hs
+++ haskell/playpen/blockchain/2018-06-lambda-conf/01-bc/test/BCSpec.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -9,14 +11,55 @@ import           Test.Hspec
 ------------------------------------------------------------------------------
 import           BC
 
-spec :: Spec
 spec = do
-  testEvidence
+  testAddTransaction
+  testMine
   testProofOfWork
-  testIsValidChain
+  testEvidence
   testResolveConflicts
+  testIsValidChain
 
-testEvidence :: Spec
+------------------------------------------------------------------------------
+testAddTransaction =
+  describe "testAddTransaction" $do
+    it "empty bcTXPool in initialState" $
+      initialBCState
+      `shouldBe`
+      BCState { bcTXPool = []
+              , bcChain = [Block {bPrevHash = "1", bIndex = 0, bTXs = [], bProof = 100}]
+              , bcProofDifficulty = 4}
+    it "adds to bcTXPool" $
+      addTransaction initialBCState "TX1"
+      `shouldBe`
+      BCState { bcTXPool = ["TX1"]
+              , bcChain = [Block {bPrevHash = "1", bIndex = 0, bTXs = [], bProof = 100}]
+              , bcProofDifficulty = 4}
+
+------------------------------------------------------------------------------
+testMine =
+  describe "testMine" $do
+    it "addBlock" $
+      let (s, _) = mine (addTransaction (addTransaction initialBCState "TX1") "TX2")
+       in s `shouldBe`
+          BCState { bcTXPool = []
+                  , bcChain = [Block { bPrevHash = "1", bIndex = 0, bTXs = [], bProof = 100}
+                              ,Block { bPrevHash = "\202\169KU\139\ETX\212\&3W\145`\229\224S\159\177\253\nF\167\158\227\250\255\244\v\207\228z\233\171\237"
+                                     , bIndex = 1
+                                     , bTXs = ["TX1","TX2"]
+                                     , bProof = 134530}]
+                  , bcProofDifficulty = 4}
+
+------------------------------------------------------------------------------
+(bcTX1,_) = mine (addTransaction initialBCState "TX1")
+
+testProofOfWork =
+  describe "proofOfWork" $ do
+    it "from genesisBlock" $
+      proofOfWork 4 genesisBlock `shouldBe`  bProof (bcChain bcTX1 !! 1)
+    it "from genesisBlock + 1" $
+      proofOfWork 4 (bcChain bcTX1 !! 1) `shouldBe` 52668
+
+------------------------------------------------------------------------------
 testEvidence =
   describe "evidence" $ do
     it "from genesisBlock" $
@@ -24,36 +67,45 @@ testEvidence =
       `shouldBe`
       "0000DDF9FB09F9A9C5A0EF57DC3E2916633BEDB95B38D54BDBFFF0B7D4D6E515"
     it "from genesisBlock + 1" $
-      evidence (bProof (bcChain eTX0 !! 1)) (hashBlock (bcChain eTX0 !! 1))  3805
+      evidence (bProof (bcChain bcTX1 !! 1)) (hashBlock (bcChain bcTX1 !! 1))  52668
       `shouldBe`
-      "0000C5DD35BB87E83CFF98E2CE3D27C3CF7704362045D7D111B2F2D6D6C9DC03"
+      "000072456DC7CC3975C0CC3543B6BA201E6F4D056679970C3644D2DDEB4EEA67"
 
-testProofOfWork :: Spec
-testProofOfWork =
-  describe "proofOfWork" $ do
-    it "from genesisBlock" $
-      proofOfWork 4 genesisBlock `shouldBe`          bProof (bcChain eTX0 !! 1)
-    it "from genesisBlock + 1" $
-      proofOfWork 4 (bcChain eTX0 !! 1) `shouldBe`      3805
+------------------------------------------------------------------------------
+eTX0 :: BCState
+(eTX0,_) = mine (addTransaction initialBCState "TX-0")
 
-testIsValidChain :: Spec
-testIsValidChain =
-  describe "isValidChain" $ do
-    it "invalid empty" $
-      isValidChain 4 [] `shouldBe` Left "empty blockchain"
-    it "valid genesis" $
-      isValidChain 4 [genesisBlock] `shouldBe` Right ()
-    it "invalid genesis" $
-      let bg = genesisBlock { bIndex = 6 }
-      in isValidChain 4 [bg] `shouldBe` Left "invalid genesis block"
-    it "valid eTX0" $
-      isValidChain 4 (bcChain eTX0) `shouldBe` Right ()
-    it "invalid previous hash" $
-      isValidChain 4 (bcChain e1BadPHash) `shouldBe` Left "invalid bPrevHash at 1"
-    it "invalid proof" $
-      isValidChain 4 (bcChain e1BadProof) `shouldBe` Left "invalid bProof at 1"
+eLongerChainAndPoolUpdateIn :: BCState
+eLongerChainAndPoolUpdateIn = initialBCState { bcTXPool = ["TX-0","TX-should-stay"] }
 
-testResolveConflicts :: Spec
+e1LongerChainAndPoolUpdateOut :: BCState
+e1LongerChainAndPoolUpdateOut = eTX0 { bcTXPool = ["TX-should-stay"] }
+
+e1BadPHash :: BCState
+e1BadPHash = eTX0 { bcChain = makeChain "X" 658 }
+
+e1BadProof :: BCState
+e1BadProof = eTX0 { bcChain = makeChain (hashBlock genesisBlock) 0 }
+
+makeChain :: BHash -> Proof -> Chain
+makeChain ph p =
+  [genesisBlock
+  ,Block { bPrevHash = ph
+         , bIndex = 1, bTXs = ["TX-0"]
+         , bProof = p}]
+
+e1NotLost :: BCState
+(e1NotLost,_) = mine (addTransaction (addTransaction initialBCState "TX1") "TX2")
+
+e2NotLost :: BCState
+e2NotLost =
+  let (etx1,_) = mine (addTransaction initialBCState "TX1")
+      (etx2,_) = mine (addTransaction etx1       "TX3")
+   in  etx2
+
+e1NotLastAfterResolve :: BCState
+e1NotLastAfterResolve = e2NotLost { bcTXPool = ["TX2"] }
+
 testResolveConflicts =
   describe "ResolveConflicts" $ do
     it "found longer chain" $
@@ -79,48 +131,23 @@ testResolveConflicts =
                  , (True, ""))
 
 ------------------------------------------------------------------------------
--- test data
-
-eTX0 :: BCState
-(eTX0,_) = mine (addTransaction initialBCState "TX-0")
-
-eLongerChainAndPoolUpdateIn :: BCState
-eLongerChainAndPoolUpdateIn = initialBCState { bcTXPool = ["TX-0","TX-should-stay"] }
-
-e1LongerChainAndPoolUpdateOut :: BCState
-e1LongerChainAndPoolUpdateOut = eTX0 { bcTXPool = ["TX-should-stay"] }
-
-e1BadPHash :: BCState
-e1BadPHash = eTX0 { bcChain = makeChain "X" 658 }
-
-e1BadProof :: BCState
-e1BadProof = eTX0 { bcChain = makeChain (hashBlock genesisBlock) 0 }
-
-makeChain :: BHash -> Proof -> Chain
-makeChain ph p =
-  [genesisBlock
-  ,Block { bPrevHash = ph
-         , bIndex = 1
-         , bTXs = ["TX-0"]
-         , bProof = p
-         }
-  ]
+testIsValidChain =
+  describe "isValidChain" $ do
+    it "invalid empty" $
+      isValidChain 4 [] `shouldBe` Left "empty blockchain"
+    it "valid genesis" $
+      isValidChain 4 [genesisBlock] `shouldBe` Right ()
+    it "invalid genesis" $
+      let bg = genesisBlock { bIndex = 6 }
+      in isValidChain 4 [bg] `shouldBe` Left "invalid genesis block"
+    it "valid eTX0" $
+      isValidChain 4 (bcChain eTX0) `shouldBe` Right ()
+    it "invalid previous hash" $
+      isValidChain 4 (bcChain e1BadPHash) `shouldBe` Left "invalid bPrevHash at 1"
+    it "invalid proof" $
+      isValidChain 4 (bcChain e1BadProof) `shouldBe` Left "invalid bProof at 1"
 
 ------------------------------------------------------------------------------
--- data to ensure TXs not lost
-
-e1NotLost :: BCState
-(e1NotLost,_) = mine (addTransaction (addTransaction initialBCState "TX1") "TX2")
-
-e2NotLost :: BCState
-e2NotLost =
-  let (etx1,_) = mine (addTransaction initialBCState "TX1")
-      (etx2,_) = mine (addTransaction etx1       "TX3")
-   in  etx2
-
-e1NotLastAfterResolve :: BCState
-e1NotLastAfterResolve = e2NotLost { bcTXPool = ["TX2"] }
-
 {-
 :set -XOverloadedStrings
 import           Data.List                            ((\\))
