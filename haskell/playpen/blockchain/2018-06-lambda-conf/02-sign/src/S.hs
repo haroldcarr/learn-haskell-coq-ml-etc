d177bc9fde8ab40ecd61a13bc0f4f54df8632b76
Harold Carr <harold.carr@gmail.com>
2018-04-29 09:09:41 -0700

    gcoin works now

--- haskell/playpen/blockchain/2018-06-lambda-conf/02-sign/src/S.hs
+++ haskell/playpen/blockchain/2018-06-lambda-conf/02-sign/src/S.hs
@@ -37,6 +37,7 @@ generatePKSKIO
   => m (RSA.PublicKey, RSA.PrivateKey)
 generatePKSKIO = RSA.generate 200 0x10001
 
+-- TODO : tests do not work when using this one
 generatePKSK
   :: Either RSA.Error (RSA.PublicKey, RSA.PrivateKey)
 generatePKSK = E.maybeToRight RSA.InvalidParameters
@@ -55,19 +56,14 @@ signMsg
   :: RSA.PrivateKey
   -> Msg
   -> Either RSA.Error Signature
-signMsg p m =
-  let r = E.mapRight Signature (PK.sign Nothing (Just H.SHA512) p (getMsg m))
-  in ("signMsg: msg: " ++ show m ++ "; sig: " ++ show r) `trace` r
+signMsg p m = E.mapRight Signature (PK.sign Nothing (Just H.SHA512) p (getMsg m))
 
 verifyMsgSig
   :: RSA.PublicKey
   -> Msg
   -> Signature
   -> Bool
-verifyMsgSig p m s =
-  PK.verify (Just H.SHA512) p (getMsg m)
-    ((\x -> ("verifyMsgSig: msg:" ++ show m ++ "; sig: " ++ show x) `trace` x)
-     (getSig s))
+verifyMsgSig p m s = PK.verify (Just H.SHA512) p (getMsg m) (getSig s)
 
 doitIO :: RT.MonadRandom m
      => Msg
@@ -88,21 +84,18 @@ top = doitIO (Msg "this is a message")
 
 ------------------------------------------------------------------------------
 
-newtype UUID   = UUID   { getUuid   :: ByteString } deriving (Generic, Show)
-newtype TXHash = TXHash { getTXHash :: ByteString } deriving (Eq, Generic, Ord, Show)
-newtype Hash   = Hash   { getHash   :: ByteString } deriving (Generic, Show)
+newtype UUID    = UUID    { getUuid    :: ByteString } deriving (Generic, Show)
+newtype STXHash = STXHash { getSTXHash :: ByteString } deriving (Eq, Generic, Ord, Show)
+newtype Hash    = Hash    { getHash    :: ByteString } deriving (Generic, Show)
 instance S.Serialize UUID
-instance S.Serialize TXHash
+instance S.Serialize STXHash
 instance S.Serialize Hash
 deriving instance Generic     RSA.PublicKey
 deriving instance S.Serialize RSA.PublicKey
 
-hash :: BS.ByteString -> Hash
-hash = Hash . BA.convert . H.hashWith H.SHA256
-
 data TX
   = CreateCoin UUID
-  | TransferCoin TXHash RSA.PublicKey
+  | TransferCoin STXHash RSA.PublicKey
   deriving (Generic, Show)
 instance S.Serialize TX
 
@@ -112,6 +105,11 @@ data SignedTX = SignedTX
   } deriving (Generic, Show)
 instance S.Serialize SignedTX
 
+createUUID :: IO UUID
+createUUID = do
+  u <- U.nextRandom
+  return (UUID (BSL.toStrict (U.toByteString u)))
+
 signTX :: RSA.PrivateKey -> TX -> Either RSA.Error SignedTX
 signTX sk tx = E.mapRight (SignedTX tx) (signMsg sk (Msg (S.encode tx)))
 
@@ -120,11 +118,6 @@ verifyTXSig pk stx = case stx of
   (SignedTX tx@(CreateCoin _)     sig) -> v tx sig
   (SignedTX tx@(TransferCoin _ _) sig) -> v tx sig
  where v tx = verifyMsgSig pk (Msg (S.encode tx))
-
-createUUID :: IO UUID
-createUUID = do
-  u <- U.nextRandom
-  return (UUID (BSL.toStrict (U.toByteString u)))
 
 createCoinIO :: RSA.PrivateKey -> IO (Either RSA.Error SignedTX)
 createCoinIO sk = do
@@ -136,53 +129,63 @@ createCoin u sk = signTX sk (CreateCoin u)
 
 transferCoin :: SignedTX -> RSA.PrivateKey -> RSA.PublicKey -> Either RSA.Error SignedTX
 transferCoin fromCoin ownerSK toPK = do
-  let fromHash = hash (S.encode fromCoin)
-      toCoin   = TransferCoin (TXHash (getHash fromHash)) toPK
+  let fromHash = hashSignedTX fromCoin
+      toCoin   = TransferCoin fromHash toPK
   signTX ownerSK toCoin
 
+hashSignedTX :: SignedTX -> STXHash
+hashSignedTX = STXHash . getHash . hash . S.encode
+
+hash :: BS.ByteString -> Hash
+hash = Hash . BA.convert . H.hashWith H.SHA256
+
 testIt = do
-  u <- runIO createUUID
-  let Right (creatorPK, creatorSK) = generatePKSK
-  {-
-  let Right (alicePK  , aliceSK)   = generatePKSK
-  let Right (bobPK    , _bobSK)    = generatePKSK
-  let Right (jimPK    , _jimSK)    = generatePKSK
-  -}
-  let Right cc   = createCoin   u    creatorSK
-  {-
-  let Right cToA = transferCoin cc   creatorSK alicePK
-  let Right aToB = transferCoin cToA aliceSK   bobPK
-  let Right aToJ = transferCoin cToA aliceSK   jimPK
-  let chain1 = addToChain M.empty [cc{-, cToA-}]
-  let chain2 = addToChain M.empty [cc, cToA]
-  -}
-  describe "GoofyCoin" $
+  u                      <- runIO createUUID
+  (creatorPK, creatorSK) <- runIO generatePKSKIO
+  (alicePK  , aliceSK)   <- runIO generatePKSKIO
+  (bobPK    , bobSK)     <- runIO generatePKSKIO
+  (jimPK    , _jimSK)    <- runIO generatePKSKIO
+  let Right cc    = createCoin   u     creatorSK
+  let Right cToA  = transferCoin cc    creatorSK alicePK
+  let Right aToB1 = transferCoin cToA  aliceSK   bobPK
+  let Right aToJ  = transferCoin cToA  aliceSK   jimPK
+  let Right bToA  = transferCoin aToB1 bobSK     alicePK
+  let Right aToB2 = transferCoin bToA  aliceSK   bobPK
+  let chain    = addToChain M.empty [cc, cToA, aToB1, bToA]
+  let chainBad = addToChain M.empty     [cToA, aToB1, bToA]
+  describe "GoofyCoin" $ do
     it "isValidCoin createCoin" $
       isValidCoin creatorPK M.empty cc `shouldBe` Right True
-    {-
     it "isValidCoin chain1 aToJ" $
-      isValidCoin creatorPK chain1 aToJ `shouldBe` Right True
+      isValidCoin creatorPK chain aToJ `shouldBe` Right True
     it "isValidCoin chain1 aToJ (double spend)" $
-      isValidCoin creatorPK chain2 aToB `shouldBe` Right True
-    -}
+      isValidCoin creatorPK chain aToB1 `shouldBe` Right True
+    it "isValidCoin bad sig" $
+      isValidCoin creatorPK M.empty (cc { sSig = Signature "BAD" }) `shouldBe` Right False
+    it "isValidCoin aToB2" $
+      isValidCoin creatorPK chain aToB2 `shouldBe` Right True
+    it "isValidCoin chainBad aToB2" $
+      isValidCoin creatorPK chainBad aToB2 `shouldBe`
+      Left ("No TX for : " <> show ((\(TransferCoin h _) -> h) (sTX cToA)))
  where
-  addToChain :: M.Map TXHash SignedTX -> [SignedTX] -> M.Map TXHash SignedTX
-  addToChain = foldr (\x cc -> M.insert (TXHash (getHash (hash (S.encode x)))) x cc)
+  addToChain :: M.Map STXHash SignedTX -> [SignedTX] -> M.Map STXHash SignedTX
+  addToChain = foldr (\x cc -> M.insert (STXHash (getHash (hash (S.encode x)))) x cc)
   isValidCoin
     :: RSA.PublicKey          -- ^ creator public key
-    -> M.Map TXHash SignedTX  -- ^ the "chain"
+    -> M.Map STXHash SignedTX  -- ^ the "chain"
     -> SignedTX               -- ^ TX to verify
     -> Either Text Bool
-  isValidCoin cpk _ stx@(SignedTX (CreateCoin _) _) =
-    return $ verifyTXSig cpk stx
-  isValidCoin cpk m stx@(SignedTX (TransferCoin txh _) _) =
-    case M.lookup txh m of
-      Nothing
-        -> Left ("No TX for : " <> show txh)
-      Just cc@(SignedTX (CreateCoin _) _)
-        -> isValidCoin cpk m cc
-      Just    (SignedTX (TransferCoin _ opk) _)
-        -> if verifyTXSig opk stx
-           then return True
+  isValidCoin cpk m stx = case stx of
+    (SignedTX (CreateCoin _) _) ->
+      return $ verifyTXSig cpk stx
+    (SignedTX (TransferCoin txh _) _) ->
+      case M.lookup txh m of
+        Nothing ->
+          Left ("No TX for : " <> show txh)
+        Just   cc@(SignedTX (CreateCoin _) _) ->
+          isValidCoin cpk m cc
+        Just next@(SignedTX (TransferCoin _ opk) _) ->
+           if verifyTXSig opk stx
+           then isValidCoin cpk m next
            else return False
 
