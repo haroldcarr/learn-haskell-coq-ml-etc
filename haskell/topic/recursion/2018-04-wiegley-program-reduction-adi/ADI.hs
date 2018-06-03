9497a7c6808825f712b38cc15270708104b2ecbd
Harold Carr <harold.carr@gmail.com>
2018-06-02 21:05:06 -0700

    adi : simplified example

--- haskell/topic/recursion/2018-04-wiegley-program-reduction-adi/ADI.hs
+++ haskell/topic/recursion/2018-04-wiegley-program-reduction-adi/ADI.hs
@@ -26,45 +26,47 @@ adi   f g   = g         (f . fmap (adi  f g) . unFix)
 adi'  f g x = g ( \z -> (f . fmap (adi  f g) . unFix) z)   x
 adi'' f g x = g ( \z ->  f  (fmap (adi  f g)  (unFix  z))) x
 
-data NAtom = NInt Int | NBool Bool deriving Show
+data Atom = AInt Int | ABool Bool deriving Show
 
-data NExprF r
-  = NConstant NAtom
-  | NAdd r r
-  | NIf r r r
+data ExprF r
+  = Const Atom
+  | Add r r
+  | If r r r
   deriving (Show, Functor)
 
-type NExpr = Fix NExprF
+type Expr = Fix ExprF
 
-evaluate :: NExpr -> Int
+evaluate :: Expr -> Int
 evaluate = cata $ \case
-  NConstant (NInt  n) -> n
-  NConstant (NBool b) -> if b then 1 else 0
-  NAdd l r            -> l + r
-  NIf  c t e          -> if c /= 0 then t else e
+  Const (AInt  n) -> n
+  Const (ABool b) -> if b then 1 else 0
+  Add   l r       -> l + r
+  If    c t e     -> if c /= 0 then t else e
 
-iff :: NExpr
-iff = Fix (NIf (Fix (NConstant (NInt 0)))
-               (Fix (NConstant (NInt 1)))
-               (Fix (NConstant (NInt 2))))
+iff :: Expr
+iff = Fix (If (Fix (Const (AInt 0)))
+              (Fix (Const (AInt 1)))
+              (Fix (Const (AInt 2))))
 
 eiff :: Int
 eiff = evaluate iff
 
-eval :: NExpr -> Int
+-- | psi runs first.  It is given a continuation `k`.
+--   Whatever it returns to `k` is the given to phi.
+eval :: Expr -> Int
 eval  = adi phi psi
   where
-    phi          (NConstant (NInt  n))  = trace "\nNInt" $ 100*n
-    phi          (NConstant (NBool b))  = if b then 1 else 0
-    phi          (NAdd l r)             = trace "\nNAdd" $ l + r
-    phi          (NIf  c t e)           = if c /= 0 then t else e
-    psi k   (Fix (NConstant (NInt  n))) = trace "\nFIX NInt" $ k (Fix (NConstant (NInt (n-3))))
-    psi k v@(Fix (NConstant (NBool _))) = k v
-    psi k   (Fix (NAdd l _))            = trace "\nFIX NAdd" $ k (Fix (NConstant (NInt (psi k l))))
-    psi k v@(Fix (NIf  _ _ _))          = k v
+    phi          (Const (AInt  n))  = trace "\nNInt" $ 100*n
+    phi          (Const (ABool b))  = if b then 1 else 0
+    phi          (Add l r)          = trace "\nNAdd" $ l + r
+    phi          (If  c t e)        = if c /= 0 then t else e
+    psi k   (Fix (Const (AInt  n))) = trace "\nFIX NInt" $ k (Fix (Const (AInt (n-3))))
+    psi k v@(Fix (Const (ABool _))) = k v
+    psi k   (Fix (Add l _))         = trace "\nFIX NAdd" $ k (Fix (Const (AInt (psi k l))))
+    psi k v@(Fix (If  _ _ _))       = k v
 
 eadi1,eadi2 :: Int
-eadi1 = eval (Fix (NConstant (NInt 0)))
-eadi2 = eval (Fix (NAdd (Fix (NConstant (NInt 1)))
-                        (Fix (NConstant (NInt 3)))))
+eadi1 = eval (Fix (Const (AInt 0)))
+eadi2 = eval (Fix (Add (Fix (Const (AInt 1)))
+                       (Fix (Const (AInt 3)))))
 
