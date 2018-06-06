7f1a8bcd5105b99e5f3c70dfdeabc3fea12cdbf9
Harold Carr <harold.carr@gmail.com>
2018-06-06 13:26:27 -0700

    refactor

--- haskell/playpen/blockchain/2018-06-lambda-conf-DELETE/01-bc/app/Main.hs
+++
@@ -1,7 +0,0 @@
-import System.Environment
-import BC
-
-main :: IO ()
-main = do
-  args <- getArgs
-  run args
--- haskell/playpen/blockchain/2018-06-lambda-conf-DELETE/01-bc/test/GCoinSpec.hs
+++
@@ -1,9 +0,0 @@
-module GCoinSpec where
-
-import           Test.Hspec
-------------------------------------------------------------------------------
-import           GCoin
-
-spec :: Spec
-spec =
-  testIsValidCoin isValidCoinTest addToChainTest emptyChainTest
