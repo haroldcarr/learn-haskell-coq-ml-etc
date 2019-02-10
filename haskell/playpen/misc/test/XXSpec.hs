019af84a772131c7716e10253db7ba6f500ffca6
Harold Carr <harold.carr@gmail.com>
2019-02-10 13:14:31 -0700

    closer, but still incorrect

--- haskell/playpen/misc/test/XXSpec.hs
+++ haskell/playpen/misc/test/XXSpec.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
 
 module XXSpec where
 
 import qualified Crypto.Hash     as Hash
+import qualified Data.ByteString as BS
 import           Data.Foldable   (foldl', foldr')
 import qualified Data.Map.Strict as Map
 import           Data.Word       (Word64)
 import           Test.Hspec
+
+{-# ANN module ("HLint: ignore Eta reduce"::String) #-}
 
 newtype LogIndex = LogIndex Int
   deriving (Show, Read, Eq, Ord, Enum, Num, Real, Integral)
@@ -20,7 +24,10 @@ data NodeId = NodeId { _host :: !String, _port :: !Port, _fullAddr :: !String }
   deriving (Eq,Ord,Read,Show)
 
 newtype CumulativeHash = CumulativeHash { unCumulativeHash :: Hash.Digest Hash.SHA256 }
-  deriving (Show, Eq, Ord)
+  deriving (Eq, Ord)
+
+instance Show CumulativeHash where
+  show (CumulativeHash h) = take 7 (show h)
 
 type AppendEntriesResponse = Int
 
@@ -28,12 +35,6 @@ type CommitProofMap = Map.Map (LogIndex, Term)
                               (Map.Map NodeId
                                        (Map.Map CumulativeHash
                                                 AppendEntriesResponse))
-
-spec :: Spec
-spec =
-  describe "XX" $
-    it "gets correct shardID" $
-      True `shouldBe` False
 
 getCommitProofMapAERs :: CommitProofMap -> [AppendEntriesResponse]
 getCommitProofMapAERs
@@ -53,47 +54,117 @@ commitProofMapSummaryString cpm
 
 commitProofMapSummary :: CommitProofMap -> [(Term, LogIndex, [(Int, CumulativeHash)])]
 commitProofMapSummary = countCommon . cpms
- where
-  cpms                  :: CommitProofMap -> [(Term, LogIndex, [[CumulativeHash]])]
-  cpms     cpm           =         Map.foldlWithKey' goOuter  []      cpm
-  goOuter  acc (li, t) v = (t, li, Map.foldl'        goMiddle [] v) : acc
-  goMiddle acc         v =         Map.foldlWithKey' goInner  [] v  : acc
-  goInner  acc hash    _ = hash                                     : acc
 
-  countCommon           :: [(Term, LogIndex, [[CumulativeHash]])]
-                        -> [(Term, LogIndex, [(Int, CumulativeHash)])]
-  countCommon            = map (\(t, li, xs) -> (t, li, cc [] xs))
+cpms                  :: CommitProofMap -> [(Term, LogIndex, [[CumulativeHash]])]
+cpms     cpm           =         Map.foldlWithKey' goOuter  []      cpm
 
-  cc                    :: [(Int, CumulativeHash)]                     -> [[CumulativeHash]]
+goOuter :: [(a1, b, [[a2]])] -> (b, a1) -> Map.Map k (Map.Map a2 p) -> [(a1, b, [[a2]])]
+goOuter  acc (li, t) v = (t, li, Map.foldl'        goMiddle [] v) : acc
+
+goMiddle :: [[a]] -> Map.Map a p -> [[a]]
+goMiddle acc         v =         Map.foldlWithKey' goInner  [] v  : acc
+
+goInner :: [a] -> a -> p -> [a]
+goInner  acc hash    _ = hash                                     : acc
+
+countCommon           :: [(Term, LogIndex, [[CumulativeHash]])]
+                      -> [(Term, LogIndex, [(Int, CumulativeHash)])]
+countCommon            = map (\(t, li, xs) -> (t, li, cc [] xs))
+
+cc                    :: [(Int, CumulativeHash)]                     -> [[CumulativeHash]]
+                      -> [(Int, CumulativeHash)]
+cc acc []       = acc
+cc acc (hs:hss) = cc (ccx acc hs hss) hss
+
+ccx                   :: [(Int, CumulativeHash)] -> [CumulativeHash] -> [[CumulativeHash]]
+                      -> [(Int, CumulativeHash)]
+ccx acc []    _        = acc
+ccx acc (h:hs) hss     = if h `isIn` acc
+                         then ccx acc                      hs hss
+                         else ccx (inOtherLists acc h hss) hs hss
+
+isIn                  :: CumulativeHash -> [(Int, CumulativeHash)] -> Bool
+isIn h acc             = foldr' (\(_, h') acc' -> h == h' || acc') False acc
+
+inOtherLists          :: [(Int, CumulativeHash)] -> CumulativeHash -> [[CumulativeHash]]
                         -> [(Int, CumulativeHash)]
-  cc acc []       = acc
-  cc acc (hs:hss) = cc (ccx acc hs hss) hss
+inOtherLists acc h hss = foldl' (gol h) acc hss
 
-  ccx                   :: [(Int, CumulativeHash)] -> [CumulativeHash] -> [[CumulativeHash]]
-                        -> [(Int, CumulativeHash)]
-  ccx acc []    _        = acc
-  ccx acc (h:hs) hss     = if h `isIn` acc
-                           then ccx acc                      hs hss
-                           else ccx (inOtherLists acc h hss) hs hss
+gol                   :: CumulativeHash -> [(Int, CumulativeHash)] -> [CumulativeHash]
+                      -> [(Int, CumulativeHash)]
+gol h acc l            = if h `elem` l then updateAcc h acc else acc
 
-  isIn                  :: CumulativeHash -> [(Int, CumulativeHash)] -> Bool
-  isIn h acc             = foldr' (\(_, h') acc' -> h == h' || acc') False acc
+updateAcc             :: CumulativeHash -> [(Int, CumulativeHash)]
+                      -> [(Int, CumulativeHash)]
+updateAcc h acc        =
+  let acc'' = foldr' (\(i, h') acc' ->
+                         if h == h' then (i+1, h') : acc'
+                                    else (i  , h') : acc')
+                     []
+                     acc
+   in if h `isIn` acc'' then acc'' else (1, h) : acc''
 
-  inOtherLists          :: [(Int, CumulativeHash)] -> CumulativeHash -> [[CumulativeHash]]
-                        -> [(Int, CumulativeHash)]
-  inOtherLists acc h hss = foldl' (go h) acc hss
+------------------------------------------------------------------------------
 
-  go                    :: CumulativeHash -> [(Int, CumulativeHash)] -> [CumulativeHash]
-                        -> [(Int, CumulativeHash)]
-  go h acc l             = if h `elem` l then updateAcc h acc else acc
+spec :: Spec
+spec = do
+  isInSpec
+  updateAccSpec
+  ccSpec
 
-  updateAcc             :: CumulativeHash -> [(Int, CumulativeHash)]
-                        -> [(Int, CumulativeHash)]
-  updateAcc h acc        =
-    let acc'' = foldl' (\acc' (i, h') ->
-                           if h == h' then (i+1, h) : acc'
-                                      else (i  , h) : acc')
-                       []
-                       acc
-     in if h `isIn` acc'' then acc'' else (1, h) : acc''
+isInSpec :: Spec
+isInSpec = describe "isIn" $ do
 
+  it "False" $
+    isIn (cHash "0") [(1, cHash "1"), (1, cHash "2"), (1, cHash "3")]
+    `shouldBe` False
+
+  it "front" $
+    isIn (cHash "1") [(1, cHash "1"), (1, cHash "2"), (1, cHash "3")]
+    `shouldBe` True
+
+  it "middle" $
+    isIn (cHash "2") [(1, cHash "1"), (1, cHash "2"), (1, cHash "3")]
+    `shouldBe` True
+
+  it "end" $
+    isIn (cHash "3") [(1, cHash "1"), (1, cHash "2"), (1, cHash "3")]
+    `shouldBe` True
+
+updateAccSpec :: Spec
+updateAccSpec = describe "updateAcc" $ do
+
+  it "new empty" $
+    updateAcc (cHash "1") []
+    `shouldBe`            [(1, cHash "1")]
+
+  it "new existing results" $
+    updateAcc (cHash "1") [                (2, cHash "2")]
+    `shouldBe`            [(1, cHash "1"), (2, cHash "2")]
+
+  it "existing" $
+    updateAcc (cHash "1") [(1, cHash "1")]
+    `shouldBe`            [(2, cHash "1")]
+
+  it "existing front" $
+    updateAcc (cHash "2") [(1, cHash "2"), (3, cHash "3"), (4, cHash "4")]
+    `shouldBe`            [(2, cHash "2"), (3, cHash "3"), (4, cHash "4")]
+
+  it "existing middle" $
+    updateAcc (cHash "2") [(1, cHash "1"), (1, cHash "2"), (3, cHash "3")]
+    `shouldBe`            [(1, cHash "1"), (2, cHash "2"), (3, cHash "3")]
+
+  it "existing end" $
+    updateAcc (cHash "3") [(1, cHash "1"), (2, cHash "2"), (2, cHash "3")]
+    `shouldBe`            [(1, cHash "1"), (2, cHash "2"), (3, cHash "3")]
+
+ccSpec :: Spec
+ccSpec = describe "cc" $
+  it "x" $
+    cc []      [   [cHash "1"],    [cHash "2"],    [cHash "3"]]
+    `shouldBe` [(1, cHash "1"), (1, cHash "2"), (1, cHash "3")]
+
+------------------------------------------------------------------------------
+
+cHash :: BS.ByteString -> CumulativeHash
+cHash = CumulativeHash . Hash.hash
