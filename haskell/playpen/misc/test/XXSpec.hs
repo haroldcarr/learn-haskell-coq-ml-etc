0f4d367fc0ce9813b2d2545629e682f12655d23f
Harold Carr <harold.carr@gmail.com>
2019-02-10 15:10:25 -0700

    works; tests; impl could be better/more beautiful

--- haskell/playpen/misc/test/XXSpec.hs
+++ haskell/playpen/misc/test/XXSpec.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedLists            #-}
 {-# LANGUAGE OverloadedStrings          #-}
 
 module XXSpec where
 
 import qualified Crypto.Hash     as Hash
 import qualified Data.ByteString as BS
-import           Data.Foldable   (foldl', foldr')
+import qualified Data.List       as DL
 import qualified Data.Map.Strict as Map
+import           Data.Ord        (Down (..))
 import           Data.Word       (Word64)
 import           Test.Hspec
 
@@ -21,7 +23,13 @@ newtype Term = Term Int
 type Port = Word64
 
 data NodeId = NodeId { _host :: !String, _port :: !Port, _fullAddr :: !String }
-  deriving (Eq,Ord,Read,Show)
+  deriving (Eq,Ord,Read)
+
+instance Show NodeId where
+  show nid = show (_fullAddr nid)
+
+mkNid :: String -> NodeId
+mkNid s = NodeId "host" 8080 s
 
 newtype CumulativeHash = CumulativeHash { unCumulativeHash :: Hash.Digest Hash.SHA256 }
   deriving (Eq, Ord)
@@ -36,133 +44,91 @@ type CommitProofMap = Map.Map (LogIndex, Term)
                                        (Map.Map CumulativeHash
                                                 AppendEntriesResponse))
 
-getCommitProofMapAERs :: CommitProofMap -> [AppendEntriesResponse]
-getCommitProofMapAERs
-  = concatMap Map.elems -- inner map
-  . concatMap Map.elems -- middle map
-  . Map.elems           -- outer map
-
-commitProofMapSummaryString :: CommitProofMap  -> String
-commitProofMapSummaryString cpm
-  = show
-    ( cpm
-    , map (\(Term t, LogIndex li, hs) -> (t, li, map go hs))
-      $ commitProofMapSummary cpm
-    )
- where
-  go (i, CumulativeHash h) = (i, take 7 (show h))
-
 commitProofMapSummary :: CommitProofMap -> [(Term, LogIndex, [(Int, CumulativeHash)])]
 commitProofMapSummary = countCommon . cpms
 
 cpms                  :: CommitProofMap -> [(Term, LogIndex, [[CumulativeHash]])]
 cpms     cpm           =         Map.foldlWithKey' goOuter  []      cpm
 
-goOuter :: [(a1, b, [[a2]])] -> (b, a1) -> Map.Map k (Map.Map a2 p) -> [(a1, b, [[a2]])]
+goOuter               :: [(a1, b, [[a2]])] -> (b, a1) -> Map.Map k (Map.Map a2 p)
+                      -> [(a1, b, [[a2]])]
 goOuter  acc (li, t) v = (t, li, Map.foldl'        goMiddle [] v) : acc
 
-goMiddle :: [[a]] -> Map.Map a p -> [[a]]
+goMiddle              :: [[a]] -> Map.Map a p -> [[a]]
 goMiddle acc         v =         Map.foldlWithKey' goInner  [] v  : acc
 
-goInner :: [a] -> a -> p -> [a]
+goInner               :: [a] -> a -> p -> [a]
 goInner  acc hash    _ = hash                                     : acc
 
 countCommon           :: [(Term, LogIndex, [[CumulativeHash]])]
                       -> [(Term, LogIndex, [(Int, CumulativeHash)])]
-countCommon            = map (\(t, li, xs) -> (t, li, cc [] xs))
+countCommon            = map (\(t, li, xs) -> (t, li, cc xs))
 
-cc                    :: [(Int, CumulativeHash)]                     -> [[CumulativeHash]]
-                      -> [(Int, CumulativeHash)]
-cc acc []       = acc
-cc acc (hs:hss) = cc (ccx acc hs hss) hss
-
-ccx                   :: [(Int, CumulativeHash)] -> [CumulativeHash] -> [[CumulativeHash]]
-                      -> [(Int, CumulativeHash)]
-ccx acc []    _        = acc
-ccx acc (h:hs) hss     = if h `isIn` acc
-                         then ccx acc                      hs hss
-                         else ccx (inOtherLists acc h hss) hs hss
-
-isIn                  :: CumulativeHash -> [(Int, CumulativeHash)] -> Bool
-isIn h acc             = foldr' (\(_, h') acc' -> h == h' || acc') False acc
-
-inOtherLists          :: [(Int, CumulativeHash)] -> CumulativeHash -> [[CumulativeHash]]
-                        -> [(Int, CumulativeHash)]
-inOtherLists acc h hss = foldl' (gol h) acc hss
-
-gol                   :: CumulativeHash -> [(Int, CumulativeHash)] -> [CumulativeHash]
-                      -> [(Int, CumulativeHash)]
-gol h acc l            = if h `elem` l then updateAcc h acc else acc
-
-updateAcc             :: CumulativeHash -> [(Int, CumulativeHash)]
-                      -> [(Int, CumulativeHash)]
-updateAcc h acc        =
-  let acc'' = foldr' (\(i, h') acc' ->
-                         if h == h' then (i+1, h') : acc'
-                                    else (i  , h') : acc')
-                     []
-                     acc
-   in if h `isIn` acc'' then acc'' else (1, h) : acc''
+cc                    :: [[CumulativeHash]] -> [(Int, CumulativeHash)]
+cc                     = DL.sortOn (Down . fst)
+                       . map (\g -> (length g, head g))
+                       . DL.group . DL.sort . DL.concat
 
 ------------------------------------------------------------------------------
 
 spec :: Spec
 spec = do
-  isInSpec
-  updateAccSpec
   ccSpec
-
-isInSpec :: Spec
-isInSpec = describe "isIn" $ do
-
-  it "False" $
-    isIn (cHash "0") [(1, cHash "1"), (1, cHash "2"), (1, cHash "3")]
-    `shouldBe` False
-
-  it "front" $
-    isIn (cHash "1") [(1, cHash "1"), (1, cHash "2"), (1, cHash "3")]
-    `shouldBe` True
-
-  it "middle" $
-    isIn (cHash "2") [(1, cHash "1"), (1, cHash "2"), (1, cHash "3")]
-    `shouldBe` True
-
-  it "end" $
-    isIn (cHash "3") [(1, cHash "1"), (1, cHash "2"), (1, cHash "3")]
-    `shouldBe` True
-
-updateAccSpec :: Spec
-updateAccSpec = describe "updateAcc" $ do
-
-  it "new empty" $
-    updateAcc (cHash "1") []
-    `shouldBe`            [(1, cHash "1")]
-
-  it "new existing results" $
-    updateAcc (cHash "1") [                (2, cHash "2")]
-    `shouldBe`            [(1, cHash "1"), (2, cHash "2")]
-
-  it "existing" $
-    updateAcc (cHash "1") [(1, cHash "1")]
-    `shouldBe`            [(2, cHash "1")]
-
-  it "existing front" $
-    updateAcc (cHash "2") [(1, cHash "2"), (3, cHash "3"), (4, cHash "4")]
-    `shouldBe`            [(2, cHash "2"), (3, cHash "3"), (4, cHash "4")]
-
-  it "existing middle" $
-    updateAcc (cHash "2") [(1, cHash "1"), (1, cHash "2"), (3, cHash "3")]
-    `shouldBe`            [(1, cHash "1"), (2, cHash "2"), (3, cHash "3")]
-
-  it "existing end" $
-    updateAcc (cHash "3") [(1, cHash "1"), (2, cHash "2"), (2, cHash "3")]
-    `shouldBe`            [(1, cHash "1"), (2, cHash "2"), (3, cHash "3")]
+  commitProofMapSummarySpec
 
 ccSpec :: Spec
-ccSpec = describe "cc" $
-  it "x" $
-    cc []      [   [cHash "1"],    [cHash "2"],    [cHash "3"]]
-    `shouldBe` [(1, cHash "1"), (1, cHash "2"), (1, cHash "3")]
+ccSpec = describe "cc" $ do
+
+  it "distinct" $
+    cc         [   [cHash "1"],    [cHash "2"],    [cHash "3"]]
+    `shouldBe` [(1, cHash "3"), (1, cHash "1"), (1, cHash "2")]
+
+  it "2 2s" $
+    cc         [   [cHash "1"],    [cHash "2"],    [cHash "3", cHash "2"]]
+    `shouldBe` [(2, cHash "2"), (1, cHash "3"), (1, cHash "1")]
+
+  it "a lot" $
+    cc         [ [cHash "1", cHash "3"],[cHash "2"],[cHash "3", cHash "2"],[cHash "3", cHash "99"]]
+    `shouldBe` [(3,cHash "3"),(2,cHash "2"),(1,cHash "1"),(1,cHash "99")]
+
+commitProofMapSummarySpec :: Spec
+commitProofMapSummarySpec = describe "commitProofMapSummary" $ do
+
+  it "(LogIndex 0, Term 0)" $
+    commitProofMapSummary
+      [( (LogIndex 0, Term 0)
+       , [ ( mkNid "1", [ (cHash "1", 1), (cHash "2", 2) ] )
+         , ( mkNid "2", [ (cHash "2", 2), (cHash "3", 3), (cHash "99", 99) ] )
+         , ( mkNid "3", [ (cHash "3", 3) ] )
+         , ( mkNid "4", [ (cHash "3", 3) ] )
+         ]
+       )
+      ]
+    `shouldBe`
+    [ (Term 0,LogIndex 0,[(3,cHash "3"),(2,cHash "2"),(1,cHash "1"),(1,cHash "99")])
+    ]
+
+  it "(LogIndex 0, Term 0), (LogIndex 0, Term 1)" $
+    commitProofMapSummary
+      [( (LogIndex 0, Term 0)
+       , [ ( mkNid "1", [ (cHash "1", 1), (cHash "2", 2) ] )
+         , ( mkNid "2", [ (cHash "2", 2), (cHash "3", 3), (cHash "99", 99) ] )
+         , ( mkNid "3", [ (cHash "3", 3) ] )
+         , ( mkNid "4", [ (cHash "3", 3) ] )
+         ]
+       )
+      ,( (LogIndex 0, Term 1)
+       , [ ( mkNid "1", [ (cHash "1", 1), (cHash "2", 2) ] )
+         , ( mkNid "2", [ (cHash "2", 2), (cHash "3", 3), (cHash "99", 99) ] )
+         , ( mkNid "3", [ (cHash "3", 3) ] )
+         , ( mkNid "4", [ (cHash "3", 3) ] )
+         ]
+       )
+      ]
+    `shouldBe`
+    [ (Term 1,LogIndex 0,[(3,cHash "3"),(2,cHash "2"),(1,cHash "1"),(1,cHash "99")])
+    , (Term 0,LogIndex 0,[(3,cHash "3"),(2,cHash "2"),(1,cHash "1"),(1,cHash "99")])
+    ]
 
 ------------------------------------------------------------------------------
 
