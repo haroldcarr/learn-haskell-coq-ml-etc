26e500893b64e12f54314a9a49c390c74f7d41ec
Harold Carr <harold.carr@gmail.com>
2019-05-28 16:12:59 -0600

    TAPL

--- foundations/book/TAPL/tapl/src/Ch06_Nameless_Rep_of_Terms.hs
+++ foundations/book/TAPL/tapl/src/Ch06_Nameless_Rep_of_Terms.hs
@@ -48,3 +48,27 @@ mkNames  = doNames lkup
     Just v  -> Right (v, j)
     Nothing -> Left $ show j <> " not present"
 
+------------------------------------------------------------------------------
+-- p 78 shifting : renumbering indices of Vars in Term
+
+{-
+When a substitution goes under an Abs (i.e., [x ->s](λy.x),
+assuming 1 is the index of x in outer context),
+the context in which the substitution is taking place becomes one var longer than the original,
+so increment the indices of free Vars in s so they keep referring to
+the same names in new context as they did before.
+
+But do NOT shift BOUND Vars in s.
+
+e.g., s = 2 (λ.0) (i.e., s = z (λw.w), assume 2 is index of z)
+- shift the 2 but not the 0
+
+shifting function takes a "cutoff" parameter 'c' that controls which Vars should be shifted
+- 'c' starts at 0 : meaning all variables should be shifted
+- gets (+1) each time the shifting function goes through a binder
+
+when calculating shift t
+- know that 't' comes from inside c-many binders in the original argument to shift
+- so all Vars k < c in 't' are bound in the original argument and should not be shifted
+-        Vars k ≥ c in 't' are free and should be shifted
+-}
