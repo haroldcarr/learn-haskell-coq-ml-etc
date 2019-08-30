0c58a238d910abef7ba11849f28a04750ec1c0ad
Harold Carr <harold.carr@gmail.com>
2019-08-30 08:14:23 -0600

    recursion schemes

--- haskell/book/Haskell_Design_Patterns/test/HDPSpec.hs
+++ haskell/book/Haskell_Design_Patterns/test/HDPSpec.hs
@@ -2,10 +2,12 @@ module HDPSpec where
 
 import           Ch00_Tests
 import           Ch06_01_generics_sum_of_products
+import           Ch06_02_generics_origami_recursion_schemes_fix
 import           Test.Hspec
 
 spec :: Spec
 spec = do
   _ <- runIO main
-  describe "HDP"
+  describe "HDP" $ do
     ch06_01_Test
+    ch06_02_Test
