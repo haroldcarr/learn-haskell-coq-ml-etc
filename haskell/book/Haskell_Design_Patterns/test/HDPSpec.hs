3c87e24c2040ff874368970a40e47f783e2a7d12
Harold Carr <harold.carr@gmail.com>
2019-08-30 20:42:11 -0600

    SYB

--- haskell/book/Haskell_Design_Patterns/test/HDPSpec.hs
+++ haskell/book/Haskell_Design_Patterns/test/HDPSpec.hs
@@ -3,6 +3,7 @@ module HDPSpec where
 import           Ch00_Tests
 import           Ch06_01_generics_sum_of_products
 import           Ch06_02_generics_origami_recursion_schemes_fix
+import           Ch06_03_generics_scrap_your_boilerplate
 import           Test.Hspec
 
 spec :: Spec
@@ -11,3 +12,4 @@ spec = do
   describe "HDP" $ do
     ch06_01_Test
     ch06_02_Test
+    ch06_03_Test
