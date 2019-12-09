fa57ff5d12f9d3d0ec078491355226c71be4ed17
Harold Carr <harold.carr@gmail.com>
2019-12-09 12:48:59 -0700

    checkpoint

--- haskell/topic/lens/hc-usage/src/X.hs
+++ haskell/topic/lens/hc-usage/src/X.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE FunctionalDependencies     #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MonoLocalBinds             #-}
@@ -13,8 +14,8 @@ module X where
 import           Control.Lens
 -- import qualified Control.Lens.Internal.FieldTH  as FTH
 -- import           Control.Lens.TH
--- import           Control.Monad.Trans.RWS.Strict
--- import qualified Data.Set                       as Set
+import           Control.Monad.Trans.RWS.Strict
+import qualified Data.Map.Strict                       as Map
 -- import qualified Language.Haskell.TH.Datatype   as THD
 -- import qualified Language.Haskell.TH.Lib        as THL
 -- import qualified Language.Haskell.TH.Syntax     as THS
@@ -25,10 +26,10 @@ import           Protolude                      hiding (get, gets, round, to)
 {-# ANN module ("HLint: ignore Reduce duplication" :: Prelude.String) #-}
 {-# ANN module ("HLint: ignore Redundant return" :: Prelude.String) #-}
 
-newtype Author    = Author   { _getAuthor   :: Text             } deriving (Eq, Ord, Show)
+newtype Author    = Author   { _authorAuthor   :: Text             } deriving (Eq, Ord, Show)
 newtype Epoch     = Epoch    { _epochEpoch    :: Int              } deriving (Eq, Num, Ord, Show)
-newtype HashValue = HashValue{ _getHashValue:: ByteString       } deriving (Eq, Ord, Show)
-newtype Round     = Round    { _getRound    :: Int              } deriving (Eq, Num, Ord, Show)
+newtype HashValue = HashValue{ _hashValueHashValue:: ByteString       } deriving (Eq, Ord, Show)
+newtype Round     = Round    { _roundRound    :: Int              } deriving (Eq, Num, Ord, Show)
 
 makeClassyFor "RWAuthor" "lAuthor" [("_getAuthor", "getAuthor")] ''Author
 makeClassyFor "RWEpoch" "lEpoch" [("_epochEpoch", "getEpoch")] ''Epoch
@@ -40,36 +41,112 @@ makeFields ''HashValue
 makeFields ''Round
 
 data BlockInfo = BlockInfo
-  { _biAuthor :: !Author
-  , _biEpoch  :: !Epoch
-  , _biRound  :: !Round
-  , _biId     :: !HashValue
+  { _blockInfoAuthor :: !Author
+  , _blockInfoEpoch  :: !Epoch
+  , _blockInfoRound  :: !Round
+  , _blockInfoId     :: !HashValue
   } deriving (Eq, Show)
 makeClassyFor "RWBlockInfo" "lBlockInfo"
-  [ ("_biAuthor" , "biAuthor")
-  , ("_biEpoch"  , "biEpoch")
-  , ("_biRound"  , "biRound")
-  , ("_biId"     , "biId") ]
+  [ ("_blockInfoAuthor" , "biAuthor")
+  , ("_blockInfoEpoch"  , "biEpoch")
+  , ("_blockInfoRound"  , "biRound")
+  , ("_blockInfoId"     , "biId") ]
   ''BlockInfo
 makeFields ''BlockInfo
 
 data VoteData = VoteData
-  { _vdProposed :: !BlockInfo
-  , _vdParent   :: !BlockInfo
+  { _voteDataProposed :: !BlockInfo
+  , _voteDataParent   :: !BlockInfo
   } deriving (Eq, Show)
 makeClassyFor "RWVoteData" "lVoteData"
-  [ ("_vdProposed", "vdProposed")
-  , ("_vdParent"  , "vdParent") ]
+  [ ("_voteDataProposed", "vdProposed")
+  , ("_voteDataParent"  , "vdParent") ]
   ''VoteData
 makeFields ''VoteData
 
-foo :: Author -> Epoch -> HashValue -> Round -> BlockInfo -> VoteData -> Text
+data BlockTree a = BlockTree
+  { _blockTreeVoteData      :: !(Map HashValue VoteData)
+  , _blockTreeRootId        :: !HashValue
+  } deriving (Eq, Show)
+makeClassyFor "RWBlockTree" "lBlockTree"
+  [ ("_blockTreeVoteData", "btVoteData")
+  , ("_blockTreeRootId",   "btRootId") ]
+  ''BlockTree
+makeFields ''BlockTree
+
+newtype BlockStore a = BlockStore
+  { _blockStoreInner         :: BlockTree a
+  } deriving (Eq, Show)
+makeClassyFor "RWBlockStore" "lBlockStore"
+  [ ("_blockStoreInner", "bsInner") ]
+  ''BlockStore
+makeFields ''BlockStore
+
+data Pacemaker = Pacemaker
+  { _pacemakerHighestCommittedRound :: !Round
+  , _pacemakerCurrentRound          :: !Round
+  } deriving (Eq, Show)
+makeClassyFor "RWPacemaker" "lPacemaker"
+  [ ("_pacemakerHighestCommittedRound", "psHighestCommittedRound")
+  , ("_pacemakerCurrentRound", "psCurrentRound") ]
+  ''Pacemaker
+
+data Vote = Vote
+  { _voteVoteData         :: !VoteData
+  , _voteAuthor           :: !Author
+  } deriving (Eq, Show)
+makeClassyFor "RWVote" "lVote"
+  [ ("_voteVoteData", "vVoteData")
+  , ("_voteAuthor", "vAuthor") ]
+  ''Vote
+makeFields ''Vote
+
+data EventProcessor a = EventProcessor
+  { _eventProcessorBlockStore        :: !(BlockStore a)
+  , _eventProcessorPacemaker         :: !Pacemaker
+  , _eventProcessorLastVoteSend      :: !(Maybe (Vote, Round))
+  } deriving (Eq, Show)
+makeClassyFor "RWEventProcessor" "lEventProcessor"
+  [ ("_eventProcessorBlockStore", "epsBlockStore")
+  , ("_eventProcessorPacemaker", "epsPacemaker")
+  , ("_eventProcessorLastVoteSend",  "epsLastVoteSend") ]
+  ''EventProcessor
+makeFields ''EventProcessor
+
+bi :: BlockInfo
+bi  = BlockInfo (Author "biauthor") (Epoch 0) (Round 0) (HashValue "0")
+
+ep :: EventProcessor ByteString
+ep  = EventProcessor
+        (BlockStore (BlockTree Map.empty (HashValue "btrootid")))
+        (Pacemaker (Round 100) (Round 101))
+        (Just ( Vote
+                  (VoteData bi bi)
+                  (Author "epauthor")
+              , Round 45))
+
+foo :: (Monad m, HasBlockStore s a, Show a) => RWST () [Text] s m ()
+foo  = do
+  bs <- use blockStore
+  tell ["TELL " <> show bs]
+  pure ()
+
+rfoo :: Monad m => m ((), EventProcessor ByteString, [Text])
+rfoo  = runRWST foo () ep
+{-
+foo
+  :: (RWBlockInfo x, RWVoteData x)
+  => Author -> Epoch -> HashValue -> Round -> BlockInfo -> VoteData
+  -> Text
 foo author epoch hashValue round blockInfo voteData =
   show (author, epoch, hashValue, round, blockInfo, voteData)
   <>
   show ( author^.getAuthor, epoch^.getEpoch, hashValue^.getHashValue, round^.getRound
        , blockInfo^.biAuthor
        , voteData^.vdProposed)
+  <>
+  bar voteData
+
 
 bar :: (RWBlockInfo x, RWVoteData x) => x -> Text
 bar  x = show (x^.vdProposed) <> show (baz x)
@@ -79,6 +156,13 @@ baz  x = show (x^.biAuthor.getAuthor)
 
 qux :: HasEpoch s Int => s -> Int
 qux x = x^.epoch
+
+r :: Text
+r  = foo (Author "author") (Epoch 1) (HashValue "HashValue") (Round 2)
+         (BlockInfo (Author "biauthor0") (Epoch 10) (Round 20) (HashValue "bid0"))
+         (VoteData (BlockInfo (Author "biauthor1") (Epoch 11) (Round 21) (HashValue "bid1"))
+                   (BlockInfo (Author "biauthor2") (Epoch 12) (Round 22) (HashValue "bid2")))
+-}
 {-
 obmMakeClassyFor
   :: Prelude.String -> Prelude.String -> [(Prelude.String, Prelude.String)] -> THS.Name
