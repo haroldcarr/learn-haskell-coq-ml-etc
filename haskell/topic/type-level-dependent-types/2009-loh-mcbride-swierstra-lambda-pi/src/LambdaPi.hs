6214cef887cddc634ce1fd8f02a16efce04c1ac9
Harold Carr <harold.carr@gmail.com>
2020-01-02 18:56:25 -0700

    doc; change throwError to Left; subst and quote

--- haskell/topic/type-level-dependent-types/2009-loh-mcbride-swierstra-lambda-pi/src/LambdaPi.hs
+++ haskell/topic/type-level-dependent-types/2009-loh-mcbride-swierstra-lambda-pi/src/LambdaPi.hs
@@ -668,32 +668,6 @@ mainLP = repLP True
 -- p3 : (STLC) Abstract syntax
 
 {-
-terms
-
-e ::= e :: τ  annotated term
-    | x       variable
-    | e e'    application
-    | λx → e  lambda abstraction
--}
-data ITerm -- Inferable
-     = Ann   CTerm Type -- explicit Annotation
-     | Bound Int        -- variables : deBruijn indice
-     | Free  Name       -- variable : e.g., top level
-     | ITerm :@: CTerm  -- application
-    deriving (Show, Eq)
-
-data CTerm -- Checkable
-     = Inf ITerm -- Inferable embedded in a CTerm
-     | Lam CTerm -- Lambda abstraction
-    deriving (Show, Eq)
-
-data Name
-     = Global String
-     | Local  Int -- when passing a binding into an algorithm, temporarily convert a bound var into a free
-     | Quote  Int
-    deriving (Show, Eq)
-
-{-
 types
 
 τ ::= α       base type
@@ -702,8 +676,36 @@ types
 data Type
      = TFree Name       -- type identifier
      | Fun   Type Type  -- function arrows (not polymorphic)
-    deriving (Show, Eq)
+    deriving (Eq, Show)
+{-
+terms
 
+e ::= e :: τ  annotated term
+    | x       variable
+    | e e'    application
+    | λx → e  lambda abstraction
+
+(Note: using numbers for local, and names for global is called a "locally nameless" representation)
+-}
+data ITerm -- Inferable (Term ↑)
+     = Ann   CTerm Type -- explicit Annotation
+     | Bound Int        -- variable : de Bruijn indice (i.e., no need to alpha reduce)
+                        -- num binders between its binder and occurrence
+     | Free  Name       -- variable : e.g., top level
+     | ITerm :@: CTerm  -- application
+    deriving (Eq, Show)
+
+data CTerm -- Checkable (Term ↓)
+     = Inf ITerm -- inferable embedded in a CTerm
+     | Lam CTerm -- lambda abstraction -- no explicit vars because using de Bruijn
+    deriving (Eq, Show)
+
+data Name
+     = Global String
+     | Local  Int    -- when passing a binder in an algorithm,
+                     -- temporarily convert a bound var into a free var
+     | Quote  Int
+    deriving (Eq, Show)
 {-
 values
 
@@ -711,11 +713,17 @@ v ::= n       neutral term
     | λx → v  lambda abstraction
 -}
 data Value
-     = VLam     (Value -> Value) -- lambda abstraction (HOAS : rep funs as Haskell funs)
-     | VNeutral Neutral
--- Eq and Show are just for testing.
+     = VNeutral Neutral
+     | VLam     (Value -> Value) -- lambda abstraction (HOAS : rep funs as Haskell funs)
+{-
+Since using HOAS, define a quote function to take a Value back to a term.
+- VLam takes a function as argument
+- therefore cannot derive Eq and Show
+- use quote (below) to get back at internal structure of a value (e.g., to display)
+-}
+-- Eq and Show are ONLY for testing.
 instance Eq Value where
-  (==) (VLam     _) (VLam     _) = True -- True of testing
+  (==) (VLam     _) (VLam     _) = True -- True for testing
   (==) (VLam     _)           _  = False
   (==)           _  (VLam     _) = False
   (==) (VNeutral l) (VNeutral r) = l == r
@@ -729,10 +737,11 @@ n ::= x     variable
     | n v   application
 -}
 data Neutral
-     = NFree Name
-     | NApp  Neutral Value
-     deriving (Show, Eq)
+     = NFree Name          -- free var
+     | NApp  Neutral Value -- application of neutral term to a value
+     deriving (Eq, Show)
 
+-- create the value corresponding to a free var
 vfree :: Name -> Value
 vfree n = VNeutral (NFree n)
 
@@ -748,7 +757,97 @@ examples:
 (id :: α → α) y ⇓ y
 (const :: (β → β) → α → β → β) id y ⇓ id
 -}
+{-
+Substitution handled via env of values.
+Bound vars represented as integers.
+Env's i-th position corresponds to value of var i.
+Add new element to Env whenever evaluating under a binder.
+Lookup element when encountering a bound var.
+-}
+type Env     = [Value]
 
+{-
+inferable eval (eval ↑)
+-}
+iEval :: ITerm -> (NameEnv Value,Env) -> Value
+{-
+ignore annotation
+
+e ⇓ v
+----------
+e :: τ ⇓ v
+-}
+iEval (Ann  e _)  d = cEval e d
+{-
+variables eval to themselves
+
+------
+x ⇓ x
+-}
+iEval (Free   x)  d = fromMaybe (vfree x) (lookup x (fst d))
+iEval (Bound ii)  d = snd d !! ii
+{-
+application
+-}
+iEval (e1 :@: e2) d = vapp (iEval e1 d) (cEval e2 d)
+
+vapp :: Value -> Value -> Value
+{-
+application
+
+case left subterm evaluates to lambda abstraction
+β-reduce
+may produce new redexes, so evaluate result
+
+e ⇓ λx → v    v[x ↦ e'] ⇓ v'
+----------------------------
+e e' ⇓ v'
+-}
+vapp (VLam f) v = f v -- Beta-reduce
+{-
+application
+
+case left subterm evaluates to neutral term
+eval cannot proceed further
+construct new neutral term from results of evaluating the two subterms
+e ⇓ n    e' ⇓ v'
+----------------
+e e' ⇓ n v'
+
+-- TODO : this looks like eval under lambda
+e ⇓ v
+---------------
+λx → e ⇓ λx → v
+-}
+vapp (VNeutral n)  v = VNeutral (NApp n v)
+
+{-
+checkable eval (eval ↓)
+-}
+cEval :: CTerm -> (NameEnv Value, Env) -> Value
+cEval (Inf  ii) d  = iEval ii d
+-- create a Haskell fun and add the bound var x to Env while evaluating the body
+cEval (Lam  e0) d0 = VLam (\x -> cEval e0 ((\(e, d) -> (e,  x : d)) d0))
+
+------------------------------------------------------------------------------
+-- p4 : (STLC) Type System
+
+--------------------------------------------------
+{-
+context and well-formedness
+
+Γ ⊢ e :: t -- means e is type t in context Γ
+
+context
+- lists valid base types
+  - α :: ∗ - indicates α is a base type
+- associates identifiers with type information
+  - x :: t - indicates x is a term of type t
+
+Γ ::= ε          empty context
+    | Γ, α :: ∗  adding a type identifier (base types)
+    | Γ, x :: τ  adding a term identifier (type info)
+-}
 data Kind = Star
   deriving (Eq, Show)
 
@@ -758,95 +857,52 @@ data Info
   deriving (Eq, Show)
 
 type Context = [(Name, Info)]
-type Env     = [Value]
-
-iEval :: ITerm -> (NameEnv Value,Env) -> Value
-
 {-
-ignore annotation
+Γ(z) denotes information associated with identifier z by context Γ
 
-e ⇓ v
-----------
-e :: τ ⇓ v
--}
-iEval (Ann  e _)  d = cEval e d
-
-{-
-variables eval to themselves
-
-------
-x ⇓ x
--}
-iEval (Free   x)  d = fromMaybe (vfree x) (lookup x (fst d))
-iEval (Bound ii)  d = snd d !! ii
-
--- application
-iEval (e1 :@: e2) d = vapp (iEval e1 d) (cEval e2 d)
-
-vapp :: Value -> Value -> Value
-{-
-application, case left subterm evaluates to lambda abstraction
-β-reduce
-may produce new redexes, so evaluate result
-
-e ⇓ λx → v    v[x ↦ e'] ⇓ v'
-----------------------------
-e e' ⇓ v'
--}
-vapp (VLam f) v = f v -- Beta-reduce
-
-{-
-application, case left subterm evaluates to neutral term
-eval cannot proceed further
-construct new neutral term from results of evaluating the two subterms
-e ⇓ n    e' ⇓ v'
-----------------
-e e' ⇓ n v'
-
-e ⇓ v
----------------
-λx → e ⇓ λx → v
--}
-vapp (VNeutral n)  v = VNeutral (NApp n v)
-
-cEval :: CTerm -> (NameEnv Value, Env) -> Value
-cEval (Inf  ii) d  = iEval ii d
-cEval (Lam  e0) d0 = VLam (\x -> cEval e0 ((\(e, d) -> (e,  x : d)) d0)) -- eval under lambda
-
-------------------------------------------------------------------------------
--- p4 : (STLC) Type System
-
-{-
-Γ ::= ε          empty context
-    | Γ, α :: ∗  adding a type identifier
-    | Y, x :: τ  adding a term identifier
+definitions of contexts and their validity (i.e., "well-formed"):
 
                valid (Γ)               valid (Γ)   Γ ⊢ τ :: ∗
 ---------      -----------------       ----------------------
 valid (ε)      valid (Γ, α :: ∗)       valid (Γ,  x :: τ )
 
-Γ(α) = ∗                 Γ ⊢ τ :: ∗   Γ ⊢ τ' :: ∗
----------- (TVAR)        ------------------------- (FUN)
-Γ ⊢ α :: ∗               Γ ⊢ τ      →     τ' :: ∗
-
-Figure 2.  definitions of contexts and their validity
-
-- Γ ⊢ e::t : e is type t in context Γ
-- context
-  - lists valid base types
-  - associates identifiers with type information
-- α :: ∗ - indicates α is a base type
-- x :: t - indicates x is a term of type t
 - free vars in terms and types must occur in the context, e.g.,
   - for const to be of type (β → β) → α → β → β
   - context must contain at least: α :: ∗, β :: ∗, const :: (β → β) → α → β → β
-  - Note α and β are introduced before they are used in the type of const
+  - α and β must be introduced before using them in the type of const
 - multiple bindings for same var can occur, rightmost binding takes precedence
-- Γ(z) denotes information associated with identifier z by context Γ
-- TVAR, FUN explain when a type is well-formed
-  - i.e., when all its free variables appear in the context
-- in the well-formedness and type rules assume all contexts are valid
+- the well-formedness and type rules assume all contexts are valid
+-}
+{-
+check well-formedness of types (kind ↓)
 
+TVAR, FUN explain when a type is well-formed
+- i.e., when all its free vars appear in the context
+-}
+cKind :: Context -> Type -> Kind -> Either String ()
+{-
+  Γ(α) = ∗
+---------- (TVAR)
+Γ ⊢ α :: ∗
+-}
+cKind g (TFree x) Star = case lookup x g of
+  Just (HasKind Star) -> pure ()
+  Just z              -> Left ("cKind: not covered: " ++ show x ++ " : " ++ show z)
+  Nothing             -> Left "cKind: unknown identifier"
+{-
+Γ ⊢ τ :: ∗   Γ ⊢ τ' :: ∗
+------------------------- (FUN)
+Γ ⊢ τ      →     τ' :: ∗
+-}
+cKind g (Fun kk kk') Star = do
+  cKind g kk   Star
+  cKind g kk'  Star
+{-
+--------------------------------------------------
+type rules
+
+:: ↓ -- type is an input to type checking algorithm
+:: ↑ -- type is produced by type checking algorithm
 
 Γ ⊢ τ :: ∗   Γ ⊢ e :: ↓ τ
 ------------------------- (ANN)
@@ -873,8 +929,6 @@ Figure 3 Type rules for λ →
 - no type inference of types of lambda-bound vars
 - only type checking
 - for annotated terms, vars and applications the types can be determined
-- :: ↓ -- type is an input to type checking algorithm
-- :: ↑ -- type is produced by type checking algorithm
 
 - inferable terms
   - ANN : check annotated terms against their type annotation, and then return the type
@@ -882,17 +936,13 @@ Figure 3 Type rules for λ →
   - APP : 1st postion must be of a function type;
           check arg against fun’s domain;
           return fun range as the result type
+
+type-checking functions take an Int indicating the number of encountered.
+On the initial call, it is 0.
 -}
-
-cKind :: Context -> Type -> Kind -> Either String ()
-cKind g (TFree x) Star = case lookup x g of
-  Just (HasKind Star) -> pure ()
-  Just z              -> throwError ("cKind: not covered: " ++ show x ++ " : " ++ show z)
-  Nothing             -> throwError "cKind: unknown identifier"
-cKind g (Fun kk kk') Star = do
-  cKind g kk   Star
-  cKind g kk'  Star
-
+{-
+infer term type (type ↑)
+-}
 iType0 :: Context -> ITerm -> Either String Type
 iType0 = iType 0
 
@@ -903,82 +953,99 @@ iType ii g (Ann e ty) = do
   pure ty
 iType  _ g (Free x) = case lookup x g of
   Just (HasType ty) -> pure ty
-  Just z            -> throwError ("iType: not covered: " ++ show z)
-  Nothing           -> throwError "unknown identifier"
+  Just z            -> Left ("iType: not covered: " ++ show z)
+  Nothing           -> Left "unknown identifier"
 iType ii g (e1 :@: e2) = do
   si <- iType ii g e1
   case si of
     Fun ty ty' -> do cType ii g e2 ty; pure ty'
-    _          -> throwError "illegal application"
-iType _ _ x = throwError ("iType: not covered: " ++ show x)
+    _          -> Left "illegal application"
+iType _ _ x = Left ("iType: not covered: " ++ show x)
 
+{-
+check term type (type ↓)
+-}
 cType :: Int -> Context -> CTerm -> Type -> Either String ()
+{-
+The type eq check performed for an inferable term is syntactic equality on 'Type'.
+The type checker does not perform unification.
+-}
 cType ii g (Inf e) ty           = do
   ty' <- iType ii g e
-  unless (ty == ty') (throwError "type mismatch")
+  unless (ty == ty') (Left "type mismatch")
+{-
+type rule for lambda abstraction
+- add the bound var to the context while checking the body
+- ii indicates the number of binders
+- Local i is a fresh name associated with the bound var
+- add Local i to the context Γ (i.e., 'g') when checking the body
+- because a bound var is turned into a free var
+  - perform corresponding substitution on the body
+- the type checker will never encounter a bound var;
+  - so the function type ↑ has no case for Bound.
+-}
 cType ii g (Lam e) (Fun ty ty') =
   cType  (ii + 1) ((Local ii, HasType ty) : g) (cSubst 0 (Free (Local ii)) e) ty'
-cType  _ _ _ _                  = throwError "type mismatch"
+cType  _ _ _ _                  = Left "type mismatch"
+{-
+substitution
 
+Int arg indicates which var is to be substituted
+-}
+{-
+inferable substitution (subst ↑)
+-}
 iSubst :: Int -> ITerm -> ITerm -> ITerm
 iSubst ii r (Ann e ty)  = Ann (cSubst ii r e) ty
+{-
+Bound : check if the var encountered is the one to be substituted or not
+-}
 iSubst ii r (Bound j)   = if ii == j then r else Bound j
 iSubst  _ _ (Free y)    = Free y
 iSubst ii r (e1 :@: e2) = iSubst ii r e1 :@: cSubst ii r e2
-
+{-
+checkable substitution (subst ↓)
+-}
 cSubst :: Int -> ITerm -> CTerm -> CTerm
 cSubst ii r (Inf e) = Inf (iSubst ii r e)
+{-
+Lam : increase i to indicate that var to substitute is underneath another binder
+-}
 cSubst ii r (Lam e) = Lam (cSubst (ii + 1) r e)
 
+--------------------------------------------------
+{-
+quote takes Int indicating num binders traversed, initially 0
+-}
 quote0 :: Value -> CTerm
 quote0 = quote 0
 
 quote :: Int -> Value -> CTerm
+{-
+lambda abstraction
+- generate fresh variable Quote i
+- apply Haskell function f to the fresh variable
+- resulting value is quoted at level i + 1
+- Quote takes an Int arg to ensure newly created names do not clash
+-}
 quote ii (VLam f)     = Lam (quote (ii + 1) (f (vfree (Quote ii))))
+{-
+neutral term (application of free var to other values)
+-}
 quote ii (VNeutral n) = Inf (neutralQuote ii n)
-
+{-
+neutralQuote : to quote args
+-}
 neutralQuote :: Int -> Neutral -> ITerm
 neutralQuote ii (NFree x)  = boundfree ii x
 neutralQuote ii (NApp n v) = neutralQuote ii n :@: quote ii v
-
+{-
+boundfree
+- checks if var at head of application is a Quote, therefore a bound var, or a free name
+-}
 boundfree :: Int -> Name -> ITerm
 boundfree ii (Quote k) = Bound (ii - k - 1)
 boundfree  _ x         = Free x
-
-id',const' :: CTerm
-id'      = Lam (Inf (Bound 0))
-const'   = Lam (Lam (Inf (Bound 1)))
-
-tfree   :: String -> Type
-tfree a  = TFree (Global a)
-free    :: String -> CTerm
-free x   = Inf (Free (Global x))
-
-term1,term2 :: ITerm
-term1    = Ann id' (Fun (tfree "a") (tfree "a")) :@: free "y"
-term2    = Ann const' (Fun  (Fun (tfree "b") (tfree "b"))
-                               (Fun  (tfree "a")
-                                     (Fun (tfree "b") (tfree "b"))))
-              :@: id' :@: free "y"
-
-env1,env2 :: [(Name, Info)]
-env1     = [ (Global "y", HasType (tfree "a"))
-            , (Global "a", HasKind Star) ]
-env2     =   (Global "b", HasKind Star) : env1
-
-test_eval1,test_eval2 :: CTerm
-test_eval1= quote0 (iEval term1 ([],[]))
-   {-  \eval{test_eval1}  -}
-
-test_eval2= quote0 (iEval term2 ([],[]))
-   {-  \eval{test_eval2}  -}
-
-test_type1,test_type2 :: Either String Type
-test_type1= iType0 env1 term1
-   {-  \eval{test_type1}  -}
-
-test_type2= iType0 env2 term2
-   {-  \eval{test_type2}  -}
 
 ------------------------------------------------------------------------------
 -- 3. Dependent Types
@@ -998,7 +1065,7 @@ data CTerm_
      | FZero_ CTerm_
      | FSucc_ CTerm_ CTerm_
 
-    deriving (Show, Eq)
+    deriving (Eq, Show)
 
 data ITerm_
      = Ann_ CTerm_ CTerm_
@@ -1020,7 +1087,7 @@ data ITerm_
      | Fin_ CTerm_
      | FinElim_ CTerm_ CTerm_ CTerm_ CTerm_ CTerm_
 
-    deriving (Show, Eq)
+    deriving (Eq, Show)
 
 data Value_
     = VLam_  (Value_ -> Value_)
@@ -1281,13 +1348,13 @@ iType_ ii g (Pi_ tyt tyt')
 iType_  _ g (Free_ x)
     =    case lookup x (snd g) of
             Just ty        -> pure ty
-            Nothing        -> throwError ("unknown identifier: " ++ PP.render (iPrint_ 0 0 (Free_ x)))
+            Nothing        -> Left ("unknown identifier: " ++ PP.render (iPrint_ 0 0 (Free_ x)))
 iType_ ii g (e1 :$: e2)
     =    do si <- iType_ ii g e1
             case si of
                 VPi_  ty ty'  -> do cType_ ii g e2 ty
                                     pure ( ty' (cEval_ e2 (fst g, [])))
-                _             -> throwError "illegal application"
+                _             -> Left "illegal application"
 
 iType_  _ _ Nat_                  = pure VStar_
 iType_ ii g (NatElim_ m mz ms n)  =
@@ -1347,12 +1414,12 @@ iType_ i g (EqElim_ a m mr x y eq) =
        -- let eqVal = cEval_ eq (fst g, [])
        pure (foldl vapp_ mVal [xVal, yVal])
 
-iType_ _i _g x = throwError ("iType: not covered: " ++ show x)
+iType_ _i _g x = Left ("iType: not covered: " ++ show x)
 
 cType_ :: Int -> (NameEnv Value_,Context_) -> CTerm_ -> Type_ -> Either String ()
 cType_ ii g (Inf_ e) v
     = do v' <- iType_ ii g e
-         unless ( quote0_ v == quote0_ v') (throwError ("type mismatch:\n" ++ "type inferred:  " ++ PP.render (cPrint_ 0 0 (quote0_ v')) ++ "\n" ++ "type expected:  " ++ PP.render (cPrint_ 0 0 (quote0_ v)) ++ "\n" ++ "for expression: " ++ PP.render (iPrint_ 0 0 e)))
+         unless ( quote0_ v == quote0_ v') (Left ("type mismatch:\n" ++ "type inferred:  " ++ PP.render (cPrint_ 0 0 (quote0_ v')) ++ "\n" ++ "type expected:  " ++ PP.render (cPrint_ 0 0 (quote0_ v)) ++ "\n" ++ "for expression: " ++ PP.render (iPrint_ 0 0 e)))
 cType_ ii g0 (Lam_ e) ( VPi_ ty ty')
     =    cType_  (ii + 1) ((\(d,g) -> (d,  (Local ii, ty ) : g)) g0)
                  (cSubst_ 0 (Free_ (Local ii)) e) ( ty' (vfree_ (Local ii)))
@@ -1364,16 +1431,16 @@ cType_ ii g (Nil_ a) (VVec_ bVal VZero_) =
     do cType_ ii g a VStar_
        let aVal = cEval_ a (fst g, [])
        unless  (quote0_ aVal == quote0_ bVal)
-               (throwError "type mismatch")
+               (Left "type mismatch")
 cType_ ii g (Cons_ a n x xs) (VVec_ bVal (VSucc_ k)) =
     do cType_ ii g a VStar_
        let aVal = cEval_ a (fst g, [])
        unless  (quote0_ aVal == quote0_ bVal)
-               (throwError "type mismatch")
+               (Left "type mismatch")
        cType_ ii g n VNat_
        let nVal = cEval_ n (fst g, [])
        unless  (quote0_ nVal == quote0_ k)
-               (throwError "number mismatch")
+               (Left "number mismatch")
        cType_ ii g x aVal
        cType_ ii g xs (VVec_ bVal k)
 
@@ -1381,13 +1448,13 @@ cType_ ii g (Refl_ a z) (VEq_ bVal xVal yVal) =
     do cType_ ii g a VStar_
        let aVal = cEval_ a (fst g, [])
        unless  (quote0_ aVal == quote0_ bVal)
-               (throwError "type mismatch")
+               (Left "type mismatch")
        cType_ ii g z aVal
        let zVal = cEval_ z (fst g, [])
        unless  (quote0_ zVal == quote0_ xVal && quote0_ zVal == quote0_ yVal)
-               (throwError "type mismatch")
+               (Left "type mismatch")
 
-cType_ _ _ _ _ = throwError "type mismatch"
+cType_ _ _ _ _ = Left "type mismatch"
 {-# ANN cType_ ("HLint: ignore Reduce duplication" :: Prelude.String) #-}
 
 data Nat = Zero | Succ Nat
--- haskell/topic/type-level-dependent-types/2009-loh-mcbride-swierstra-lambda-pi/test/TestLPSpec.hs
+++ haskell/topic/type-level-dependent-types/2009-loh-mcbride-swierstra-lambda-pi/test/TestLPSpec.hs
@@ -11,10 +11,73 @@ import qualified Text.ParserCombinators.Parsec as P
 
 spec :: Spec
 spec  = do
-  parseST
   evalST
+  quoteST
+  parseST
   handleST
   parseLP
+
+------------------------------------------------------------------------------
+
+id',const' :: CTerm
+id'      = Lam (Inf (Bound 0))
+const'   = Lam (Lam (Inf (Bound 1)))
+
+tfree   :: String -> Type
+tfree a  = TFree (Global a)
+free    :: String -> CTerm
+free x   = Inf (Free (Global x))
+
+term1,term2 :: ITerm
+term1    = Ann id' (Fun (tfree "a") (tfree "a")) :@: free "y"
+term2    = Ann const' (Fun (Fun (tfree "b") (tfree "b"))
+                           (Fun (tfree "a")
+                                (Fun (tfree "b") (tfree "b"))))
+           :@: id' :@: free "y"
+
+env1,env2 :: [(Name, Info)]
+env1     = [ (Global "y", HasType (tfree "a"))
+           , (Global "a", HasKind Star) ]
+env2     =   (Global "b", HasKind Star) : env1
+
+test_eval1,test_eval2 :: CTerm
+test_eval1= quote0 (iEval term1 ([],[]))
+test_eval2= quote0 (iEval term2 ([],[]))
+
+test_type1,test_type2 :: Either String Type
+test_type1= iType0 env1 term1
+test_type2= iType0 env2 term2
+
+evalST :: Spec
+evalST  = describe "evalST" $ do
+  it "eval term1" $ test_eval1 `shouldBe` (Inf (Free (Global "y")))
+  it "eval term2" $ test_eval2 `shouldBe` (Lam (Inf  (Bound  0)))
+  it "type term1" $ test_type1 `shouldBe` (Right (TFree (Global "a")))
+  it "type term2" $ test_type2 `shouldBe` (Right (Fun (TFree (Global "b")) (TFree (Global "b"))))
+
+------------------------------------------------------------------------------
+
+vconst :: Value
+vconst  = VLam (\x -> VLam (\_y -> x))
+
+{-
+When quote moves underneath a binder, a temp name is created for the bound var,
+via constructor Quote (during the quotation process to avoid name clashes).
+
+If the bound var occurs in the body then give the de Bruijn index via
+the number of binders passed between introducing and finding the Quote variable.
+
+quote 0 (VLam (λx →     VLam (λy → x)))
+=         Lam (quote 1 (VLam (λy →            vfree (Quote 0))))
+=         Lam           (Lam (quote        2 (vfree (Quote 0))))
+=         Lam           (Lam (neutralQuote 2 (NFree (Quote 0))))
+=         Lam           (Lam (Bound 1))
+-}
+quoteST :: Spec
+quoteST  = describe "quoteST" $
+  it "quote0 const" $ quote0 vconst `shouldBe` Lam (Lam (Inf (Bound 1)))
+
+------------------------------------------------------------------------------
 
 parseST :: Spec
 parseST  = describe "parseST" $ do
@@ -34,9 +97,54 @@ parseST  = describe "parseST" $ do
              (Lam (Lam (Inf (Bound 1))))
              (Fun
                (TFree (Global "Nat"))
-               (Fun
-                 (TFree (Global "Nat"))
-                 (TFree (Global "Nat"))))))
+               (Fun (TFree (Global "Nat")) (TFree (Global "Nat"))))))
+
+------------------------------------------------------------------------------
+
+handleST :: Spec
+handleST  = describe "handleST" $ do
+  it "define and apply 'id'" $
+    stHandle [ "assume (a :: *) (y :: a)"
+             , "let id = (\\x -> x) :: a -> a"
+             , "id y" ]
+    `shouldBe`
+    ( [ APutStrLn "id :: a -> a"
+      , APutStrLn "y :: a" ]
+    , ( True
+      , ""
+      , [(Global "it",VNeutral (NFree (Global "y")))
+        ,(Global "id",VLam id)]  -- N.B., 'id' (see Eq instance for Value)
+      , [(Global "it",HasType (TFree (Global "a")))
+        ,(Global "id",HasType (Fun (TFree (Global "a")) (TFree (Global "a"))))
+        ,(Global "y",HasType (TFree (Global "a")))
+        ,(Global "a",HasKind Star) ] ))
+  it "define and apply 'const'" $
+    stHandle [ "assume (a :: *) (y :: a)"
+             , "assume (b :: *)"
+             , "let const = (\\x y -> x) :: (b -> b) -> a -> b -> b"
+             , "let id = (\\x -> x) :: b -> b"
+             , "const id y" ]
+    `shouldBe`
+    ( [ APutStrLn "const :: b -> b -> a -> b -> b"
+      , APutStrLn "id :: b -> b"
+      , APutStrLn "\\x -> x :: b -> b" ]
+    , ( True
+      , ""
+      , [(Global "it",    VLam id)
+        ,(Global "id",    VLam id)
+        ,(Global "const", VLam id)]
+      , [(Global "it",HasType (Fun (TFree (Global "b")) (TFree (Global "b"))))
+       , (Global "id",HasType (Fun (TFree (Global "b")) (TFree (Global "b"))))
+       , (Global "const",HasType (Fun
+                                   (Fun (TFree (Global "b")) (TFree (Global "b")))
+                                   (Fun
+                                     (TFree (Global "a"))
+                                     (Fun (TFree (Global "b")) (TFree (Global "b"))))))
+       , (Global "b",HasKind Star)
+       , (Global "y",HasType (TFree (Global "a")))
+       , (Global "a",HasKind Star) ] ))
+
+------------------------------------------------------------------------------
 
 parseLP :: Spec
 parseLP  = describe "parseLP" $ do
@@ -65,65 +173,6 @@ parseLP  = describe "parseLP" $ do
                                    (Inf_ (Pi_
                                           (Inf_ (Bound_ 1))
                                           (Inf_ (Bound_ 3))))))))))))
-
-evalST :: Spec
-evalST  = describe "evalST" $ do
-  it "eval term1" $ test_eval1 `shouldBe` (Inf (Free (Global "y")))
-  it "eval term2" $ test_eval2 `shouldBe` (Lam (Inf  (Bound  0)))
-  it "type term1" $ test_type1 `shouldBe` (Right (TFree (Global "a")))
-  it "type term2" $ test_type2 `shouldBe` (Right (Fun (TFree (Global "b")) (TFree (Global "b"))))
-
-handleST :: Spec
-handleST  = describe "handleST" $ do
-  it "define and apply 'id'" $
-    stHandle [ "assume (a :: *) (y :: a)"
-             , "let id = (\\x -> x) :: a -> a"
-             , "id y" ]
-    `shouldBe`
-    ( [ APutStrLn "id :: a -> a"
-      , APutStrLn "y :: a" ]
-    , ( True
-      , ""
-      , [(Global "it",VNeutral (NFree (Global "y")))
-        ,(Global "id",VLam id)]  -- N.B., 'id' (see Eq instance for Value)
-      , [(Global "it",HasType (TFree (Global "a")))
-        ,(Global "id",HasType (Fun
-                               (TFree (Global "a"))
-                               (TFree (Global "a"))))
-        ,(Global "y",HasType (TFree (Global "a")))
-        ,(Global "a",HasKind Star) ] ))
-  it "define and apply 'const'" $
-    stHandle [ "assume (a :: *) (y :: a)"
-             , "assume (b :: *)"
-             , "let const = (\\x y -> x) :: (b -> b) -> a -> b -> b"
-             , "let id = (\\x -> x) :: b -> b", "const id y" ]
-    `shouldBe`
-    ( [ APutStrLn "const :: b -> b -> a -> b -> b"
-      , APutStrLn "id :: b -> b"
-      , APutStrLn "\\x -> x :: b -> b" ]
-    , ( True
-      , ""
-      , [(Global "it",    VLam id)
-        ,(Global "id",    VLam id)
-        ,(Global "const", VLam id)]
-      , [(Global "it",HasType (Fun
-                               (TFree (Global "b"))
-                               (TFree (Global "b"))))
-       , (Global "id",HasType (Fun
-                               (TFree (Global "b"))
-                               (TFree (Global "b"))))
-       , (Global "const",HasType (Fun
-                                   (Fun
-                                     (TFree (Global "b"))
-                                     (TFree (Global "b")))
-                                   (Fun
-                                     (TFree (Global "a"))
-                                     (Fun
-                                       (TFree (Global "b"))
-                                       (TFree (Global "b"))))))
-       , (Global "b",HasKind Star)
-       , (Global "y",HasType (TFree (Global "a")))
-       , (Global "a",HasKind Star) ] ))
 
 ------------------------------------------------------------------------------
 
