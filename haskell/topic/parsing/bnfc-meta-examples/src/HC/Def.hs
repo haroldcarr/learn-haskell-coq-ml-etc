55443a2498b61f60c3b31ad363ef0b44664c0f8b
Harold Carr <harold.carr@gmail.com>
2020-02-17 15:49:40 -0700

    bnfc checkpoint

--- haskell/topic/parsing/bnfc-meta-examples/src/HC/Def.hs
+++ haskell/topic/parsing/bnfc-meta-examples/src/HC/Def.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE LambdaCase      #-}
 {-# LANGUAGE QuasiQuotes     #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module HC.Def where
 
+import           Data.List
+import           Data.Maybe
 import           Language.LBNF
 
 bnfc [lbnf|
@@ -22,8 +25,29 @@ HC.Def.pExpr $ HC.Def.myLexer "\\ -> x"
 
 :set -XQuasiQuotes
 import Language.LBNF.Compiletime
-printTree [HC.Def.expr|\x y z -> z y x |]
+printTree        [HC.Def.expr|\x y z -> z y x |]
+deBruijnIndex [] [HC.Def.expr|\x y z -> z y x |]
 -}
 
+data ITerm -- Inferable (Term ↑)
+  = Bound Int
+  | ITerm :@: CTerm  -- application
+  deriving (Eq, Show)
 
+data CTerm -- Checkable (Term ↓)
+  = Inf ITerm
+  | Lam CTerm
+  deriving (Eq, Show)
 
+deBruijnIndex :: [Ident] -> Expr -> CTerm
+deBruijnIndex env = \case
+  -- Ap  l r           -> (iterm l) :@: (deBruijnIndex env r)
+  VL  i []     body -> Lam (deBruijnIndex (i:env)          body)
+  VL  i (x:xs) body -> Lam (deBruijnIndex (i:env) (VL x xs body))
+  Ref i             -> maybe (error ("deBruijnIndex: " <> show i))
+                             (Inf . Bound) (elemIndex i env)
+ where
+  iterm :: Expr -> ITerm
+  iterm l = case deBruijnIndex env l of
+    Inf it -> it
+    _      -> error ("iterm: " <> show l)
