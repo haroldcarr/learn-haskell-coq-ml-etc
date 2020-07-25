a5b4c9c16ae9c960a75cee263072b7af5c622dee
Harold Carr <harold.carr@gmail.com>
2020-07-25 17:26:03 -0600

    refactor

--- haskell/topic/monads/2020-06-hc-reader-and-monad-write-state-io/src/HC4.hs
+++ haskell/topic/monads/2020-06-hc-reader-and-monad-write-state-io/src/HC4.hs
@@ -1,20 +1,22 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE NoImplicitPrelude      #-}
+{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+{-# LANGUAGE TemplateHaskell        #-}
 
 module HC4 where
 
 ------------------------------------------------------------------------------
-import MyMakeClassy
-import HC3
+import           HC3
+import           MyMakeClassy
 ------------------------------------------------------------------------------
-import Data.IORef
-import Control.Lens
-import Protolude
+import           Control.Lens
+import           Control.Monad.Writer.Strict
+import           Data.IORef
+import           Protolude
 ------------------------------------------------------------------------------
 
 data BlockStore a = BlockStore
@@ -24,8 +26,8 @@ data BlockStore a = BlockStore
 myMakeClassy ''BlockStore
 
 data RoundManager a = RoundManager
-  { _rmEpochState        :: Int
-  , _rmBlockStore        :: !(BlockStore a)
+  { _rmEpochState :: Int
+  , _rmBlockStore :: !(BlockStore a)
   } deriving (Eq, Show)
 myMakeClassy ''RoundManager
 
@@ -33,20 +35,26 @@ instance RWBlockStore (RoundManager a) a where
   lBlockStore = lens _rmBlockStore (\x y -> x { _rmBlockStore = y})
 
 process
-  :: ( Monad m, MonadRWS r w s m
+  :: ( Monad m, MonadRWS r [Text] s m
      , RWRoundManager s a, RWBlockStore s a )
   => m Int
 process = do
   bs <- use lBlockStore
   rm <- use lRoundManager
+  tell ["Hello"]
   bsInner      .=  99
   rmEpochState .= 999
   pure (bs^.bsInner + rm^.rmEpochState)
 
 top4 :: IO ()
 top4 = do
-  ior <- newIORef ([]::[Text], RoundManager 10 (BlockStore 200 3000))
-  a   <- runRWSIO process (-1::Int, ior)
-  r   <- readIORef ior
-  print (a, r :: ([Text], RoundManager Int))
+  x@(_, ref)       <- liftIO (initMonadRWS (-1::Int)
+                                           (RoundManager 10 (BlockStore 200 (3000::Int))))
+  a1               <- runRWSIO process x
+  (w1::[Text], s1) <- readIORef ref
+  print (a1, w1, s1)
 
+  liftIO (resetMonadRWS x s1)
+  a2               <- runRWSIO process x
+  (w2::[Text], s2) <- readIORef ref
+  print (a2, w2, s2)
