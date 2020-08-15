e1d003f43f1a1023fbbc8a3a26e612525a764789
Harold Carr <harold.carr@gmail.com>
2020-08-14 21:19:16 -0600

    cleanup

--- haskell/topic/network/zeromq/src/Example.hs
+++ haskell/topic/network/zeromq/src/Example.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -12,14 +13,20 @@ import qualified Data.Serialize                as S
 import           Protolude                     hiding (to)
 ------------------------------------------------------------------------------
 
+limit :: Int
+limit  = 10000
+
+newtype SignedRPC = SignedRPC Int deriving (Eq, Generic, Show)
+instance S.Serialize SignedRPC
+
 main :: IO ()
 main  = do
   let a = Addr "tcp://127.0.0.1:10000"
       b = Addr "tcp://127.0.0.1:10001"
   (at, ainr, aobw) <- setup a
   (bt, binr, bobw) <- setup b
-  runMsgServer at
-  runMsgServer bt
+  runMsgServer (at :: TransportEnv SignedRPC)
+  runMsgServer (bt :: TransportEnv SignedRPC)
   Async.concurrently_
     (sendMsgs b aobw)
     (Async.concurrently_
@@ -29,14 +36,18 @@ main  = do
         (recvMsgs binr)))
  where
   sendMsgs to c =
-    forM_ [1::Int ..] $ \m ->
-      U.writeChan c (OutBoundMsg (ROne to) (S.encode (SignedRPC (show m))))
+    forM_ [1::Int .. limit] $ \i ->
+      U.writeChan c (OutBoundMsg (ROne to) (S.encode (SignedRPC i)))
   recvMsgs mvc = do
     {-
     m <- getMsgSync mvc
     print (["receive", "decoded", show m]::[Text])
     -}
-    ms <- getBacklog mvc 50
-    for_ ms $ \m -> print (["receive", "decoded", show m]::[Text])
+    ms <- tryGetMsgs mvc 2000
+    for_ ms $ \m@(SignedRPC i) ->
+      -- print (["receive", "decoded", show m]::[Text])
+      when (i == limit) $ do
+        print (["receive", "decoded", show m]::[Text])
+        exitSuccess
     threadDelay 10000
     recvMsgs mvc
--- haskell/topic/network/zeromq/src/Lib.hs
+++ haskell/topic/network/zeromq/src/Lib.hs
@@ -31,13 +31,11 @@ import           System.ZMQ4.Monadic
 
 ------------------------------------------------------------------------------
 
-type Address      = Text
-newtype SignedRPC = SignedRPC Text deriving (Eq, Generic, Show)
-instance S.Serialize SignedRPC
+type Address = Text
 
-newtype Addr         a   = Addr         { _unAddr         :: a } deriving (Eq, Ord, Read, Show)
-newtype ListenOn     a   = ListenOn     { _unListenOn     :: a }
-newtype AddrToListen a s = AddrToListen { _unAddrToListen :: Map.Map (Addr a) (ListenOn s) }
+newtype Addr           a   = Addr           { unAddr           :: a } deriving (Eq, Ord, Show)
+newtype PushSock       a   = PushSock       { unPushSock       :: a }
+newtype AddrToPushSock a s = AddrToPushSock { unAddrToPushSock :: Map.Map (Addr a) (PushSock s) }
 
 -- | who to send a message to
 data Recipients a
@@ -47,21 +45,20 @@ data Recipients a
   deriving (Eq, Generic, Show)
 
 data OutBoundMsg addr msg = OutBoundMsg
-  { _obmTo   :: !(Recipients addr)
-  , _obmBody :: !msg
-  } deriving (Eq, Generic, Show)
+  { obmTo   :: !(Recipients addr)
+  , obmBody :: !msg
+  } deriving (Eq, Generic)
 
-data TransportEnv = TransportEnv
-  { inboxWrite :: !(UNB.InChan SignedRPC)
+data TransportEnv rpc = TransportEnv
+  { inboxWrite :: !(UNB.InChan rpc)
   , outboxRead :: !(U.OutChan  (OutBoundMsg Address ByteString))
   , myAddr     :: !(Addr Address)
-  , addrList   :: ![Addr Address]
-  }
+  , addrList   :: ![Addr Address] }
 
 setup
   :: Addr Address
-  -> IO ( TransportEnv
-        , MVar (UNB.Stream SignedRPC)
+  -> IO ( TransportEnv rpc
+        , MVar (UNB.Stream rpc)
         , U.InChan (OutBoundMsg Address ByteString) )
 setup me = do
   (inboxWrite , inboxRead)  <- newNoBlockChan
@@ -69,85 +66,85 @@ setup me = do
   pure ( TransportEnv inboxWrite outboxRead me []
        , inboxRead, outboxWrite )
 
-runMsgServer :: TransportEnv -> IO ()
+runMsgServer :: (Show rpc, S.Serialize rpc) => TransportEnv rpc -> IO ()
 runMsgServer te@TransportEnv{..} = void $ forkIO $ forever $ do
   zmqThread <- Async.async $ runZMQ $ do
-    ztprint [show (_unAddr myAddr), "launching ZMQ_THREAD"]
+    ztprint [show (unAddr myAddr), "launching ZMQ_THREAD"]
     -------------------------
-    ztprint [show (_unAddr myAddr), "launching ZMQ_RECEIVER"]
+    ztprint [show (unAddr myAddr), "launching ZMQ_RECEIVER"]
     zmqReceiver <- async $ do
-      void (recvProcess te)
-      ztprint [show (_unAddr myAddr), "exiting ZMQ_RECEIVER"]
+      void (receiver te)
+      ztprint [show (unAddr myAddr), "exiting ZMQ_RECEIVER"]
     -------------------------
-    -- be sure that the receive side is up
+    -- ensure the receive side is up
     liftIO (threadDelay 100000)
     -------------------------
-    ztprint [show (_unAddr myAddr), "launching ZMQ_SENDER"]
+    ztprint [show (unAddr myAddr), "launching ZMQ_SENDER"]
     zmqSender <- async $ do
-      rolodex <- addNewAddrs (AddrToListen Map.empty) addrList
-      void (sendProcess myAddr outboxRead rolodex)
-      ztprint [show (_unAddr myAddr), "exiting ZMQ_SENDER"]
+      rolodex <- addNewAddrs (AddrToPushSock Map.empty) addrList
+      void (sender myAddr outboxRead rolodex)
+      ztprint [show (unAddr myAddr), "exiting ZMQ_SENDER"]
     -------------------------
     liftIO $ Async.waitEitherCancel zmqReceiver zmqSender >>= \case
-      Left () -> tprint [show (_unAddr myAddr), "ZMQ_RECEIVER ()"]
-      Right v -> tprint [show (_unAddr myAddr), "ZMQ_SENDER", show v]
+      Left () -> tprint [show (unAddr myAddr), "ZMQ_RECEIVER ()"]
+      Right v -> tprint [show (unAddr myAddr), "ZMQ_SENDER", show v]
     -------------------------
     ztprint ["exiting ZMQ_THREAD"]
 
   res <- Async.waitCatch zmqThread
   Async.cancel zmqThread >> case res of
-    Right () -> tprint [show (_unAddr myAddr), "ZMQ_MSG_SERVER died Right ()"]
-    Left err -> tprint [show (_unAddr myAddr), "ZMQ_MSG_SERVER died Left", show err]
+    Right () -> tprint [show (unAddr myAddr), "ZMQ_MSG_SERVER died Right ()"]
+    Left err -> tprint [show (unAddr myAddr), "ZMQ_MSG_SERVER died Left", show err]
 
-recvProcess :: TransportEnv -> ZMQ z ()
-recvProcess TransportEnv {..} = do
+receiver :: (Show rpc, S.Serialize rpc) => TransportEnv rpc -> ZMQ z ()
+receiver TransportEnv {..} = do
   sock <- socket Pull
-  ztprint [show (_unAddr myAddr), "about to bind"]
-  _ <- bind sock (T.unpack (_unAddr myAddr))
+  ztprint [show (unAddr myAddr), "bind"]
+  _ <- bind sock (T.unpack (unAddr myAddr))
   forever $ do
-    newMsg <- receive sock -- get message from ZMQ4
-    ztprint [show (_unAddr myAddr), "recv", show newMsg]
+    newMsg <- receive sock -- GET MESSAGE FROM ZMQ
+    ztprint [show (unAddr myAddr), "recv", show newMsg]
     case S.decode newMsg of
       Left err -> do
-        ztprint [show (_unAddr myAddr), "failed S.decode", show newMsg, show err]
+        ztprint [show (unAddr myAddr), "failed S.decode", show newMsg, show err]
         liftIO yield
-      Right s@(SignedRPC _) ->
+      Right r ->
         liftIO $ do
-          UNB.writeChan inboxWrite s
-          tprint [ show (_unAddr myAddr), "S.decode", show s]
+          UNB.writeChan inboxWrite r
+          tprint [show (unAddr myAddr), "S.decode", show r]
           yield
 
-sendProcess
+sender
   :: Addr Address
   -> U.OutChan (OutBoundMsg Address ByteString)
-  -> AddrToListen Address (Socket z Push)
+  -> AddrToPushSock Address (Socket z Push)
   -> ZMQ z ()
-sendProcess me outboxRead !r = do
+sender me outboxRead !r = do
   rMvar <- liftIO (newMVar r)
   forever $ do
     (OutBoundMsg !addrs !msg) <- liftIO $! U.readChan outboxRead
-    ztprint [show (_unAddr me), "sending to", show addrs, "MSG", show msg]
+    ztprint [show (unAddr me), "sending to", show addrs, "MSG", show msg]
     r'      <- liftIO (takeMVar rMvar)
-    !newRol <- updateAddrToListen r' addrs
-    !toPoll <- recipList newRol addrs
-    mapM_ (\s -> send s [] msg) toPoll -- Give messages to ZMQ4.
+    !newRol <- updateAddrToPushSock r' addrs
+    !socks  <- recipList newRol addrs
+    mapM_ (\s -> send s [] msg) socks -- GIVE MESSAGES TO ZMQ
     liftIO (putMVar rMvar newRol)
-    ztprint [show (_unAddr me), "sent msg"]
+    ztprint [show (unAddr me), "sent msg"]
 
 ------------------------------------------------------------------------------
 
-updateAddrToListen
-  :: AddrToListen Address (Socket z Push)
+updateAddrToPushSock
+  :: AddrToPushSock Address (Socket z Push)
   -> Recipients Address
-  -> ZMQ z (AddrToListen Address (Socket z Push))
-updateAddrToListen r@(AddrToListen !_rol) RAll = pure $! r
-updateAddrToListen r@(AddrToListen !rol) (RSome !addrs) =
+  -> ZMQ z (AddrToPushSock Address (Socket z Push))
+updateAddrToPushSock r@(AddrToPushSock !_rol) RAll = pure $! r
+updateAddrToPushSock r@(AddrToPushSock !rol) (RSome !addrs) =
   if Set.isSubsetOf addrs $! Map.keysSet rol
   then pure $! r
   else do
     !a <- addNewAddrs r $! Set.toList addrs
     pure $! a
-updateAddrToListen r@(AddrToListen !rol) (ROne !addr) =
+updateAddrToPushSock r@(AddrToPushSock !rol) (ROne !addr) =
   if Set.member addr $! Map.keysSet rol
   then pure $! r
   else do
@@ -155,26 +152,26 @@ updateAddrToListen r@(AddrToListen !rol) (ROne !addr) =
     pure $! a
 
 addNewAddrs
-  :: AddrToListen Address (Socket z Push)
+  :: AddrToPushSock Address (Socket z Push)
   -> [Addr Address]
-  -> ZMQ z (AddrToListen Address (Socket z Push))
+  -> ZMQ z (AddrToPushSock Address (Socket z Push))
 addNewAddrs !r [] = pure r
-addNewAddrs (AddrToListen !r) (x:xs) = do
+addNewAddrs (AddrToPushSock !r) (x:xs) = do
   !r' <- if Map.member x r
-         then pure $! AddrToListen r
+         then pure $! AddrToPushSock r
          else do
            s <- socket Push
-           _ <- connect s (T.unpack (_unAddr x))
-           pure $! AddrToListen $! Map.insert x (ListenOn s) r
+           _ <- connect s (T.unpack (unAddr x))
+           pure $! AddrToPushSock $! Map.insert x (PushSock s) r
   r' `seq` addNewAddrs r' xs
 
 recipList
-  :: AddrToListen Address (Socket z Push)
+  :: AddrToPushSock Address (Socket z Push)
   -> Recipients Address
   -> ZMQ z [Socket z Push]
-recipList (AddrToListen r) RAll          = pure $! _unListenOn <$> Map.elems r
-recipList (AddrToListen r) (RSome addrs) = pure $! _unListenOn . (r Map.!) <$> Set.toList addrs
-recipList (AddrToListen r) (ROne addr)   = pure $! _unListenOn <$> [r Map.! addr]
+recipList (AddrToPushSock r) RAll          = pure $! unPushSock <$> Map.elems r
+recipList (AddrToPushSock r) (RSome addrs) = pure $! unPushSock . (r Map.!) <$> Set.toList addrs
+recipList (AddrToPushSock r) (ROne addr)   = pure $! unPushSock <$> [r Map.! addr]
 
 ------------------------------------------------------------------------------
 
@@ -186,7 +183,7 @@ newNoBlockChan = do
     Just x  -> newMVar x
   pure (w, r')
 
-getMsgSync :: MVar (UNB.Stream SignedRPC) -> IO SignedRPC
+getMsgSync :: MVar (UNB.Stream msg) -> IO msg
 getMsgSync m = do
   inboxRead <- takeMVar m
   t         <- UNB.tryReadNext inboxRead
@@ -194,27 +191,28 @@ getMsgSync m = do
     UNB.Pending           -> putMVar m inboxRead  >> getMsgSync m
     UNB.Next v inboxRead' -> putMVar m inboxRead' >> pure v
 
-getBacklog :: MVar (UNB.Stream SignedRPC) -> Int -> IO [SignedRPC]
-getBacklog m cnt = do
+tryGetMsgs :: Eq msg => MVar (UNB.Stream msg) -> Int -> IO [msg]
+tryGetMsgs m i0 = do
   inboxRead <- takeMVar m
-  let go strm cnt' =
-        if cnt' <= 0
-        then putMVar m strm >> pure []
-        else do
-          s <- UNB.tryReadNext strm
-          case s of
-            UNB.Next a strm' -> fmap (a:) (go strm' (cnt'-1))
-            UNB.Pending      -> putMVar m strm >> pure []
-  blog <- go inboxRead cnt
-  if blog /= []
-    then pure blog
+  msgs <- go inboxRead i0
+  if msgs /= []
+    then pure msgs
     else threadDelay 1000 >> pure []
+ where
+  go strm i =
+    if i <= 0
+    then putMVar m strm >> pure []
+    else do
+      s <- UNB.tryReadNext strm
+      case s of
+        UNB.Next a strm' -> fmap (a:) (go strm' (i - 1))
+        UNB.Pending      -> putMVar m strm >> pure []
 
 ------------------------------------------------------------------------------
 
 ztprint :: [Text] -> ZMQ z ()
-ztprint = liftIO . tprint
+ztprint _ = pure () -- liftIO . tprint
 
 tprint :: [Text] -> IO ()
-tprint  = print
+tprint _  = pure () -- print
 
